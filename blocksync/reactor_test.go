722b7213673565baf7bfbee3a4984cc83e4b3eb0
Sergio Mena <sergio@informal.systems>
2023-01-11 11:19:01 +0100

    Update blocksync/reactor_test.go

--- blocksync/reactor_test.go
+++ blocksync/reactor_test.go
@@ -373,7 +373,7 @@ func TestCheckSwitchToConsensusLastHeightZero(t *testing.T) {
 	}
 
 	// -1 because of "-1" in IsCaughtUp
-	// -1 pool.height is points to the _next_ height
+	// -1 pool.height points to the _next_ height
 	// -1 because we measure height of block store
 	const maxDiff = 3
 	for _, r := range reactorPairs {

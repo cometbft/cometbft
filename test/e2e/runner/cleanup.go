2d953cfcea1f25ad8df34e99b25cfcbed9443575
Sergio Mena <sergio@informal.systems>
2023-01-18 11:36:56 +0100

    Rename inside `test/e2e` (#139) (#144)

--- test/e2e/runner/cleanup.go
+++ test/e2e/runner/cleanup.go
@@ -62,7 +62,7 @@ func cleanupDir(dir string) error {
 
 	logger.Info("cleanup dir", "msg", log.NewLazySprintf("Removing testnet directory %q", dir))
 
-	// On Linux, some local files in the volume will be owned by root since Tendermint
+	// On Linux, some local files in the volume will be owned by root since CometBFT
 	// runs as root inside the container, so we need to clean them up from within a
 	// container running as root too.
 	absDir, err := filepath.Abs(dir)
@@ -70,7 +70,7 @@ func cleanupDir(dir string) error {
 		return err
 	}
 	err = execDocker("run", "--rm", "--entrypoint", "", "-v", fmt.Sprintf("%v:/network", absDir),
-		"tendermint/e2e-node", "sh", "-c", "rm -rf /network/*/")
+		"cometbft/e2e-node", "sh", "-c", "rm -rf /network/*/")
 	if err != nil {
 		return err
 	}

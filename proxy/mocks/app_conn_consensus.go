c397de094495637d4ee07ddcef5006169322f540
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2023-01-24 22:47:02 +0100

    updated mocks

--- proxy/mocks/app_conn_consensus.go
+++ proxy/mocks/app_conn_consensus.go
@@ -3,8 +3,9 @@
 package mocks
 
 import (
-	mock "github.com/stretchr/testify/mock"
 	abcicli "github.com/cometbft/cometbft/abci/client"
+	mock "github.com/stretchr/testify/mock"
+
 	types "github.com/cometbft/cometbft/abci/types"
 )
 

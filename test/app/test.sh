317dedbd96cda2a4165635dec67dece426c45082
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2023-01-31 11:38:13 +0100

    Renamed tm command

--- test/app/test.sh
+++ test/app/test.sh
@@ -48,52 +48,52 @@ function kvstore_over_socket_reorder(){
 
 function counter_over_socket() {
     rm -rf $TMHOME
-    tendermint init
+    cometbft init
     echo "Starting counter_over_socket"
     abci-cli counter --serial > /dev/null &
     pid_counter=$!
-    tendermint node > tendermint.log &
-    pid_tendermint=$!
+    cometbft node > cometbft.log &
+    pid_cometbft=$!
     sleep 5
 
     echo "running test"
     bash test/app/counter_test.sh "Counter over Socket"
 
-    kill -9 $pid_counter $pid_tendermint
+    kill -9 $pid_counter $pid_cometbft
 }
 
 function counter_over_grpc() {
     rm -rf $TMHOME
-    tendermint init
+    cometbft init
     echo "Starting counter_over_grpc"
     abci-cli counter --serial --abci grpc > /dev/null &
     pid_counter=$!
-    tendermint node --abci grpc > tendermint.log &
-    pid_tendermint=$!
+    cometbft node --abci grpc > cometbft.log &
+    pid_cometbft=$!
     sleep 5
 
     echo "running test"
     bash test/app/counter_test.sh "Counter over GRPC"
 
-    kill -9 $pid_counter $pid_tendermint
+    kill -9 $pid_counter $pid_cometbft
 }
 
 function counter_over_grpc_grpc() {
     rm -rf $TMHOME
-    tendermint init
+    cometbft init
     echo "Starting counter_over_grpc_grpc (ie. with grpc broadcast_tx)"
     abci-cli counter --serial --abci grpc > /dev/null &
     pid_counter=$!
     sleep 1
     GRPC_PORT=36656
-    tendermint node --abci grpc --rpc.grpc_laddr tcp://localhost:$GRPC_PORT > tendermint.log &
-    pid_tendermint=$!
+    cometbft node --abci grpc --rpc.grpc_laddr tcp://localhost:$GRPC_PORT > cometbft.log &
+    pid_cometbft=$!
     sleep 5
 
     echo "running test"
     GRPC_BROADCAST_TX=true bash test/app/counter_test.sh "Counter over GRPC via GRPC BroadcastTx"
 
-    kill -9 $pid_counter $pid_tendermint
+    kill -9 $pid_counter $pid_cometbft
 }
 
 case "$1" in 

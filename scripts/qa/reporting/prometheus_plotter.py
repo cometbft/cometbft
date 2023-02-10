b43f3163fc53a7d55e26caf60e7a0e1f18c83bd3
lasarojc <lasaro@informal.systems>
2023-02-10 12:23:50 -0300

    Updating scripts

--- scripts/qa/reporting/prometheus_plotter.py
+++ scripts/qa/reporting/prometheus_plotter.py
@@ -15,23 +15,40 @@ from urllib.parse import urljoin
 from prometheus_pandas import query
 
 release = 'v0.34.27'
-path = os.path.join('imgs','cmt2tm1')
+path = os.path.join('imgs')
 prometheus = query.Prometheus('http://localhost:9090')
+
+# Time window
+window_size = dict(seconds=120)
+ext_window_size = dict(seconds=150)
+
+#right_end = '2023-02-08T13:14:20Z' #cmt2 tm1
+right_end = '2023-02-08T10:33:20Z' #cmt1 tm2
+left_end = pd.to_datetime(right_end) - pd.Timedelta(**window_size)
+time_window = (left_end.strftime('%Y-%m-%dT%H:%M:%SZ'), right_end)
+
+ext_left_end = pd.to_datetime(right_end) - pd.Timedelta(**ext_window_size)
+ext_time_window = (ext_left_end.strftime('%Y-%m-%dT%H:%M:%SZ'), right_end)
 
 # Do prometheus queries
 queries = [ 
-    (( 'cometbft_p2p_peers',                        '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'peers',            dict(ylabel='Peers',     xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'cometbft_mempool_size',                     '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'mempool_size',     dict(ylabel='TXs',       xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True, kind='area',stacked=True)),
-    (( 'avg(cometbft_mempool_size)',                '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'avg_mempool_size', dict(ylabel='TXs',       xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'cometbft_consensus_rounds',                 '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'rounds',           dict(ylabel='# Rounds',  xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'cometbft_consensus_height',                 '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'blocks',           dict(ylabel='# Blocks',  xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)), 
-    (( 'rate(cometbft_consensus_height[1m])*60',    '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'block_rate',       dict(ylabel='Blocks/s',  xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'cometbft_consensus_total_txs',              '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'total_txs',        dict(ylabel='# TXs',     xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'rate(cometbft_consensus_total_txs[1m])*60', '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'total_txs_rate',   dict(ylabel='TXs/s',     xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'process_resident_memory_bytes',             '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'memory',           dict(ylabel='Bytes',     xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'avg(process_resident_memory_bytes)',        '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'avg_memory',       dict(ylabel='Bytes',     xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
-    (( 'node_load1',                                '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'cpu',              dict(ylabel='CPU Time',  xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)), 
-    (( 'avg(node_load1)',                           '2023-02-08T13:09:20Z', '2023-02-08T13:14:20Z', '1s'), 'avg_cpu',          dict(ylabel='CPU Time',  xlabel='time (s)', title='Peers', legend=False, figsize=(10,6), grid=True)),
+    (( 'cometbft_p2p_peers',                        time_window[0], time_window[1], '1s'), 'peers',            dict(ylabel='Peers',             xlabel='time (s)', title='Peers',                   legend=False, figsize=(10,6), grid=True)),
+    (( 'cometbft_mempool_size',                     time_window[0], time_window[1], '1s'), 'mempool_size',     dict(ylabel='TXs',               xlabel='time (s)', title='Mempool Size',            legend=False, figsize=(10,6), grid=True, kind='area',stacked=True)),
+    (( 'avg(cometbft_mempool_size)',                time_window[0], time_window[1], '1s'), 'avg_mempool_size', dict(ylabel='TXs',               xlabel='time (s)', title='Average Mempool Size',    legend=False, figsize=(10,6), grid=True)),
+    (( 'cometbft_consensus_rounds',                 time_window[0], time_window[1], '1s'), 'rounds',           dict(ylabel='# Rounds',          xlabel='time (s)', title='Rounds per block',        legend=False, figsize=(10,6), grid=True)),
+    (( 'cometbft_consensus_height',                 time_window[0], time_window[1], '1s'), 'blocks_regular',           dict(ylabel='# Blocks',          xlabel='time (s)', title='Blocks in time',          legend=False, figsize=(10,6), grid=True)), 
+    (( 'rate(cometbft_consensus_height[1m])*60',    time_window[0], time_window[1], '1s'), 'block_rate_regular',       dict(ylabel='Blocks/s',          xlabel='time (s)', title='Rate of block creation',  legend=False, figsize=(10,6), grid=True)),
+    (( 'cometbft_consensus_total_txs',              time_window[0], time_window[1], '1s'), 'total_txs_regular',        dict(ylabel='# TXs',             xlabel='time (s)', title='Transactions in time',    legend=False, figsize=(10,6), grid=True)),
+    (( 'rate(cometbft_consensus_total_txs[1m])*60', time_window[0], time_window[1], '1s'), 'total_txs_rate_regular',   dict(ylabel='TXs/s',             xlabel='time (s)', title='Rate of transaction processing', legend=False, figsize=(10,6), grid=True)),
+    (( 'process_resident_memory_bytes',             time_window[0], time_window[1], '1s'), 'memory',           dict(ylabel='Memory (bytes)',    xlabel='time (s)', title='Memory usage',            legend=False, figsize=(10,6), grid=True)),
+    (( 'avg(process_resident_memory_bytes)',        time_window[0], time_window[1], '1s'), 'avg_memory',       dict(ylabel='Memory (bytes)',    xlabel='time (s)', title='Average Memory usage',    legend=False, figsize=(10,6), grid=True)),
+    (( 'node_load1',                                time_window[0], time_window[1], '1s'), 'cpu',              dict(ylabel='Load',              xlabel='time (s)', title='Node load',               legend=False, figsize=(10,6), grid=True)), 
+    (( 'avg(node_load1)',                           time_window[0], time_window[1], '1s'), 'avg_cpu',          dict(ylabel='Load',              xlabel='time (s)', title='Average Node load',       legend=False, figsize=(10,6), grid=True)),
+    #extended window metrics
+    (( 'cometbft_consensus_height',                 ext_time_window[0], ext_time_window[1], '1s'), 'blocks',           dict(ylabel='# Blocks',          xlabel='time (s)', title='Blocks in time',          legend=False, figsize=(10,6), grid=True)), 
+    (( 'rate(cometbft_consensus_height[1m])*60',    ext_time_window[0], ext_time_window[1], '1s'), 'block_rate',       dict(ylabel='Blocks/s',          xlabel='time (s)', title='Rate of block creation',  legend=False, figsize=(10,6), grid=True)),
+    (( 'cometbft_consensus_total_txs',              ext_time_window[0], ext_time_window[1], '1s'), 'total_txs',        dict(ylabel='# TXs',             xlabel='time (s)', title='Transactions in time',    legend=False, figsize=(10,6), grid=True)),
+    (( 'rate(cometbft_consensus_total_txs[1m])*60', ext_time_window[0], ext_time_window[1], '1s'), 'total_txs_rate',   dict(ylabel='TXs/s',             xlabel='time (s)', title='Rate of transaction processing', legend=False, figsize=(10,6), grid=True)),
 ]
 
 for (query, file_name, pandas_params)  in queries:

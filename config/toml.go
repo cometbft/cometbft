cd2027cb6c396d714ef9fc0e5bdd5d07982ff350
hvanz <hernan.vanzetto@gmail.com>
2023-02-28 15:22:45 +0100

    Mark BlockSyncMode as deprecated; log warning message

--- config/toml.go
+++ config/toml.go
@@ -90,6 +90,7 @@ moniker = "{{ .BaseConfig.Moniker }}"
 # If this node is many blocks behind the tip of the chain, BlockSync
 # allows them to catchup quickly by downloading blocks in parallel
 # and verifying their commits
+# Deprecated: BlockSync will be enabled by default in the next major release.
 block_sync = {{ .BaseConfig.BlockSyncMode }}
 
 # Database backend: goleveldb | cleveldb | boltdb | rocksdb | badgerdb

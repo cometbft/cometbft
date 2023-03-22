209460394a527cfe2689968ee4ff004751d0a42d
Hernán Vanzetto <15466498+hvanz@users.noreply.github.com>
2023-03-22 12:59:02 +0100

    Add metrics to BlockSync (#538)
    * Add 4 metrics related to block
    
    * Remove unused function
    
    * Unregister metrics when BlockSync finishes
    
    * Add changelog
    
    * Rename method
    
    * Move method
    
    * Do not export unregister method
    
    * Do not unregister metrics

--- blocksync/reactor.go
+++ blocksync/reactor.go
@@ -396,6 +396,7 @@ FOR_LOOP:
 				// TODO This is bad, are we zombie?
 				panic(fmt.Sprintf("Failed to process committed block (%d:%X): %v", first.Height, first.Hash(), err))
 			}
+			bcR.metrics.recordBlockMetrics(first)
 			blocksSynced++
 
 			if blocksSynced%100 == 0 {

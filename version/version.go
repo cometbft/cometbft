cf82f58edcdc005ac0a1c007dd2288fd0bde1dd1
Thane Thomson <connect@thanethomson.com>
2023-03-28 17:12:18 -0400

    Bump ABCI version to 2.0.0
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- version/version.go
+++ version/version.go
@@ -5,7 +5,7 @@ const (
 	// when not using git describe. It is formatted with semantic versioning.
 	TMCoreSemVer = "0.39.0-dev"
 	// ABCISemVer is the semantic version of the ABCI protocol
-	ABCISemVer  = "1.0.0"
+	ABCISemVer  = "2.0.0"
 	ABCIVersion = ABCISemVer
 	// P2PProtocol versions all p2p behavior and msgs.
 	// This includes proposer selection.

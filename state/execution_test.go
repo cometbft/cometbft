c9e015e3449125e110a406783f394b5ecf3eee99
Sergio Mena <sergio@informal.systems>
2023-04-24 19:17:42 +0200

    Fill `time` field in `ExecCommitBlock` (#746)
    * Rephrase log when committed TX not in local mempool + make it debug
    
    * Add failing UT
    
    * Fill time in ExecCommitBlock
    
    * Fix UT logic
    
    * Revert unrelated commit "Rephrase log when committed TX not in local mempool + make it debug"
    
    * Fix lint
    
    (cherry picked from commit b8187b0f538195eae07ef4c79c2eb3c927bc181c)

--- state/execution_test.go
+++ state/execution_test.go
@@ -206,10 +206,11 @@ func TestFinalizeBlockValidators(t *testing.T) {
 		desc                     string
 		lastCommitSigs           []types.ExtendedCommitSig
 		expectedAbsentValidators []int
+		shouldHaveTime           bool
 	}{
-		{"none absent", []types.ExtendedCommitSig{commitSig0, commitSig1}, []int{}},
-		{"one absent", []types.ExtendedCommitSig{commitSig0, absentSig}, []int{1}},
-		{"multiple absent", []types.ExtendedCommitSig{absentSig, absentSig}, []int{0, 1}},
+		{"none absent", []types.ExtendedCommitSig{commitSig0, commitSig1}, []int{}, true},
+		{"one absent", []types.ExtendedCommitSig{commitSig0, absentSig}, []int{1}, true},
+		{"multiple absent", []types.ExtendedCommitSig{absentSig, absentSig}, []int{0, 1}, false},
 	}
 
 	for _, tc := range testCases {
@@ -223,7 +224,12 @@ func TestFinalizeBlockValidators(t *testing.T) {
 		block := makeBlock(state, 2, lastCommit.ToCommit())
 
 		_, err = sm.ExecCommitBlock(proxyApp.Consensus(), block, log.TestingLogger(), stateStore, 1)
-		require.Nil(t, err, tc.desc)
+		require.NoError(t, err, tc.desc)
+		require.True(t,
+			!tc.shouldHaveTime ||
+				app.LastTime.Equal(now) || app.LastTime.After(now),
+			"'last_time' should be at or after 'now'; tc %v, last_time %v, now %v", tc.desc, app.LastTime, now,
+		)
 
 		// -> app receives a list of validators with a bool indicating if they signed
 		ctr := 0

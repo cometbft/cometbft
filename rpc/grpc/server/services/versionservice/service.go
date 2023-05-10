86fda8a6248834f41721beaa87319c23c205aa75
Thane Thomson <connect@thanethomson.com>
2023-05-09 21:00:38 -0400

    rpc/grpc: Add server with support for version service
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

---
+++ rpc/grpc/server/services/versionservice/service.go
@@ -0,0 +1,25 @@
+package versionservice
+
+import (
+	context "context"
+
+	v1 "github.com/cometbft/cometbft/proto/tendermint/services/version/v1"
+	"github.com/cometbft/cometbft/version"
+)
+
+type versionServiceServer struct{}
+
+// New creates a new CometBFT version service server.
+func New() v1.VersionServiceServer {
+	return &versionServiceServer{}
+}
+
+// GetVersion implements v1.VersionServiceServer
+func (s *versionServiceServer) GetVersion(context.Context, *v1.GetVersionRequest) (*v1.GetVersionResponse, error) {
+	return &v1.GetVersionResponse{
+		Node:  version.TMCoreSemVer,
+		Abci:  version.ABCIVersion,
+		P2P:   version.P2PProtocol,
+		Block: version.BlockProtocol,
+	}, nil
+}

5a2f80a661e6549e1b56a0d573fdb12dd708f208
Thane Thomson <connect@thanethomson.com>
2023-05-12 08:10:19 -0400

    rpc/grpc: Update log message format to conform to RPC log style
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- rpc/grpc/server/server.go
+++ rpc/grpc/server/server.go
@@ -57,7 +57,7 @@ func WithVersionService() Option {
 // gRPC server does not log anything.
 func WithLogger(logger log.Logger) Option {
 	return func(b *serverBuilder) {
-		b.logger = logger.With("module", "grpcserver")
+		b.logger = logger.With("module", "grpc-server")
 	}
 }
 
@@ -84,6 +84,6 @@ func Serve(listener net.Listener, opts ...Option) error {
 		pbversionsvc.RegisterVersionServiceServer(server, b.versionService)
 		b.logger.Debug("Registered version service")
 	}
-	b.logger.Info("Starting gRPC server", "addr", listener.Addr())
+	b.logger.Info("serve", "msg", fmt.Sprintf("Starting gRPC server on %s", listener.Addr()))
 	return server.Serve(b.listener)
 }

d4422e785dbcc62dd837aba05fffbc1393ed85c4
Sergio Mena <sergio@informal.systems>
2023-05-16 20:31:22 +0200

    Extend the infrastructure provider with `StartNodes` and `StopTestnet` (#796)
    * shim in start and stop methods
    
    * rename to 'tendermint'
    
    * rename to 'kill tendermint'
    
    * add terminate vs kill
    
    * add command package
    
    * add param names
    
    * switch exec compose to docker pkg
    
    * all exec switched to docker package
    
    * remove runner exec
    
    * functionality in place for start kill terminate
    
    * exec compose takes context
    
    * compose verbose takes context
    
    * docker.exec takes context
    
    * add create node to infra provider
    
    * start and create calls replaced with infra provider
    
    * commit exec pkg
    
    * add ssh pkg
    
    * add digital ocean package
    
    * add client creation logic
    
    * use ssh agent to auth to remote
    
    * plump infra data into tests
    
    * move agent socket dial to client connection creation
    
    * error if auth sock not defined
    
    * allow unknown hosts to be accessed
    
    * return session error
    
    * close connection in ssh exec
    
    * gofmt
    
    * use IP.Equal instead of bytes.Equal
    
    * correct the nolint comment format
    
    * remove backup for INFRASTRUCTURE_DATA missing
    
    * Update test/e2e/pkg/infra/digitalocean/digitalocean.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * remove hostkeyalgorithm setting
    
    * rename IP node field
    
    * add external address field
    
    * use external IP in digital ocean
    
    * use correct variable in e2e compose
    
    * use --no-start
    
    * Fix reachable IPs on docker
    
    * Rename to CometBFT
    
    * Simplify Path
    
    * Revert  GetReachableIP
    
    * Improve ext IP
    
    * Remove CreateNode
    
    * Adapt provider interface to actual needs
    
    * Minor in digitalocean
    
    * Doc
    
    * Fix contexts
    
    * Update test/e2e/pkg/infra/digitalocean/digitalocean.go
    
    * Update test/e2e/pkg/exec/exec.go
    
    Co-authored-by: Lasaro <lasaro@informal.systems>
    
    * Remove `cli.ifd`, first version
    
    * Remove `cli.ifd`, reduce duplication
    
    * Remove references to Docker in help strings
    
    ---------
    
    Co-authored-by: William Banfield <wbanfield@gmail.com>
    Co-authored-by: William Banfield <4561443+williambanfield@users.noreply.github.com>
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    Co-authored-by: Lasaro <lasaro@informal.systems>
    (cherry picked from commit 0070a86b17357ad61c55f34742df414111900d8c)

--- test/e2e/runner/perturb.go
+++ test/e2e/runner/perturb.go
@@ -8,6 +8,7 @@ import (
 	"github.com/cometbft/cometbft/libs/log"
 	rpctypes "github.com/cometbft/cometbft/rpc/core/types"
 	e2e "github.com/cometbft/cometbft/test/e2e/pkg"
+	"github.com/cometbft/cometbft/test/e2e/pkg/infra/docker"
 )
 
 // Perturbs a running testnet.
@@ -28,7 +29,7 @@ func Perturb(ctx context.Context, testnet *e2e.Testnet) error {
 // after recovering.
 func PerturbNode(ctx context.Context, node *e2e.Node, perturbation e2e.Perturbation) (*rpctypes.ResultStatus, error) {
 	testnet := node.Testnet
-	out, err := execComposeOutput(testnet.Dir, "ps", "-q", node.Name)
+	out, err := docker.ExecComposeOutput(context.Background(), testnet.Dir, "ps", "-q", node.Name)
 	if err != nil {
 		return nil, err
 	}
@@ -45,36 +46,36 @@ func PerturbNode(ctx context.Context, node *e2e.Node, perturbation e2e.Perturbat
 	switch perturbation {
 	case e2e.PerturbationDisconnect:
 		logger.Info("perturb node", "msg", log.NewLazySprintf("Disconnecting node %v...", node.Name))
-		if err := execDocker("network", "disconnect", testnet.Name+"_"+testnet.Name, name); err != nil {
+		if err := docker.Exec(context.Background(), "network", "disconnect", testnet.Name+"_"+testnet.Name, name); err != nil {
 			return nil, err
 		}
 		time.Sleep(10 * time.Second)
-		if err := execDocker("network", "connect", testnet.Name+"_"+testnet.Name, name); err != nil {
+		if err := docker.Exec(context.Background(), "network", "connect", testnet.Name+"_"+testnet.Name, name); err != nil {
 			return nil, err
 		}
 
 	case e2e.PerturbationKill:
 		logger.Info("perturb node", "msg", log.NewLazySprintf("Killing node %v...", node.Name))
-		if err := execCompose(testnet.Dir, "kill", "-s", "SIGKILL", name); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "kill", "-s", "SIGKILL", name); err != nil {
 			return nil, err
 		}
-		if err := execCompose(testnet.Dir, "start", name); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "start", name); err != nil {
 			return nil, err
 		}
 
 	case e2e.PerturbationPause:
 		logger.Info("perturb node", "msg", log.NewLazySprintf("Pausing node %v...", node.Name))
-		if err := execCompose(testnet.Dir, "pause", name); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "pause", name); err != nil {
 			return nil, err
 		}
 		time.Sleep(10 * time.Second)
-		if err := execCompose(testnet.Dir, "unpause", name); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "unpause", name); err != nil {
 			return nil, err
 		}
 
 	case e2e.PerturbationRestart:
 		logger.Info("perturb node", "msg", log.NewLazySprintf("Restarting node %v...", node.Name))
-		if err := execCompose(testnet.Dir, "restart", name); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "restart", name); err != nil {
 			return nil, err
 		}
 
@@ -95,11 +96,11 @@ func PerturbNode(ctx context.Context, node *e2e.Node, perturbation e2e.Perturbat
 			log.NewLazySprintf("Upgrading node %v from version '%v' to version '%v'...",
 				node.Name, oldV, newV))
 
-		if err := execCompose(testnet.Dir, "stop", name); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "stop", name); err != nil {
 			return nil, err
 		}
 		time.Sleep(10 * time.Second)
-		if err := execCompose(testnet.Dir, "up", "-d", name+"_u"); err != nil {
+		if err := docker.ExecCompose(context.Background(), testnet.Dir, "up", "-d", name+"_u"); err != nil {
 			return nil, err
 		}
 

50145bfe37a2528cc54b30a208b36e18c6398ed6
Hernán Vanzetto <15466498+hvanz@users.noreply.github.com>
2023-06-12 21:50:09 +0200

    e2e: Generate prometheus.yaml on setup (#954)
    * Generate prometheus yaml on setup
    
    * change file extension
    
    * move template to file
    
    * add templates dir
    
    (cherry picked from commit 3701c9f3c1a12a0ce35cef98788bdebb382bd74e)

--- test/e2e/pkg/testnet.go
+++ test/e2e/pkg/testnet.go
@@ -1,21 +1,26 @@
 package e2e
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
 	"io"
 	"math/rand"
 	"net"
+	"os"
 	"path/filepath"
 	"sort"
 	"strconv"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/cometbft/cometbft/crypto"
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/cometbft/cometbft/crypto/secp256k1"
 	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
+
+	_ "embed"
 )
 
 const (
@@ -468,6 +473,34 @@ func (t Testnet) HasPerturbations() bool {
 		}
 	}
 	return false
+}
+
+//go:embed templates/prometheus-yaml.tmpl
+var prometheusYamlTemplate string
+
+func (t Testnet) prometheusConfigBytes() ([]byte, error) {
+	tmpl, err := template.New("prometheus-yaml").Parse(prometheusYamlTemplate)
+	if err != nil {
+		return nil, err
+	}
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, t)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (t Testnet) WritePrometheusConfig() error {
+	bytes, err := t.prometheusConfigBytes()
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(filepath.Join(t.Dir, "prometheus.yaml"), bytes, 0o644) //nolint:gosec
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Address returns a P2P endpoint address for the node.

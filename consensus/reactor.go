0e78f7e4137506d12038fc5a14a0b43684971985
mmsqe <mavis@crypto.com>
2023-06-14 21:28:48 +0800

    fix: avoid recursive call after rename to (*PeerState).MarshalJSON (#865)
    * avoid recursive call after rename to (*PeerState).MarshalJSON
    
    * add test
    
    * add change doc
    
    * explain for nolint
    
    * fix lint
    
    * fix golangci-lint to v1.52.2
    
    * fix golangci-lint to v1.52.2
    
    * Revert "fix golangci-lint to v1.52.2"
    
    This reverts commit 598a9ef4c86fc29cf038251676c33a222217826c.
    
    * Revert "fix golangci-lint to v1.52.2"
    
    This reverts commit a8aad121e27382813e95b1911b1b560c62e1c7c3.
    
    * Reintroduced `cmtjson`
    
    * Avoid copying Mutex
    
    * Avoid copying Mutex -- 2nd try, more succint
    
    * Update .changelog/unreleased/bug-fixes/865-fix-peerstate-marshaljson.md
    
    * Update consensus/reactor_test.go
    
    ---------
    
    Co-authored-by: Sergio Mena <sergio@informal.systems>

--- consensus/reactor.go
+++ consensus/reactor.go
@@ -1089,7 +1089,8 @@ func (ps *PeerState) MarshalJSON() ([]byte, error) {
 	ps.mtx.Lock()
 	defer ps.mtx.Unlock()
 
-	return cmtjson.Marshal(ps)
+	type jsonPeerState PeerState
+	return cmtjson.Marshal((*jsonPeerState)(ps))
 }
 
 // GetHeight returns an atomic snapshot of the PeerRoundState's height

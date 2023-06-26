b73d518b1386432fcd02c2bd574493723ca1f0b1
otrack <0track@gmail.com>
2023-06-26 18:53:52 +0200

    update

--- test/simulator/pkg/infra/docker/docker.go
+++ test/simulator/pkg/infra/docker/docker.go
@@ -40,7 +40,7 @@ func (p Provider) StartNodes(ctx context.Context, nodes ...*e2e.Node) error {
 	for i, n := range nodes {
 		nodeNames[i] = n.Name
 	}
-	return ExecCompose(ctx, p.Testnet.Dir, append([]string{"up", "-d", "prometheus"}, nodeNames...)...)
+	return ExecCompose(ctx, p.Testnet.Dir, "up", "--detach")
 }
 func (p Provider) StopTestnet(ctx context.Context) error {
 	return ExecCompose(ctx, p.Testnet.Dir, "down")
@@ -159,22 +159,23 @@ services:
 
 // ExecCompose runs a Docker Compose command for a testnet.
 func ExecCompose(ctx context.Context, dir string, args ...string) error {
-	return exec.Command(ctx, append(
-		[]string{"docker-compose", "-f", filepath.Join(dir, "docker-compose.yml")},
-		args...)...)
+	str := append(
+		[]string{"docker", "compose", "-f", filepath.Join(dir, "docker-compose.yml")},
+		args...)
+	return exec.Command(ctx, str...)
 }
 
 // ExecCompose runs a Docker Compose command for a testnet and returns the command's output.
 func ExecComposeOutput(ctx context.Context, dir string, args ...string) ([]byte, error) {
 	return exec.CommandOutput(ctx, append(
-		[]string{"docker-compose", "-f", filepath.Join(dir, "docker-compose.yml")},
+		[]string{"docker", "compose", "-f", filepath.Join(dir, "docker-compose.yml")},
 		args...)...)
 }
 
 // ExecComposeVerbose runs a Docker Compose command for a testnet and displays its output.
 func ExecComposeVerbose(ctx context.Context, dir string, args ...string) error {
 	return exec.CommandVerbose(ctx, append(
-		[]string{"docker-compose", "-f", filepath.Join(dir, "docker-compose.yml")},
+		[]string{"docker", "compose", "-f", filepath.Join(dir, "docker-compose.yml")},
 		args...)...)
 }
 

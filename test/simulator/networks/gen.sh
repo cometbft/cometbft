37d969d6d504e0a1deab831a7ddde4c2638ac4d6
otrack <0track@gmail.com>
2023-06-27 10:23:58 +0200

    update

--- test/simulator/networks/gen.sh
+++ test/simulator/networks/gen.sh
@@ -7,10 +7,12 @@ fi
 
 NODES=$1
 RATE=$2
+SEED="validator01"
 PREAMBLE="prometheus = true\nload_tx_size_bytes = 100\nload_tx_batch_size = 100\npropagation_ratio=${RATE}"
 
 echo -e $PREAMBLE
 for i in $(seq -f "%02g" 1 ${NODES})
 do
     echo "[node.validator${i}]"
+    echo "  seeds = [\"${SEED}\"]"
 done

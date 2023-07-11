e9859338133a65c4105ff3070eca7c478a6fd73a
Andy Nogueira <me@andynogueira.dev>
2023-07-11 17:48:48 -0400

    changing client return type, use core type (#1094)

--- rpc/grpc/client/block_service.go
+++ rpc/grpc/client/block_service.go
@@ -4,12 +4,19 @@ import (
 	"context"
 
 	v1 "github.com/cometbft/cometbft/proto/tendermint/services/block/v1"
+	"github.com/cometbft/cometbft/types"
 	"github.com/cosmos/gogoproto/grpc"
 )
 
+// ResultBlock Single block (with meta)
+type ResultBlock struct {
+	BlockID types.BlockID `json:"block_id"`
+	Block   *types.Block  `json:"block"`
+}
+
 // BlockServiceClient provides block information
 type BlockServiceClient interface {
-	GetBlock(ctx context.Context, request *v1.GetBlockRequest) (*v1.GetBlockResponse, error)
+	GetBlock(ctx context.Context, height int64) (*ResultBlock, error)
 }
 
 type blockServiceClient struct {
@@ -23,12 +30,31 @@ func newBlockServiceClient(conn grpc.ClientConn) BlockServiceClient {
 }
 
 // GetBlock implements BlockServiceClient
-func (c *blockServiceClient) GetBlock(ctx context.Context, request *v1.GetBlockRequest) (*v1.GetBlockResponse, error) {
-	res, err := c.client.GetBlock(ctx, request)
+func (c *blockServiceClient) GetBlock(ctx context.Context, height int64) (*ResultBlock, error) {
+	req := v1.GetBlockRequest{
+		Height: height,
+	}
+	res, err := c.client.GetBlock(ctx, &req)
 	if err != nil {
 		return nil, err
 	}
-	return res, nil
+
+	partSetHeader := types.PartSetHeader{
+		Total: res.BlockId.PartSetHeader.Total,
+		Hash:  res.BlockId.PartSetHeader.Hash,
+	}
+
+	blockID := types.BlockID{
+		Hash:          res.BlockId.Hash,
+		PartSetHeader: partSetHeader,
+	}
+
+	// TODO: convert Block
+	response := ResultBlock{
+		BlockID: blockID,
+		Block:   nil,
+	}
+	return &response, nil
 }
 
 type disabledBlockServiceClient struct{}
@@ -38,6 +64,6 @@ func newDisabledBlockServiceClient() BlockServiceClient {
 }
 
 // GetBlock implements BlockServiceClient
-func (*disabledBlockServiceClient) GetBlock(context.Context, *v1.GetBlockRequest) (*v1.GetBlockResponse, error) {
+func (*disabledBlockServiceClient) GetBlock(context.Context, int64) (*ResultBlock, error) {
 	panic("block service client is disabled")
 }

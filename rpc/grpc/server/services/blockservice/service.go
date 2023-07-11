c8e5989b86dca23b2efb0c2b1e3354cc5efc1be7
Andy Nogueira <me@andynogueira.dev>
2023-07-11 11:22:36 -0400

    convert core type to proto type (#1094)

--- rpc/grpc/server/services/blockservice/service.go
+++ rpc/grpc/server/services/blockservice/service.go
@@ -3,8 +3,6 @@ package blockservice
 import (
 	context "context"
 	v1 "github.com/cometbft/cometbft/proto/tendermint/services/block/v1"
-	proto "github.com/cometbft/cometbft/proto/tendermint/types"
-	cmtversion "github.com/cometbft/cometbft/proto/tendermint/version"
 	"github.com/cometbft/cometbft/rpc/core"
 	rpctypes "github.com/cometbft/cometbft/rpc/jsonrpc/types"
 )
@@ -25,89 +23,12 @@ func (s *blockServiceServer) GetBlock(ctx context.Context, req *v1.GetBlockReque
 		return nil, err
 	}
 
-	partSetHeader := proto.PartSetHeader{
-		Total: resp.BlockID.PartSetHeader.Total,
-		Hash:  resp.BlockID.PartSetHeader.Hash,
+	block, err := resp.Block.ToProto()
+	if err != nil {
+		return nil, err
 	}
-
-	blockID := proto.BlockID{
-		Hash:          resp.BlockID.Hash,
-		PartSetHeader: partSetHeader,
-	}
-
-	lastBlockIDPartSetHeader := proto.PartSetHeader{
-		Total: resp.Block.LastBlockID.PartSetHeader.Total,
-		Hash:  resp.Block.LastBlockID.PartSetHeader.Hash,
-	}
-
-	lastBlockID := proto.BlockID{
-		Hash:          resp.Block.LastBlockID.Hash,
-		PartSetHeader: lastBlockIDPartSetHeader,
-	}
-
-	version := cmtversion.Consensus{
-		Block: resp.Block.Version.Block,
-		App:   resp.Block.Version.Block,
-	}
-
-	header := proto.Header{
-		Version:            version,
-		ChainID:            resp.Block.ChainID,
-		Height:             resp.Block.Height,
-		Time:               resp.Block.Time,
-		LastBlockId:        lastBlockID,
-		LastCommitHash:     resp.Block.LastCommitHash,
-		DataHash:           resp.Block.DataHash,
-		ValidatorsHash:     resp.Block.ValidatorsHash,
-		NextValidatorsHash: resp.Block.NextValidatorsHash,
-		ConsensusHash:      resp.Block.ConsensusHash,
-		AppHash:            resp.Block.AppHash,
-		LastResultsHash:    resp.Block.LastResultsHash,
-		EvidenceHash:       resp.Block.EvidenceHash,
-		ProposerAddress:    resp.Block.ProposerAddress,
-	}
-
-	data := proto.Data{
-		Txs: resp.Block.Data.Txs.ToSliceOfBytes(),
-	}
-
-	var commitSigs []proto.CommitSig
-
-	for _, signature := range resp.Block.LastCommit.Signatures {
-		commit := proto.CommitSig{
-			BlockIdFlag:      proto.BlockIDFlag(signature.BlockIDFlag),
-			ValidatorAddress: signature.ValidatorAddress,
-			Timestamp:        signature.Timestamp,
-			Signature:        signature.Signature,
-		}
-		commitSigs = append(commitSigs, commit)
-	}
-
-	lastCommitBlockIDPartSetHeader := proto.PartSetHeader{
-		Total: resp.Block.LastCommit.BlockID.PartSetHeader.Total,
-		Hash:  resp.Block.LastCommit.BlockID.PartSetHeader.Hash,
-	}
-
-	lastCommitBlockID := proto.BlockID{
-		Hash:          resp.Block.LastCommit.BlockID.Hash,
-		PartSetHeader: lastCommitBlockIDPartSetHeader,
-	}
-
-	var lastCommit = proto.Commit{
-		Height:     resp.Block.LastCommit.Height,
-		Round:      resp.Block.LastCommit.Round,
-		BlockID:    lastCommitBlockID,
-		Signatures: commitSigs,
-	}
-	block := proto.Block{
-		Header:     header,
-		Data:       data,
-		Evidence:   proto.EvidenceList{}, //TODO: Convert evidence to proto
-		LastCommit: &lastCommit,
-	}
-
 	return &v1.GetBlockResponse{
-		BlockId: blockID,
-		Block:   block,
+		BlockId: resp.BlockID.ToProto(),
+		Block:   *block,
 	}, nil
 }

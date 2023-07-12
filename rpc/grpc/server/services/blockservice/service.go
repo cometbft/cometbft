f0cb09f975e4f2cd271ddb9ba3151306432fe8a6
Andy Nogueira <me@andynogueira.dev>
2023-07-12 16:26:07 -0400

    adding proper grpc error handling and return (#1094)

--- rpc/grpc/server/services/blockservice/service.go
+++ rpc/grpc/server/services/blockservice/service.go
@@ -2,9 +2,13 @@ package blockservice
 
 import (
 	context "context"
+	"fmt"
 
 	v1 "github.com/cometbft/cometbft/proto/tendermint/services/block/v1"
 	"github.com/cometbft/cometbft/store"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type blockServiceServer struct {
@@ -22,12 +26,36 @@ func New(store *store.BlockStore) v1.BlockServiceServer {
 func (s *blockServiceServer) GetBlock(ctx context.Context, req *v1.GetBlockRequest) (*v1.GetBlockResponse, error) {
 	var height int64
 
+	// retrieve the last height in the store
+	latestHeight := s.store.Height()
+
 	// validate height parameter, if height is 0 or
 	// the request is nil, then use the latest height
 	if req.Height == 0 {
-		height = s.store.Height()
+		height = latestHeight
 	} else {
 		height = req.Height
+	}
+
+	// check if the height requested is not higher
+	// than the latest height in the store
+	if height > latestHeight {
+		st := status.New(codes.InvalidArgument, "invalid height")
+		description := fmt.Sprintf("height requested (%d) is higher than the latest available height (%d)", height, latestHeight)
+		v := &errdetails.BadRequest_FieldViolation{
+			Field:       "height",
+			Description: description,
+		}
+		br := &errdetails.BadRequest{}
+		br.FieldViolations = append(br.FieldViolations, v)
+		st, err := st.WithDetails(br)
+		// if there is an issue adding details just return a simple
+		// error message without details
+		if err != nil {
+			err := status.Error(codes.InvalidArgument, description)
+			return nil, err
+		}
+		return nil, st.Err()
 	}
 
 	block := s.store.LoadBlock(height)

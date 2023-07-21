a7dceb4e432a8d50752dede059caccf121124b9c
otrack <0track@gmail.com>
2023-07-21 15:58:23 +0200

    remove the use of experimental options in generate.go

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -65,11 +65,10 @@ var (
 )
 
 type generateConfig struct {
-	randSource                         *rand.Rand
-	outputDir                          string
-	multiVersion                       string
-	prometheus                         bool
-	experimentalGossipPropagationRatio float32
+	randSource   *rand.Rand
+	outputDir    string
+	multiVersion string
+	prometheus   bool
 }
 
 // Generate generates random testnets using the given RNG.
@@ -111,7 +110,7 @@ func Generate(cfg *generateConfig) ([]e2e.Manifest, error) {
 	}
 	manifests := []e2e.Manifest{}
 	for _, opt := range combinations(testnetCombinations) {
-		manifest, err := generateTestnet(cfg.randSource, opt, upgradeVersion, cfg.prometheus, cfg.experimentalGossipPropagationRatio)
+		manifest, err := generateTestnet(cfg.randSource, opt, upgradeVersion, cfg.prometheus)
 		if err != nil {
 			return nil, err
 		}
@@ -121,19 +120,18 @@ func Generate(cfg *generateConfig) ([]e2e.Manifest, error) {
 }
 
 // generateTestnet generates a single testnet with the given options.
-func generateTestnet(r *rand.Rand, opt map[string]interface{}, upgradeVersion string, prometheus bool, propagationRatio float32) (e2e.Manifest, error) {
+func generateTestnet(r *rand.Rand, opt map[string]interface{}, upgradeVersion string, prometheus bool) (e2e.Manifest, error) {
 	manifest := e2e.Manifest{
-		IPv6:                              ipv6.Choose(r).(bool),
-		ABCIProtocol:                      nodeABCIProtocols.Choose(r).(string),
-		InitialHeight:                     int64(opt["initialHeight"].(int)),
-		InitialState:                      opt["initialState"].(map[string]string),
-		Validators:                        &map[string]int64{},
-		ValidatorUpdates:                  map[string]map[string]int64{},
-		Evidence:                          evidence.Choose(r).(int),
-		Nodes:                             map[string]*e2e.ManifestNode{},
-		UpgradeVersion:                    upgradeVersion,
-		Prometheus:                        prometheus,
-		ExperimentalGossipPropagationRate: propagationRatio,
+		IPv6:             ipv6.Choose(r).(bool),
+		ABCIProtocol:     nodeABCIProtocols.Choose(r).(string),
+		InitialHeight:    int64(opt["initialHeight"].(int)),
+		InitialState:     opt["initialState"].(map[string]string),
+		Validators:       &map[string]int64{},
+		ValidatorUpdates: map[string]map[string]int64{},
+		Evidence:         evidence.Choose(r).(int),
+		Nodes:            map[string]*e2e.ManifestNode{},
+		UpgradeVersion:   upgradeVersion,
+		Prometheus:       prometheus,
 	}
 
 	switch abciDelays.Choose(r).(string) {

54d328a01133d08497ad4953b688997b2e329a9f
Thane Thomson <connect@thanethomson.com>
2023-07-27 14:25:07 -0400

    blockservice: Add trivial RPC error response tracing mechanism for easier debugging
    Signed-off-by: Thane Thomson <connect@thanethomson.com>

--- rpc/grpc/server/services/blockservice/service.go
+++ rpc/grpc/server/services/blockservice/service.go
@@ -4,12 +4,12 @@ import (
 	context "context"
 	"fmt"
 
+	"github.com/cometbft/cometbft/internal/rpctrace"
 	"github.com/cometbft/cometbft/libs/log"
 	cmtpubsub "github.com/cometbft/cometbft/libs/pubsub"
 	blocksvc "github.com/cometbft/cometbft/proto/tendermint/services/block/v1"
 	"github.com/cometbft/cometbft/store"
 	"github.com/cometbft/cometbft/types"
-	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -73,20 +73,17 @@ func (s *blockServiceServer) GetByHeight(_ context.Context, req *blocksvc.GetByH
 func (s *blockServiceServer) GetLatestHeight(_ *blocksvc.GetLatestHeightRequest, stream blocksvc.BlockService_GetLatestHeightServer) error {
 	logger := s.logger.With("endpoint", "GetLatestHeight")
 
-	// Generate a unique subscriber ID using a UUID
-	// The subscriber needs to be unique across all clients
-	id, err := uuid.NewUUID()
+	traceID, err := rpctrace.New()
 	if err != nil {
-		// cannot generate unique id
-		logger.Error("Error generating subscriber id", "err", err)
-		return status.Error(codes.Internal, "Error generating subscriber id")
+		logger.Error("Error generating RPC trace ID", "err", err)
+		return status.Error(codes.Internal, "Internal server error")
 	}
-	subscriber := id.String()
 
-	sub, err := s.eventBus.Subscribe(context.Background(), subscriber, types.QueryForEvent(types.EventNewBlock), 1)
+	// The trace ID is reused as a unique subscriber ID
+	sub, err := s.eventBus.Subscribe(context.Background(), traceID, types.QueryForEvent(types.EventNewBlock), 1)
 	if err != nil {
-		logger.Error("Cannot subscribe to new block events", "err", err)
-		return status.Error(codes.Internal, "Cannot subscribe to new block events")
+		logger.Error("Cannot subscribe to new block events", "err", err, "traceID", traceID)
+		return status.Errorf(codes.Internal, "Cannot subscribe to new block events (see logs for trace ID: %s)", traceID)
 	}
 
 	for {
@@ -95,8 +92,8 @@ func (s *blockServiceServer) GetLatestHeight(_ *blocksvc.GetLatestHeightRequest,
 			switch eventType := msg.Data().(type) {
 			case types.EventDataNewBlock:
 				if err := stream.Send(&blocksvc.GetLatestHeightResponse{Height: eventType.Block.Height}); err != nil {
-					logger.Error("Failed to stream new block", "err", err, "height", eventType.Block.Height, "subscriber", subscriber)
-					return status.Error(codes.Unavailable, "Cannot send stream response")
+					logger.Error("Failed to stream new block", "err", err, "height", eventType.Block.Height, "traceID", traceID)
+					return status.Errorf(codes.Unavailable, "Cannot send stream response (see logs for trace ID: %s)", traceID)
 				}
 			}
 		case <-sub.Canceled():
@@ -106,15 +103,15 @@ func (s *blockServiceServer) GetLatestHeight(_ *blocksvc.GetLatestHeightRequest,
 			case nil:
 				return status.Error(codes.Canceled, "Subscription canceled without errors")
 			default:
-				logger.Info("Subscription canceled with errors", "err", sub.Err(), "subscriber", subscriber)
-				return status.Error(codes.Canceled, "Subscription canceled with errors")
+				logger.Info("Subscription canceled with errors", "err", sub.Err(), "traceID", traceID)
+				return status.Errorf(codes.Canceled, "Subscription canceled with errors (see logs for trace ID: %s)", traceID)
 			}
 		default:
 			continue
 		}
 		if sub.Err() != nil {
-			logger.Error("New block subscription error", "err", sub.Err(), "subscriber", subscriber)
-			return status.Error(codes.Internal, "New block subscription error")
+			logger.Error("New block subscription error", "err", sub.Err(), "traceID", traceID)
+			return status.Errorf(codes.Internal, "New block subscription error (see logs for trace ID: %s)", traceID)
 		}
 	}
 }

ca0a1ba8f4a12037368dfff11b13eb7573966b42
Pierre Sutra <0track@gmail.com>
2023-07-31 18:23:36 +0200

    Update test/e2e/fast-prototyping/render.sh
    Co-authored-by: lasaro <lasaro@gmail.com>

--- test/e2e/fast-prototyping/render.sh
+++ test/e2e/fast-prototyping/render.sh
@@ -6,7 +6,7 @@ source ${DIR}/utils.sh
 trap "pkill -KILL -P $$; exit 255" SIGINT SIGTERM
 
 python3 -m http.server --directory ${DIR}/render 8080 &
-browse http://localhost:8080/experiments.html &
+echo "Point your browser to http://localhost:8080/experiments.html"
 
 wait
 

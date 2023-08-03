38098f89ca1b8e3dffb9a73b2343b419ead72c14
lg <8335464+glnro@users.noreply.github.com>
2023-08-03 23:04:44 +0200

    Add BlockResults gRPC service (#1168)
    * Add block results grpc endpoint
    
    * Make requested changes
    
    * Fix linting
    
    * Lint proto
    
    * Fix test race condition
    
    * Update test order
    
    * Update rpc/grpc/server/services/blockresultservice/service.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * Update rpc/grpc/server/services/blockresultservice/service.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * Update config/toml.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * Update rpc/grpc/server/services/blockresultservice/service.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * Update rpc/grpc/server/services/blockresultservice/service.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * Format protos
    
    * Format upstream changes
    
    * Test height for failing grpc call
    
    * Test w additional logging
    
    * Remove accidental formal
    
    * Revert "Remove accidental formal"
    
    This reverts commit 81cb70297a9879e197eb355591d2989ed6945d0f.
    
    * Make additional req changes
    
    * Requested changes for formatting/client signature
    
    * Add changelog entry
    
    * Add separate endpoint for latest BlockResults
    
    * Fix linting
    
    * Clean
    
    * Update changelog
    
    * Update rpc/grpc/server/services/blockresultservice/service.go
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>
    
    * Update test
    
    ---------
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>

--- test/e2e/tests/grpc_test.go
+++ test/e2e/tests/grpc_test.go
@@ -2,6 +2,7 @@ package e2e_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 	"time"
 
@@ -149,6 +150,62 @@ func TestGRPC_Block_GetLatestHeight(t *testing.T) {
 		case result := <-resultCh:
 			require.NoError(t, result.Error)
 			require.True(t, result.Height == status.SyncInfo.LatestBlockHeight || result.Height == status.SyncInfo.LatestBlockHeight+1)
+		}
+	})
+}
+
+func TestGRPC_GetBlockResults(t *testing.T) {
+	testNode(t, func(t *testing.T, node e2e.Node) {
+		if node.Mode != e2e.ModeFull && node.Mode != e2e.ModeValidator {
+			return
+		}
+
+		client, err := node.Client()
+		require.NoError(t, err)
+		status, err := client.Status(ctx)
+		require.NoError(t, err)
+
+		first := status.SyncInfo.EarliestBlockHeight
+		last := status.SyncInfo.LatestBlockHeight
+		if node.RetainBlocks > 0 {
+			first++
+		}
+
+		ctx, ctxCancel := context.WithTimeout(context.Background(), time.Minute)
+		defer ctxCancel()
+		gRPCClient, err := node.GRPCClient(ctx)
+		require.NoError(t, err)
+
+		// GetLatestBlockResults
+		latestBlockResults, err := gRPCClient.GetLatestBlockResults(ctx)
+
+		require.GreaterOrEqual(t, last, latestBlockResults.Height)
+		require.NoError(t, err, "Unexpected error for GetLatestBlockResults")
+		require.NotNil(t, latestBlockResults)
+
+		successCases := []struct {
+			expectedHeight int64
+		}{
+			{first},
+			{latestBlockResults.Height},
+		}
+		errorCases := []struct {
+			requestHeight int64
+		}{
+			{first - 2},
+			{last + 100000},
+		}
+
+		for _, tc := range successCases {
+			res, err := gRPCClient.GetBlockResults(ctx, tc.expectedHeight)
+
+			require.NoError(t, err, fmt.Sprintf("Unexpected error for GetBlockResults at expected height: %d", tc.expectedHeight))
+			require.NotNil(t, res)
+			require.Equal(t, res.Height, tc.expectedHeight)
+		}
+		for _, tc := range errorCases {
+			_, err = gRPCClient.GetBlockResults(ctx, tc.requestHeight)
+			require.Error(t, err)
 		}
 	})
 }

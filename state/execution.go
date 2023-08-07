8437b23e6b943a939a7d18732ffd0e0a867809bb
Sergio Mena <sergio@informal.systems>
2023-08-07 18:35:13 +0200

    Log proposer's address when correctly accepting a proposal (#1079)
    * Log proposer when logging received proposal
    
    * Addressed review comments
    
    * Promote updates to validator to Info level

--- state/execution.go
+++ state/execution.go
@@ -224,7 +224,7 @@ func (blockExec *BlockExecutor) ApplyBlock(
 		return state, 0, err
 	}
 	if len(validatorUpdates) > 0 {
-		blockExec.logger.Debug("updates to validators", "updates", types.ValidatorListString(validatorUpdates))
+		blockExec.logger.Info("updates to validators", "updates", types.ValidatorListString(validatorUpdates))
 		blockExec.metrics.ValidatorSetUpdates.Add(1)
 	}
 	if abciResponses.EndBlock.ConsensusParamUpdates != nil {

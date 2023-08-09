c83ea3e536fc009ac3ce6f1c4cd39a2f59ff0342
hvanz <hernan.vanzetto@gmail.com>
2023-08-09 18:38:29 +0300

    Fix lints

--- mempool/mempoolTx.go
+++ mempool/mempoolTx.go
@@ -7,7 +7,7 @@ import (
 )
 
 // mempoolTx is an entry in the mempool
-type MempoolTx struct {
+type MempoolTx struct { //nolint: revive // The variable name stutters; this is only an ad-hoc hack to access this struct from the test/e2e/fast-prototyping.
 	height    int64    // height that this tx had been validated in
 	gasWanted int64    // amount of gas this tx states it will require
 	tx        types.Tx // validated by the application
--- test/e2e/app/app.go
+++ test/e2e/app/app.go
@@ -30,7 +30,7 @@ import (
 const (
 	appVersion                 = 1
 	voteExtensionKey    string = "extensionSum"
-	voteExtensionMaxLen int64  = 1024 * 1024 * 128 //TODO: should be smaller.
+	voteExtensionMaxLen int64  = 1024 * 1024 * 128 // TODO: should be smaller.
 	voteExtensionMaxVal int64  = 128
 	prefixReservedKey   string = "reservedTxKey_"
 	suffixChainID       string = "ChainID"
@@ -102,13 +102,13 @@ type Config struct {
 	// Vote extension padding size, to simulate different vote extension sizes.
 	VoteExtensionSize uint `toml:"vote_extension_size"`
 
-	//Experimental
+	// Experimental
 	// Inject custom reactors (see node/main.go#startNode for a list of possibilities)
- 	ExperimentalCustomReactors map[string]string `toml:"experimental_custom_reactors"`
+	ExperimentalCustomReactors map[string]string `toml:"experimental_custom_reactors"`
 
- 	// Experimental gossip "propagation rate" and "send once" features
- 	ExperimentalGossipPropagationRate float32 `toml:"experimental_gossip_propagation_rate"`
- 	ExperimentalGossipSendOnce        bool    `toml:"experimental_gossip_send_once"`
+	// Experimental gossip "propagation rate" and "send once" features
+	ExperimentalGossipPropagationRate float32 `toml:"experimental_gossip_propagation_rate"`
+	ExperimentalGossipSendOnce        bool    `toml:"experimental_gossip_send_once"`
 }
 
 func DefaultConfig(dir string) *Config {

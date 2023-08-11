c710bb8361dc37132e68eedafba06ae4312768af
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2023-08-11 22:18:31 +0200

    ADR-101: Metrics to monitor the pruning  (#1234)
    * Metrics to report on the data companion retain height
    
    * Added metric to report the application retain height as well
    
    * Added metrics to report the blockstore base height and the abci results base height
    ---------
    
    Co-authored-by: Thane Thomson <connect@thanethomson.com>

--- state/metrics.go
+++ state/metrics.go
@@ -26,4 +26,24 @@ type Metrics struct {
 	// updated the validator set since process start.
 	//metrics:Number of validator set updates returned by the application since process start.
 	ValidatorSetUpdates metrics.Counter
+
+	// PruningServiceBlockRetainHeight is the accepted block
+	// retain height set by the data companion
+	PruningServiceBlockRetainHeight metrics.Gauge
+
+	// PruningServiceBlockResultsRetainHeight is the accepted block results
+	// retain height set by the data companion
+	PruningServiceBlockResultsRetainHeight metrics.Gauge
+
+	// ApplicationBlockRetainHeight is the accepted block
+	// retain height set by the application
+	ApplicationBlockRetainHeight metrics.Gauge
+
+	// BlockStoreBaseHeight shows the first height at which
+	// a block is available
+	BlockStoreBaseHeight metrics.Gauge
+
+	// ABCIResultsBaseHeight shows the first height at which
+	// abci results are available
+	ABCIResultsBaseHeight metrics.Gauge
 }

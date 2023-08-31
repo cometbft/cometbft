cbecdd403a91add6d5e2c88383d00bebd4609c51
Lukasz Cwik <lcwik@apache.org>
2023-08-31 08:49:39 -0700

    [CLOB-547] `IsClobOrderTransaction` only filter out Short-Term orders/cancels. (#7)
    * generate protos for dydx
    
    * add the matches and tx protos
    
    * add dydx order id logic and constants
    
    * IsClobOrderTransaction => IsShortTermClobOrderTransaction
    
    * disable lint for dydx order id constants
    
    * more specific linter disables
    
    * rename proto package due to global proto registry name clash
    
    * Update import paths for dydx clob types
    
    * helpful text string function for order id
    
    * minor pr comments, no logic changes
    
    * README updates

--- mempool/dydx_helpers.go
+++ mempool/dydx_helpers.go
@@ -2,13 +2,15 @@ package mempool
 
 import (
 	"github.com/cometbft/cometbft/libs/log"
+	"github.com/cometbft/cometbft/proto/dydxcometbft/clob"
 	"github.com/cometbft/cometbft/types"
 	cosmostx "github.com/cosmos/cosmos-sdk/types/tx"
 )
 
-// isClobOrderTransaction returns true if the provided `tx` is a
-// Cosmos transaction containing a `MsgPlaceOrder` or `MsgCancelOrder` message.
-func IsClobOrderTransaction(
+// IsShortTermClobOrderTransaction returns true if the provided `tx` is a
+// Cosmos transaction containing a short-term `MsgPlaceOrder` or
+// short-term `MsgCancelOrder` message.
+func IsShortTermClobOrderTransaction(
 	tx types.Tx,
 	mempoolLogger log.Logger,
 ) bool {
@@ -18,12 +20,30 @@ func IsClobOrderTransaction(
 		mempoolLogger.Error("isClobOrderTransaction error. Invalid Cosmos Transaction.")
 		return false
 	}
-
-	if cosmosTx.Body != nil &&
-		len(cosmosTx.Body.Messages) == 1 &&
-		(cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgPlaceOrder" ||
-			cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgCancelOrder") {
-		return true
+	if cosmosTx.Body != nil && len(cosmosTx.Body.Messages) == 1 {
+		bytes := cosmosTx.Body.Messages[0].Value
+		if cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgPlaceOrder" {
+			msgPlaceOrder := &clob.MsgPlaceOrder{}
+			err := msgPlaceOrder.Unmarshal(bytes)
+			// In the case of an unmarshalling error, panic.
+			// Chances are, the protos are out of sync with the dydx v4 repo.
+			if err != nil {
+				panic(
+					"Failed to unmarshal MsgPlaceOrder from Cosmos transaction in CometBFT mempool.",
+				)
+			}
+			return msgPlaceOrder.Order.OrderId.IsShortTermOrder()
+		}
+		if cosmosTx.Body.Messages[0].TypeUrl == "/dydxprotocol.clob.MsgCancelOrder" {
+			msgCancelOrder := &clob.MsgCancelOrder{}
+			err := msgCancelOrder.Unmarshal(bytes)
+			// In the case of an unmarshalling error, panic.
+			// Chances are, the protos are out of sync with the dydx v4 repo.
+			if err != nil {
+				panic("Failed to unmarshal MsgCancelOrder from Cosmos transaction.")
+			}
+			return msgCancelOrder.OrderId.IsShortTermOrder()
+		}
 	}
 
 	return false

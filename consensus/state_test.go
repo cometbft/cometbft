89302724e8c65c20b6b077e1d229d1717c7bcafc
Daniel Cason <daniel.cason@informal.systems>
2023-09-07 14:24:43 +0200

    consensus: updated comments on added unit test

--- consensus/state_test.go
+++ consensus/state_test.go
@@ -51,7 +51,7 @@ x * TestStateLockPOLDoesNotUnlock 4 vals, one precommits, other 3 polka nil at
 next round, so we precommit nil but maintain lock
 x * TestStateLockMissingProposalWhenPOLSeenDoesNotUpdateLock - 4 vals, 1 misses proposal but sees POL.
 x * TestStateLockMissingProposalWhenPOLSeenDoesNotUnlock - 4 vals, 1 misses proposal but sees POL.
-x * TestStateLockMissingProposalWhenPOLForLockedBlock - 4 vals, 1 misses proposal but sees POL for locked block.
+  * TestStateLockMissingProposalWhenPOLForLockedBlock - 4 vals, 1 misses proposal but sees POL for locked block.
 x * TestStateLockPOLSafety1 - 4 vals. We shouldn't change lock based on polka at earlier round
 x * TestStateLockPOLSafety2 - 4 vals. After unlocking, we shouldn't relock based on polka at earlier round
 x * TestStatePrevotePOLFromPreviousRound 4 vals, prevote a proposal if a POL was seen for it in a previous round.
@@ -1250,7 +1250,7 @@ func TestStateLockMissingProposalWhenPOLSeenDoesNotUpdateLock(t *testing.T) {
 
 // TestStateLockMissingProposalWhenPOLForLockedBlock tests that observing
 // a two thirds majority for a block that matches the validator's locked block
-// causes a validator to upate its lock round and Precommit for the locked block.
+// causes a validator to upate its lock round and Precommit the locked block.
 func TestStateLockMissingProposalWhenPOLForLockedBlock(t *testing.T) {
 	cs1, vss := randState(4)
 	vs2, vs3, vs4 := vss[1], vss[2], vss[3]
@@ -1270,7 +1270,7 @@ func TestStateLockMissingProposalWhenPOLForLockedBlock(t *testing.T) {
 		Send a prevote for B from each of the validators to cs1.
 		Send a precommit for nil from all of the validators to cs1.
 
-		This ensures that cs1 will lock on B in this round but not precommit it.
+		This ensures that cs1 will lock on B in this round but not commit it.
 	*/
 	t.Log("### Starting Round 0")
 	startTestRound(cs1, height, round)
@@ -1278,16 +1278,16 @@ func TestStateLockMissingProposalWhenPOLForLockedBlock(t *testing.T) {
 	ensureNewRound(newRoundCh, height, round)
 	ensureNewProposal(proposalCh, height, round)
 	rs := cs1.GetRoundState()
-	firstBlockHash := rs.ProposalBlock.Hash()
-	firstBlockParts := rs.ProposalBlockParts.Header()
+	blockHash := rs.ProposalBlock.Hash()
+	blockParts := rs.ProposalBlockParts.Header()
 
 	ensurePrevote(voteCh, height, round) // prevote
 
-	signAddVotes(cs1, cmtproto.PrevoteType, firstBlockHash, firstBlockParts, false, vs2, vs3, vs4)
+	signAddVotes(cs1, cmtproto.PrevoteType, blockHash, blockParts, false, vs2, vs3, vs4)
 
 	ensurePrecommit(voteCh, height, round) // our precommit
 	// the proposed block should now be locked and our precommit added
-	validatePrecommit(t, cs1, round, round, vss[0], firstBlockHash, firstBlockHash)
+	validatePrecommit(t, cs1, round, round, vss[0], blockHash, blockHash)
 
 	// add precommits from the rest
 	signAddVotes(cs1, cmtproto.PrecommitType, nil, types.PartSetHeader{}, true, vs2, vs3, vs4)
@@ -1300,7 +1300,8 @@ func TestStateLockMissingProposalWhenPOLForLockedBlock(t *testing.T) {
 		The same block B is re-proposed, but it is not sent to cs1.
 		Send a prevote for B from each of the validators to cs1.
 
-		Check that cs1 maintain its locked block and updates the locked round to 1.
+		Check that cs1 precommits B, since B matches its locked value.
+		Check that cs1 maintains its lock on block B but updates its locked round.
 	*/
 	t.Log("### Starting Round 1")
 	incrementRound(vs2, vs3, vs4)
@@ -1313,13 +1314,15 @@ func TestStateLockMissingProposalWhenPOLForLockedBlock(t *testing.T) {
 	validatePrevote(t, cs1, round, vss[0], nil)
 
 	// now lets add prevotes from everyone else for the locked block
-	signAddVotes(cs1, cmtproto.PrevoteType, firstBlockHash, firstBlockParts, false, vs2, vs3, vs4)
+	signAddVotes(cs1, cmtproto.PrevoteType, blockHash, blockParts, false, vs2, vs3, vs4)
 
 	ensurePrecommit(voteCh, height, round)
-	// the validator precommits the block and updates its locked round
-	validatePrecommit(t, cs1, round, 1, vss[0], firstBlockHash, firstBlockHash)
 
-	// NOTE: the last behavior is inconsistent with Tendermint consensus pseudo-code.
+	// the validator precommits the block, because it matches its locked block,
+	// maintains the same locked block and updates its locked round
+	validatePrecommit(t, cs1, round, 1, vss[0], blockHash, blockHash)
+
+	// NOTE: this behavior is inconsistent with Tendermint consensus pseudo-code.
 	// In the pseudo-code, if a process does not receive the proposal (and block) for
 	// the current round, it cannot Precommit the proposed block ID, even thought it
 	// sees a POL for that block that matches the locked value (block).

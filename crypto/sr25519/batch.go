32a607f6be0946358dded517050dde7756bf1cf4
Tshaka Lekholoane <mail+git@tshaka.dev>
2023-10-10 13:49:44 +0200

    refactor: export crypto/sr25519 errors

--- crypto/sr25519/batch.go
+++ crypto/sr25519/batch.go
@@ -1,6 +1,7 @@
 package sr25519
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/oasisprotocol/curve25519-voi/primitives/sr25519"
@@ -9,6 +10,28 @@ import (
 )
 
 var _ crypto.BatchVerifier = &BatchVerifier{}
+
+// InvalidKeyError represents an error that could occur as a result of
+// using an invalid private or public key. It wraps errors that could
+// arise due to failures in serialization or the use of an incorrect
+// key, i.e., uninitialised or not sr25519.
+type InvalidKeyError struct{ Err error }
+
+func (e *InvalidKeyError) Error() string {
+	return fmt.Sprintf("sr25519: invalid public key: %v", e.Err)
+}
+
+func (e *InvalidKeyError) Unwrap() error { return e.Err }
+
+// InvalidSignatureError wraps an error that could occur as a result of
+// generating an invalid signature.
+type InvalidSignatureError struct{ Err error }
+
+func (e *InvalidSignatureError) Error() string {
+	return fmt.Sprintf("sr25519: invalid signature: %v", e.Err)
+}
+
+func (e *InvalidSignatureError) Unwrap() error { return e.Err }
 
 // BatchVerifier implements batch verification for sr25519.
 type BatchVerifier struct {
@@ -22,17 +45,17 @@ func NewBatchVerifier() crypto.BatchVerifier {
 func (b *BatchVerifier) Add(key crypto.PubKey, msg, signature []byte) error {
 	pk, ok := key.(PubKey)
 	if !ok {
-		return fmt.Errorf("sr25519: pubkey is not sr25519")
+		return &InvalidKeyError{Err: errors.New("sr25519: pubkey is not sr25519")}
 	}
 
 	var srpk sr25519.PublicKey
 	if err := srpk.UnmarshalBinary(pk); err != nil {
-		return fmt.Errorf("sr25519: invalid public key: %w", err)
+		return &InvalidKeyError{Err: err}
 	}
 
 	var sig sr25519.Signature
 	if err := sig.UnmarshalBinary(signature); err != nil {
-		return fmt.Errorf("sr25519: unable to decode signature: %w", err)
+		return &InvalidSignatureError{Err: err}
 	}
 
 	st := signingCtx.NewTranscriptBytes(msg)

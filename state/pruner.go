ad5bff29fdc89d73957aac7d9d4e8af8769c300a
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2023-10-13 09:37:11 +0200

    Call observer only when retain height changes (#1490)

--- state/pruner.go
+++ state/pruner.go
@@ -237,10 +237,12 @@ func (p *Pruner) pruneABCIResponses() {
 			return
 		default:
 			newRetainHeight := p.pruneABCIResToRetainHeight(lastRetainHeight)
-			p.observer.PrunerPrunedABCIRes(&ABCIResponsesPrunedInfo{
-				FromHeight: lastRetainHeight,
-				ToHeight:   newRetainHeight - 1,
-			})
+			if newRetainHeight != lastRetainHeight {
+				p.observer.PrunerPrunedABCIRes(&ABCIResponsesPrunedInfo{
+					FromHeight: lastRetainHeight,
+					ToHeight:   newRetainHeight - 1,
+				})
+			}
 			lastRetainHeight = newRetainHeight
 			time.Sleep(p.interval)
 		}
@@ -256,10 +258,12 @@ func (p *Pruner) pruneBlocks() {
 			return
 		default:
 			newRetainHeight := p.pruneBlocksToRetainHeight(lastRetainHeight)
-			p.observer.PrunerPrunedBlocks(&BlocksPrunedInfo{
-				FromHeight: lastRetainHeight,
-				ToHeight:   newRetainHeight - 1,
-			})
+			if newRetainHeight != lastRetainHeight {
+				p.observer.PrunerPrunedBlocks(&BlocksPrunedInfo{
+					FromHeight: lastRetainHeight,
+					ToHeight:   newRetainHeight - 1,
+				})
+			}
 			lastRetainHeight = newRetainHeight
 			time.Sleep(p.interval)
 		}

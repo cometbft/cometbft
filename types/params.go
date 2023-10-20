4c373ebbfbe099dcedae5024255d119cdfa0235f
Jacob Gadikian <jacobgadikian@gmail.com>
2023-10-20 09:08:19 +0000

    reduce the default maximum MaxBytes

--- types/params.go
+++ types/params.go
@@ -96,7 +96,7 @@ func DefaultConsensusParams() *ConsensusParams {
 // DefaultBlockParams returns a default BlockParams.
 func DefaultBlockParams() BlockParams {
 	return BlockParams{
-		MaxBytes: 22020096, // 21MB
+		MaxBytes: 5242880, // 5MB
 		MaxGas:   -1,
 	}
 }

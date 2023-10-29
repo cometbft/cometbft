5d94d12088d3a038635f4c7aaff3d49143035f91
Daniel <daniel.cason@informal.systems>
2023-10-29 17:19:38 -0400

    e2e: improved latency-setter script logging

--- test/e2e/scripts/latency-setter.py
+++ test/e2e/scripts/latency-setter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # Modified version of latsetter.py script from 
 # https://github.com/paulo-coelho/latency-setter
@@ -9,8 +9,8 @@ import sys
 import netifaces as nif
 
 def usage():
-    print 'Usage: \t', sys.argv[0], 'set ip-list latency-list iface-name'
-    print '\tOR:\t', sys.argv[0], 'unset iface-name'
+    print('Usage: \t', sys.argv[0], 'set ip-list latency-list iface-name')
+    print('\tOR:\t', sys.argv[0], 'unset iface-name')
     exit()
 
 if len(sys.argv) < 3:
@@ -25,7 +25,7 @@ if sys.argv[1] != 'set':
     usage()
 
 #reads IP/Zone file
-ips = open(sys.argv[2], 'rb')
+ips = open(sys.argv[2], 'r')
 ipD = csv.DictReader(ips)
 ipData = []
 
@@ -37,7 +37,7 @@ iface = sys.argv[4]
 myip = nif.ifaddresses(iface)[nif.AF_INET][0]['addr']
 
 #reads latency file
-with open(sys.argv[3], 'rb') as f:
+with open(sys.argv[3], 'r') as f:
     reader = csv.reader(f)
     lats = list(list(rec) for rec in csv.reader(f, delimiter=',')) #reads csv into a list of lists
 
@@ -47,7 +47,7 @@ for item in ipData:
         myzone = item['Zone']
 
 if myzone == '':
-    print 'IP', myip, 'not in IP file'
+    print('IP', myip, 'not in IP file')
     exit()
 
 azs = lats[0]
@@ -59,15 +59,15 @@ for l in lats[1:]:
         if value != 0:
             tlats[key] = value
 
-print '# Setting rules for interface', iface, 'in zone', myzone, 'with IP', myip
+print('# Setting rules for interface', iface, 'in zone', myzone, 'with IP', myip)
 
 command = 'tc qdisc del dev ' + iface + ' root'
 os.system(command)
 command = 'tc qdisc add dev ' + iface + ' root handle 1: htb default 10'
 os.system(command)
-command = 'tc class add dev ' + iface + ' parent 1: classid 1:1 htb rate 1gbit'
+command = 'tc class add dev ' + iface + ' parent 1: classid 1:1 htb rate 1gbit 2> /dev/null'
 os.system(command)
-command = 'tc class add dev ' + iface + ' parent 1:1 classid 1:10 htb rate 1gbit'
+command = 'tc class add dev ' + iface + ' parent 1:1 classid 1:10 htb rate 1gbit 2> /dev/null'
 os.system(command)
 command = 'tc qdisc add dev ' + iface + ' parent 1:10 handle 10: sfq perturb 10'
 os.system(command)
@@ -75,14 +75,12 @@ os.system(command)
 nextHandle = 11
 for az in azs[1:]:
     lat = tlats.get((myzone, az))
+    if lat == None:
+        continue
     if lat > 0:#az != myzone:
         lat = tlats.get((myzone, az))
-        print '# Setting latency to', lat, 'ms for zone', az
-	if lat == None:
-            continue
         delta = .05 * lat
-        print '# Setting latency to', lat, 'ms for zone', az
-        command = 'tc class add dev ' + iface + ' parent 1:1 classid 1:' + str(nextHandle) + ' htb rate 1gbit'
+        command = 'tc class add dev ' + iface + ' parent 1:1 classid 1:' + str(nextHandle) + ' htb rate 1gbit 2> /dev/null'
         os.system(command)
         command = 'tc qdisc add dev ' + iface + ' parent 1:' + str(nextHandle) + ' handle ' + str(
             nextHandle) + ': netem delay ' + str(lat) + 'ms ' + str(delta) + 'ms distribution normal'
@@ -90,7 +88,7 @@ for az in azs[1:]:
         for item in ipData:
             if item['Zone'] == az:
                 ip = item['IP']
-                print '\t# Latency from', myip, 'to', ip, 'set to', lat, '+/-', delta
+                print(f'# Configuring latency from {myip} ({myzone}) to {ip} ({az}) set to {lat}ms +/- {delta}ms')
                 command = 'tc filter add dev ' + iface + ' protocol ip parent 1: prio 1 u32 match ip dst ' + ip + '/32 flowid 1:' + str(
                     nextHandle)
                 os.system(command)

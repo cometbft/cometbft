c56f348c46e0dc9b2c0834e52076ac205ac55ee3
Hernán Vanzetto <15466498+hvanz@users.noreply.github.com>
2023-11-03 23:42:10 +0100

    mempool: Add metric size of pool in bytes (#1512)
    * mempool: Add metric `SizeBytes`
    
    * Safe concurrent read of txsBytes
    
    * Add changelog
    
    (cherry picked from commit b50bca37ca0335a89ea66bde51d7e0c375602929)
    
    # Conflicts:
    #	mempool/metrics.gen.go

--- mempool/metrics.go
+++ mempool/metrics.go
@@ -18,6 +18,9 @@ type Metrics struct {
 	// Number of uncommitted transactions in the mempool.
 	Size metrics.Gauge
 
+	// Total size of the mempool in bytes.
+	SizeBytes metrics.Gauge
+
 	// Histogram of transaction sizes in bytes.
 	TxSizeBytes metrics.Histogram `metrics_buckettype:"exp" metrics_bucketsizes:"1,3,7"`
 

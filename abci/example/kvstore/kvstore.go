afbf6a883dbda9883b3f4e4313a748c037356363
lasaro <lasaro@informal.systems>
2023-12-06 14:52:41 -0300

    Add test missing in #1687 (#1712)
    * Experimenting the fix from lx-xiang
    
    * Fixes name in the example
    
    * Reverts fix, so it is merged from the proper branch.
    
    * Adds a test that fails because the validate is wrong and accepts a block that is larger than it should.
    
    * Add changelog for the original PR
    
    * Update internal/state/execution_test.go
    
    (cherry picked from commit ce0215c435a356e2b399827c934a448fa47ee476)

--- abci/example/kvstore/kvstore.go
+++ abci/example/kvstore/kvstore.go
@@ -156,10 +156,10 @@ func isValidTx(tx []byte) bool {
 	return false
 }
 
-// PrepareProposal is called when the node is a proposer. Tendermint stages a set of transactions to the application. As the
+// PrepareProposal is called when the node is a proposer. CometBFT stages a set of transactions to the application. As the
 // KVStore has two accepted formats, `:` and `=`, we modify all instances of `:` with `=` to make it consistent. Note: this is
 // quite a trivial example of transaction modification.
-// NOTE: we assume that Tendermint will never provide more transactions than can fit in a block.
+// NOTE: we assume that CometBFT will never provide more transactions than can fit in a block.
 func (app *Application) PrepareProposal(ctx context.Context, req *types.PrepareProposalRequest) (*types.PrepareProposalResponse, error) {
 	return &types.PrepareProposalResponse{Txs: app.formatTxs(ctx, req.Txs)}, nil
 }

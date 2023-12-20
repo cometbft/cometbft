d25488cf781d1ec7470f47d2bc50d5ff1690c54c
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2023-12-20 11:45:58 +0100

    Added pruning for state store

--- internal/state/store.go
+++ internal/state/store.go
@@ -421,6 +421,10 @@ func (store dbStore) PruneStates(from int64, to int64, evidenceThresholdHeight i
 		return err
 	}
 
+	compact_err := store.db.Compact(nil, nil)
+	if compact_err != nil {
+		fmt.Println("COMPACTION ERROR")
+	}
 	return nil
 }
 

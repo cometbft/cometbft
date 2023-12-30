da358a0f94bcc450995213742c04ff2577ce7486
Jacob Gadikian <jacobgadikian@gmail.com>
2023-12-31 02:29:51 +0700

    Fix assertion in TestClientReplacesPrimaryWithWitnessIfPrimaryIsUnavailable

--- light/client_test.go
+++ light/client_test.go
@@ -774,7 +774,7 @@ func TestClientReplacesPrimaryWithWitnessIfPrimaryIsUnavailable(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.NotEqual(t, c.Primary(), deadNode)
-	assert.Len(t, 2, len(c.Witnesses()))
+	assert.Equal(t, 2, len(c.Witnesses()))
 }
 
 func TestClient_BackwardsVerification(t *testing.T) {

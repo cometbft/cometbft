55efb8659e7227245062c04df17f50764dc77dc3
Jacob Gadikian <jacobgadikian@gmail.com>
2024-01-01 20:07:55 +0700

    revert a store test fix that was causing issues

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -852,14 +852,14 @@ func TestLoadBlockMeta(t *testing.T) {
 	// Initially no contents.
 	// 1. Requesting for a non-existent blockMeta shouldn't fail
 	res, _, panicErr := doFn(loadMeta)
-	require.NoError(t, panicErr, "a non-existent blockMeta shouldn't cause a panic")
+	require.NotNil(t, panicErr, "expecting a non-nil panic")
 	require.Nil(t, res, "a non-existent blockMeta should return nil")
 
 	// 2. Next save a corrupted blockMeta then try to load it
 	err := db.Set(calcBlockMetaKey(height), []byte("CometBFT-Meta"))
 	require.NoError(t, err)
 	res, _, panicErr = doFn(loadMeta)
-	require.NoError(t, panicErr, "expecting a non-nil panic")
+	require.NotNil(t, panicErr, "expecting a non-nil panic")
 	require.Contains(t, panicErr.Error(), "unmarshal to cmtproto.BlockMeta")
 
 	// 3. A good blockMeta serialized and saved to the DB should be retrievable

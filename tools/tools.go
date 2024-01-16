e3d84c98ebcb986074dafc597d99a378fc1749f7
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-01-16 10:23:42 +0800

    misc(go.mod): remove unused peg dependency
    Refs #523

--- tools/tools.go
+++ tools/tools.go
@@ -9,6 +9,5 @@ package tools
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/pointlander/peg"
 	_ "github.com/vektra/mockery/v2"
 )

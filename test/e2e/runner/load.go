b67a9f655e5e2eae309c46b72e39b33fdd9c6c86
hvanz <hernan.vanzetto@gmail.com>
2024-02-13 12:45:06 +0100

    e2e: log number of sent txs (success and failed)
    +

--- test/e2e/runner/load.go
+++ test/e2e/runner/load.go
@@ -24,6 +24,7 @@ func Load(ctx context.Context, testnet *e2e.Testnet) error {
 	initialTimeout := 1 * time.Minute
 	stallTimeout := 30 * time.Second
 	chSuccess := make(chan struct{})
+	chFailed := make(chan struct{})
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -40,31 +41,39 @@ func Load(ctx context.Context, testnet *e2e.Testnet) error {
 		}
 
 		for w := 0; w < testnet.LoadTxConnections; w++ {
-			go loadProcess(ctx, txCh, chSuccess, n)
+			go loadProcess(ctx, txCh, chSuccess, chFailed, n)
 		}
 	}
 
-	// Monitor successful transactions, and abort on stalls.
-	success := 0
+	// Monitor successful and failed transactions, and abort on stalls.
+	success, failed := 0, 0
 	timeout := initialTimeout
 	for {
+		rate := log.NewLazySprintf("%.1f", float64(success)/time.Since(started).Seconds())
+
 		select {
 		case <-chSuccess:
 			success++
-			if testnet.LoadMaxTxs > 0 && success >= testnet.LoadMaxTxs {
-				logger.Info("load", "msg", log.NewLazySprintf("Ending transaction load after reaching %v txs (%.1f tx/s)...",
-					success, float64(success)/time.Since(started).Seconds()))
-				return nil
-			}
 			timeout = stallTimeout
+		case <-chFailed:
+			failed++
 		case <-time.After(timeout):
 			return fmt.Errorf("unable to submit transactions for %v", timeout)
 		case <-ctx.Done():
 			if success == 0 {
 				return errors.New("failed to submit any transactions")
 			}
-			logger.Info("load", "msg", log.NewLazySprintf("Ending transaction load after %v txs (%.1f tx/s)...",
-				success, float64(success)/time.Since(started).Seconds()))
+			logger.Info("load", "msg", log.NewLazySprintf("Ending transaction load after %v txs (%.1f tx/s)...", success, rate))
+			return nil
+		}
+
+		// Log every ~1 second the number of sent transactions.
+		if (success+failed)%testnet.LoadTxBatchSize == 0 {
+			logger.Debug("load", "success", success, "failed", failed, "tx/s", rate)
+		}
+
+		if testnet.LoadMaxTxs > 0 && success >= testnet.LoadMaxTxs {
+			logger.Info("load", "msg", log.NewLazySprintf("Ending transaction load after reaching %v txs (%.1f tx/s)...", success, rate))
 			return nil
 		}
 	}
@@ -135,7 +144,7 @@ func createTxBatch(ctx context.Context, txCh chan<- types.Tx, testnet *e2e.Testn
 
 // loadProcess processes transactions by sending transactions received on the txCh
 // to the client.
-func loadProcess(ctx context.Context, txCh <-chan types.Tx, chSuccess chan<- struct{}, n *e2e.Node) {
+func loadProcess(ctx context.Context, txCh <-chan types.Tx, chSuccess chan<- struct{}, chFailed chan<- struct{}, n *e2e.Node) {
 	var client *rpchttp.HTTP
 	var err error
 	s := struct{}{}
@@ -148,6 +157,8 @@ func loadProcess(ctx context.Context, txCh <-chan types.Tx, chSuccess chan<- str
 			}
 		}
 		if _, err = client.BroadcastTxSync(ctx, tx); err != nil {
+			logger.Error("failed to send transaction", "err", err)
+			chFailed <- s
 			continue
 		}
 		chSuccess <- s

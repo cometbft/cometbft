8339f438ba48af7d095d22c60449b6c3ca986cf3
hvanz <hernan.vanzetto@gmail.com>
2024-02-14 09:10:16 +0100

    add success/total to message

--- test/e2e/runner/load.go
+++ test/e2e/runner/load.go
@@ -68,8 +68,9 @@ func Load(ctx context.Context, testnet *e2e.Testnet) error {
 		}
 
 		// Log every ~1 second the number of sent transactions.
-		if (success+failed)%testnet.LoadTxBatchSize == 0 {
-			logger.Debug("load", "success", success, "failed", failed, "tx/s", rate)
+		total := success + failed
+		if total%testnet.LoadTxBatchSize == 0 {
+			logger.Debug("load", "success", success, "failed", failed, "success/total", log.NewLazySprintf("%.1f", success/total), "tx/s", rate)
 		}
 
 		// Check if reached max number of allowed transactions to send.

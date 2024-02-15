c6da4b7b9a339b325e9a5641f2441beebe86c46f
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-02-15 17:33:03 +0200

    fix conflicts

--- test/e2e/pkg/testnet.go
+++ test/e2e/pkg/testnet.go
@@ -124,7 +124,6 @@ func LoadTestnet(manifest Manifest, fname string, ifd InfrastructureData) (*Test
 	}
 
 	testnet := &Testnet{
-<<<<<<< HEAD
 		Name:              filepath.Base(dir),
 		File:              fname,
 		Dir:               dir,
@@ -137,38 +136,10 @@ func LoadTestnet(manifest Manifest, fname string, ifd InfrastructureData) (*Test
 		LoadTxSizeBytes:   manifest.LoadTxSizeBytes,
 		LoadTxBatchSize:   manifest.LoadTxBatchSize,
 		LoadTxConnections: manifest.LoadTxConnections,
+		LoadMaxTxs:        manifest.LoadMaxTxs,
 		ABCIProtocol:      manifest.ABCIProtocol,
 		UpgradeVersion:    manifest.UpgradeVersion,
 		Prometheus:        manifest.Prometheus,
-=======
-		Name:                             filepath.Base(dir),
-		File:                             file,
-		Dir:                              dir,
-		IP:                               ipNet,
-		InitialHeight:                    1,
-		InitialState:                     manifest.InitialState,
-		Validators:                       map[*Node]int64{},
-		ValidatorUpdates:                 map[int64]map[*Node]int64{},
-		Nodes:                            []*Node{},
-		DisablePexReactor:                manifest.DisablePexReactor,
-		Evidence:                         manifest.Evidence,
-		LoadTxSizeBytes:                  manifest.LoadTxSizeBytes,
-		LoadTxBatchSize:                  manifest.LoadTxBatchSize,
-		LoadTxConnections:                manifest.LoadTxConnections,
-		LoadMaxTxs:                       manifest.LoadMaxTxs,
-		ABCIProtocol:                     manifest.ABCIProtocol,
-		PrepareProposalDelay:             manifest.PrepareProposalDelay,
-		ProcessProposalDelay:             manifest.ProcessProposalDelay,
-		CheckTxDelay:                     manifest.CheckTxDelay,
-		VoteExtensionDelay:               manifest.VoteExtensionDelay,
-		FinalizeBlockDelay:               manifest.FinalizeBlockDelay,
-		UpgradeVersion:                   manifest.UpgradeVersion,
-		Prometheus:                       manifest.Prometheus,
-		VoteExtensionsEnableHeight:       manifest.VoteExtensionsEnableHeight,
-		VoteExtensionsUpdateHeight:       manifest.VoteExtensionsUpdateHeight,
-		VoteExtensionSize:                manifest.VoteExtensionSize,
-		PeerGossipIntraloopSleepDuration: manifest.PeerGossipIntraloopSleepDuration,
->>>>>>> 6722f3375 (e2e: Add `load_max_txs` option to manifest (#2094))
 		ExperimentalMaxGossipConnectionsToPersistentPeers:    manifest.ExperimentalMaxGossipConnectionsToPersistentPeers,
 		ExperimentalMaxGossipConnectionsToNonPersistentPeers: manifest.ExperimentalMaxGossipConnectionsToNonPersistentPeers,
 	}

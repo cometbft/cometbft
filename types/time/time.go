d228d300f2dc247deaaea56a2a44f0a206472aa9
Sergio Mena <sergio@informal.systems>
2024-02-27 12:36:13 +0100

    Add clock skew functionality hidden by `clock_skew` build flag

--- types/time/time.go
+++ types/time/time.go
@@ -5,11 +5,6 @@ import (
 	"time"
 )
 
-// Now returns the current time in UTC with no monotonic component.
-func Now() time.Time {
-	return Canonical(time.Now())
-}
-
 // Canonical returns UTC time with no monotonic component.
 // Stripping the monotonic component is for time equality.
 // See https://github.com/tendermint/tendermint/pull/2203#discussion_r215064334

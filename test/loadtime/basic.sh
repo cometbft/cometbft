e924ad4c28973b47580689c2d8b7789e51e37b48
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-03-12 12:44:24 +0100

    Reverted changes to loadrunner script

--- test/loadtime/basic.sh
+++ test/loadtime/basic.sh
@@ -4,54 +4,7 @@ set -euo pipefail
 
 # A basic invocation of the loadtime tool.
 
-#./build/load \
-#    -c 1 -T 600 -r 400 -s 1024 \
-#    --broadcast-tx-method sync \
-#    --endpoints ws://localhost:5701/v1/websocket
-
-#sleep 10
-
-#./build/load \
-#    -c 1 -T 600 -r 1000 -s 1024 \
-#    --broadcast-tx-method sync \
-#    --endpoints ws://localhost:5704/v1/websocket
-
-#sleep 10
-
-#echo "Sending to 5703"
-#./build/load \
-#    -c 1 -T 3600 -r 1000 -s 1024 \
-#    --broadcast-tx-method sync \
-#    --endpoints ws://localhost:5707/v1/websocket
-
-
-sleep 10 
-
-echo "Sending to 5704"
 ./build/load \
-    -c 1 -T 10 -r 500 -s 1024 \
+    -c 1 -T 100 -r 1000 -s 1024 \
     --broadcast-tx-method sync \
     --endpoints ws://localhost:26657/v1/websocket
-
-echo "Sending to 5705"
-sleep 10
-
-./build/load \
-    -c 1 -T 600 -r 400 -s 1024 \
-    --broadcast-tx-method sync \
-    --endpoints ws://localhost:5713/v1/websocket
-
-sleep 10
-echo "Sending to 5706"
-
-./build/load \
-    -c 1 -T 600 -r 400 -s 1024 \
-    --broadcast-tx-method sync \
-    --endpoints ws://localhost:5716/v1/websocket
-
-echo "Sending to 5706"
-
-./build/load \
-    -c 1 -T 600 -r 400 -s 1024 \
-    --broadcast-tx-method sync \
-    --endpoints ws://localhost:5719/v1/websocket

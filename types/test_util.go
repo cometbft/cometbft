2375e965b40b843a60441c29fd9d713f157ed308
Senna46 <29295263+Senna46@users.noreply.github.com>
2024-04-15 17:51:17 +0900

    fix: err handle

--- types/test_util.go
+++ types/test_util.go
@@ -112,7 +112,7 @@ func MakeVoteNoError(
 // It populates the same set of fields validated by ValidateBasic.
 func MakeBlock(height int64, txs []Tx, lastCommit *Commit, evidence []Evidence) *Block {
 	// <sunrise-core>
-	fmt.Println("MakeBlock", txs)
+	fmt.Println("MakeBlock txs", txs)
 	txBytes := Txs(txs).ToSliceOfBytes()
 	txsWithoutInfoBytes, dataHash, squareSize, _ := ExtractInfoFromTxs(txBytes)
 	txsWithoutInfo := ToTxs(txsWithoutInfoBytes)
@@ -141,7 +141,9 @@ func MakeBlock(height int64, txs []Tx, lastCommit *Commit, evidence []Evidence) 
 func ExtractInfoFromTxs(txsWithInfo [][]byte) (txs [][]byte, dataHash []byte, squareSize uint64, err error) {
 	length := len(txsWithInfo)
 	if length < 2 {
-		err = fmt.Errorf("txs must contain the data hash and the square size at the end, and its length must not be lower than 2, got %d", length)
+		txs = txsWithInfo
+		dataHash = nil
+		squareSize = 0
 		return
 	}
 

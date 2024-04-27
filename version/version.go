1b1216cbbd945a8f1d679904b2fa7ebf3ff0fb1f
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-04-27 15:06:50 +0800

    v0.38.7 (#2908)
    [CHANGELOG](https://github.com/cometbft/cometbft/blob/2cece8aba32a6ec8fea6a66aa540f290a561d869/CHANGELOG.md#v0387)

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 const (
 	// TMVersionDefault is the used as the fallback version of CometBFT
 	// when not using git describe. It is formatted with semantic versioning.
-	TMCoreSemVer = "0.38.6"
+	TMCoreSemVer = "0.38.7"
 	// ABCISemVer is the semantic version of the ABCI protocol
 	ABCISemVer  = "2.0.0"
 	ABCIVersion = ABCISemVer

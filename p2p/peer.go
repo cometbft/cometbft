cometbft/cometbft

commit 7673d74715d87854b7bdd8c64b33e963ac415429

feat(p2p): pre-allocate static labels used to produce metrics (#2851)
Addresses #2840.

The same approach taken for the `send()` method is also adopted for the
`onReceive()` method.

---

#### PR checklist

- [ ] Tests written/updated
- [ ] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)
- [ ] Updated relevant documentation (`docs/` or `spec/`) and code
comments
- [x] Title follows the [Conventional
Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec

p2p/peer.go
@@ -282,12 +282,12 @@ func (p *peer) send(chID byte, msg proto.Message, sendFunc func(byte, []byte) bo
 	}
 	res := sendFunc(chID, msgBytes)
 	if res {
-		labels := []string{
-			"peer_id", string(p.ID()),
-			"chID", fmt.Sprintf("%#x", chID),
-		}
-		p.metrics.PeerSendBytesTotal.With(labels...).Add(float64(len(msgBytes)))
-		p.metrics.MessageSendBytesTotal.With("message_type", metricLabelValue).Add(float64(len(msgBytes)))
+		p.metrics.PeerSendBytesTotal.
+			With("peer_id", string(p.ID()), "chID", p.mlc.ChIDToMetricLabel(chID)).
+			Add(float64(len(msgBytes)))
+		p.metrics.MessageSendBytesTotal.
+			With("message_type", metricLabelValue).
+			Add(float64(len(msgBytes)))
 	}
 	return res
 }
@@ -414,18 +414,19 @@ func createMConnection(
 		if err != nil {
 			panic(fmt.Sprintf("unmarshaling message: %v into type: %s", err, reflect.TypeOf(mt)))
 		}
-		labels := []string{
-			"peer_id", string(p.ID()),
-			"chID", fmt.Sprintf("%#x", chID),
-		}
+		metricLabelValue := p.mlc.ValueToMetricLabel(msg)
 		if w, ok := msg.(types.Unwrapper); ok {
 			msg, err = w.Unwrap()
 			if err != nil {
 				panic(fmt.Sprintf("unwrapping message: %v", err))
 			}
 		}
-		p.metrics.PeerReceiveBytesTotal.With(labels...).Add(float64(len(msgBytes)))
-		p.metrics.MessageReceiveBytesTotal.With("message_type", p.mlc.ValueToMetricLabel(msg)).Add(float64(len(msgBytes)))
+		p.metrics.PeerReceiveBytesTotal.
+			With("peer_id", string(p.ID()), "chID", p.mlc.ChIDToMetricLabel(chID)).
+			Add(float64(len(msgBytes)))
+		p.metrics.MessageReceiveBytesTotal.
+			With("message_type", metricLabelValue).
+			Add(float64(len(msgBytes)))
 		reactor.Receive(Envelope{
 			ChannelID: chID,
 			Src:       p,

p2p/metrics.go
@@ -43,6 +43,19 @@ type Metrics struct {
 type metricsLabelCache struct {
 	mtx               *sync.RWMutex
 	messageLabelNames map[reflect.Type]string
+	chIDLabelNames    map[byte]string
+}
+
+// RegisterChID pre-allocates the metric label for a chID.
+// Labels are populated by the switch, before the p2p layer is started.
+func (m *metricsLabelCache) RegisterChID(chID byte) {
+	m.chIDLabelNames[chID] = fmt.Sprintf("%#x", chID)
+}
+
+// ChIDToMetricLabel returns the metric label for a chID.
+// No need for synchronization, as labels, once populated, never change.
+func (m *metricsLabelCache) ChIDToMetricLabel(chID byte) string {
+	return m.chIDLabelNames[chID]
 }
 
 // ValueToMetricLabel is a method that is used to produce a prometheus label value of the golang
@@ -72,5 +85,6 @@ func newMetricsLabelCache() *metricsLabelCache {
 	return &metricsLabelCache{
 		mtx:               &sync.RWMutex{},
 		messageLabelNames: map[reflect.Type]string{},
+		chIDLabelNames:    map[byte]string{},
 	}
 }

p2p/switch.go
@@ -173,6 +173,7 @@ func (sw *Switch) AddReactor(name string, reactor Reactor) Reactor {
 		sw.chDescs = append(sw.chDescs, chDesc)
 		sw.reactorsByCh[chID] = reactor
 		sw.msgTypeByChID[chID] = chDesc.MessageType
+		sw.mlc.RegisterChID(chID)
 	}
 	sw.reactors[name] = reactor
 	reactor.SetSwitch(sw)

commit 33729e62743b54c361dc9e53925f588586f4120a

fix(metric): message type in metric `MessageReceiveBytesTotal` is wrong (#2984)
In the metric `MessageReceiveBytesTotal`, the only value produced in the
`message_type` label is `v1_Message`. This is because we are reading the
`msg` variable before the message is unwrapped. This bug was recently
introduced in #2851.

---

#### PR checklist

- [ ] Tests written/updated
- [ ] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)
- [ ] Updated relevant documentation (`docs/` or `spec/`) and code
comments
- [ ] Title follows the [Conventional
Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec

p2p/peer.go
@@ -414,7 +414,6 @@ func createMConnection(
 		if err != nil {
 			panic(fmt.Sprintf("unmarshaling message: %v into type: %s", err, reflect.TypeOf(mt)))
 		}
-		metricLabelValue := p.mlc.ValueToMetricLabel(msg)
 		if w, ok := msg.(types.Unwrapper); ok {
 			msg, err = w.Unwrap()
 			if err != nil {
@@ -425,7 +424,7 @@ func createMConnection(
 			With("peer_id", string(p.ID()), "chID", p.mlc.ChIDToMetricLabel(chID)).
 			Add(float64(len(msgBytes)))
 		p.metrics.MessageReceiveBytesTotal.
-			With("message_type", metricLabelValue).
+			With("message_type", p.mlc.ValueToMetricLabel(msg)).
 			Add(float64(len(msgBytes)))
 		reactor.Receive(Envelope{
 			ChannelID: chID,
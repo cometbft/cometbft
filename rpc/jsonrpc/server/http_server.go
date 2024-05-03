6f2fc747f7753f39d2e084abbfede9d0d425bcd3
Andy Nogueira <me@andynogueira.dev>
2024-05-03 10:46:22 -0400

    remove comment about default value (#2867)

--- rpc/jsonrpc/server/http_server.go
+++ rpc/jsonrpc/server/http_server.go
@@ -292,7 +292,8 @@ func PreChecksHandler(next http.Handler, config *Config) http.Handler {
 		r.Body = http.MaxBytesReader(w, r.Body, config.MaxBodyBytes)
 
 		// if maxBatchSize is 0 then don't constraint the limit of requests per batch
-		// the default value is 0, and it cannot be negative because of the config validation
+		// It cannot be negative because the config.toml validation requires it to be
+		// greater than or equal to 0
 		if config.MaxRequestBatchSize > 0 {
 			var requests []types.RPCRequest
 			var responses []types.RPCResponse

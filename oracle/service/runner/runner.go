361b4d4efa1498d255283514f7574eb47b1c7c3f
yan-soon <yan@Yans-MacBook-Pro.local>
2024-05-07 19:15:16 +0800

    Revert "remove validator field from gossipedVotes"
    This reverts commit 197c06a41babc440742485084abf0f233d753683.

--- oracle/service/runner/runner.go
+++ oracle/service/runner/runner.go
@@ -71,6 +71,7 @@ func ProcessSignVoteQueue(oracleInfo *types.OracleInfo, consensusState *cs.State
 
 	// batch sign the entire unsignedVoteBuffer and add to gossipBuffer
 	newGossipVote := &oracleproto.GossipedVotes{
+		Validator:       oracleInfo.PubKey.Address(),
 		ValidatorIndex:  validatorIndex,
 		SignedTimestamp: time.Now().Unix(),
 		Votes:           unsignedVotes,

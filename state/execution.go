c68d8716ef439ecd6287ce93185c06d24b2b6701
Sergio Mena <sergio@informal.systems>
2024-05-14 11:18:29 +0200

    perf(consensus): add simplistic block validation cache (#3070)
    Closes #2854
    
    Follow up from #2960
    
    This PR adds a simplistic 1-element block validation cache in
    `BlockExecutor`. This addresses a performance bottleneck raised as part
    of #2854
    
    ---
    
    - [ ] Tests written/updated
    - [ ] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    - [ ] Title follows the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec
    
    ---------
    
    Co-authored-by: Andy Nogueira <me@andynogueira.dev>

--- state/execution.go
+++ state/execution.go
@@ -40,6 +40,9 @@ type BlockExecutor struct {
 	mempool mempool.Mempool
 	evpool  EvidencePool
 
+	// 1-element cache of validated blocks
+	lastValidatedBlock *types.Block
+
 	logger log.Logger
 
 	metrics *Metrics
@@ -188,9 +191,11 @@ func (blockExec *BlockExecutor) ProcessProposal(
 // Validation does not mutate state, but does require historical information from the stateDB,
 // ie. to verify evidence from a validator at an old height.
 func (blockExec *BlockExecutor) ValidateBlock(state State, block *types.Block) error {
-	err := validateBlock(state, block)
-	if err != nil {
-		return err
+	if !blockExec.lastValidatedBlock.HashesTo(block.Hash()) {
+		if err := validateBlock(state, block); err != nil {
+			return err
+		}
+		blockExec.lastValidatedBlock = block
 	}
 	return blockExec.evpool.CheckEvidence(block.Evidence.Evidence)
 }
@@ -211,11 +216,12 @@ func (blockExec *BlockExecutor) ApplyVerifiedBlock(
 func (blockExec *BlockExecutor) ApplyBlock(
 	state State, blockID types.BlockID, block *types.Block,
 ) (State, error) {
-
-	if err := validateBlock(state, block); err != nil {
-		return state, ErrInvalidBlock(err)
+	if !blockExec.lastValidatedBlock.HashesTo(block.Hash()) {
+		if err := validateBlock(state, block); err != nil {
+			return state, ErrInvalidBlock(err)
+		}
+		blockExec.lastValidatedBlock = block
 	}
-
 	return blockExec.applyBlock(state, blockID, block)
 }
 

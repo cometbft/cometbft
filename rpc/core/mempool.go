0db1ef9f95c1f692d7e9e38e67e9446f98fab3a4
hvanz <hernan.vanzetto@gmail.com>
2024-05-29 08:40:36 +0200

    fix rpc

--- rpc/core/mempool.go
+++ rpc/core/mempool.go
@@ -45,7 +45,7 @@ func (env *Environment) BroadcastTxSync(ctx *rpctypes.Context, tx types.Tx) (*ct
 		return nil, err
 	}
 	go func() {
-		reqRes.Wait()
+		reqRes.InvokeCallback()
 		resCh <- reqRes.Response.GetCheckTx()
 	}()
 
@@ -102,7 +102,7 @@ func (env *Environment) BroadcastTxCommit(ctx *rpctypes.Context, tx types.Tx) (*
 		return nil, ErrTxBroadcast{Source: err, ErrReason: ErrCheckTxFailed}
 	}
 	go func() {
-		reqRes.Wait()
+		reqRes.InvokeCallback()
 		checkTxResCh <- reqRes.Response.GetCheckTx()
 	}()
 

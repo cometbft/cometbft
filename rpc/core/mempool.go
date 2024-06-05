cometbft/cometbft

commit 26cb78807e7efd17b03f0ed4b90e7fd7f6aa562d

fix(mempool): Move senders from reactor back to CList entries (#3131)
Closes #3084 

This PR re-introduces the `TxInfo` parameter with sender information
into the signature of `CheckTx` in the `Mempool` interface. The new
signature is `CheckTx(tx types.Tx, sender p2p.ID) (*abcicli.ReqRes,
error)`, instead of `CheckTx(tx types.Tx) (*abcicli.ReqRes, error)`.

PR #1010 moved the senders from the CList mempool entries to the
reactor. The reason for the changes in this PR is that storing the
sender (in the reactor, through a callback) after storing the
transaction (in the CList struct) introduces a small delay during which
the transaction doesn't have a sender, allowing, in a few cases, the
gossip routines to send back the transaction to the sender. Now the
transaction and the (first) sender are stored together when the
transaction is added to the mempool.

Note that the changes in #1010 didn't reach `v0.38.x`, so this change
will partially revert the mempool API in `main` and `v1.x`.

For reviewers, please see the github comments on the code.

<!--

Please add a reference to the issue that this PR addresses and indicate
which
files are most critical to review. If it fully addresses a particular
issue,
please include "Closes #XXX" (where "XXX" is the issue number).

If this PR is non-trivial/large/complex, please ensure that you have
either
created an issue that the team's had a chance to respond to, or had some
discussion with the team prior to submitting substantial pull requests.
The team
can be reached via GitHub Discussions or the Cosmos Network Discord
server in
the #cometbft channel. GitHub Discussions is preferred over Discord as
it
allows us to keep track of conversations topically.
https://github.com/cometbft/cometbft/discussions

If the work in this PR is not aligned with the team's current
priorities, please
be advised that it may take some time before it is merged - especially
if it has
not yet been discussed with the team.

rpc/core/mempool.go
@@ -26,7 +26,7 @@ func (env *Environment) BroadcastTxAsync(_ *rpctypes.Context, tx types.Tx) (*cty
 	if env.MempoolReactor.WaitSync() {
 		return nil, ErrEndpointClosedCatchingUp
 	}
-	_, err := env.Mempool.CheckTx(tx)
+	_, err := env.Mempool.CheckTx(tx, "")
 	if err != nil {
 		return nil, err
 	}
@@ -42,18 +42,23 @@ func (env *Environment) BroadcastTxSync(ctx *rpctypes.Context, tx types.Tx) (*ct
 	}
 
 	resCh := make(chan *abci.CheckTxResponse, 1)
-	reqRes, err := env.Mempool.CheckTx(tx)
+	reqRes, err := env.Mempool.CheckTx(tx, "")
 	if err != nil {
 		return nil, err
 	}
-	reqRes.SetCallback(func(_ *abci.Response) {
+	go func() {
+		reqRes.Wait() // wait for response
 		select {
 		case <-ctx.Context().Done():
-		case resCh <- reqRes.Response.GetCheckTx():
+		default:
+			reqRes.InvokeCallback()
+			resCh <- reqRes.Response.GetCheckTx()
 		}
-	})
+	}()
+
 	select {
 	case <-ctx.Context().Done():
+		reqRes.Done() // release waiter on goroutine
 		return nil, ErrTxBroadcast{Source: ctx.Context().Err(), ErrReason: ErrConfirmationNotReceived}
 	case res := <-resCh:
 		return &ctypes.ResultBroadcastTx{
@@ -99,19 +104,24 @@ func (env *Environment) BroadcastTxCommit(ctx *rpctypes.Context, tx types.Tx) (*
 
 	// Broadcast tx and wait for CheckTx result
 	checkTxResCh := make(chan *abci.CheckTxResponse, 1)
-	reqRes, err := env.Mempool.CheckTx(tx)
+	reqRes, err := env.Mempool.CheckTx(tx, "")
 	if err != nil {
 		env.Logger.Error("Error on broadcastTxCommit", "err", err)
 		return nil, ErrTxBroadcast{Source: err, ErrReason: ErrCheckTxFailed}
 	}
-	reqRes.SetCallback(func(_ *abci.Response) {
+	go func() {
+		reqRes.Wait() // wait for response
 		select {
 		case <-ctx.Context().Done():
-		case checkTxResCh <- reqRes.Response.GetCheckTx():
+		default:
+			reqRes.InvokeCallback()
+			checkTxResCh <- reqRes.Response.GetCheckTx()
 		}
-	})
+	}()
+
 	select {
 	case <-ctx.Context().Done():
+		reqRes.Done() // release waiter on goroutine
 		return nil, ErrTxBroadcast{Source: ctx.Context().Err(), ErrReason: ErrConfirmationNotReceived}
 	case checkTxRes := <-checkTxResCh:
 		if checkTxRes.Code != abci.CodeTypeOK {

mempool/mempool.go
@@ -6,6 +6,7 @@ import (
 
 	abcicli "github.com/cometbft/cometbft/abci/client"
 	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/cometbft/cometbft/p2p"
 	"github.com/cometbft/cometbft/types"
 )
 
@@ -25,7 +26,7 @@ const (
 type Mempool interface {
 	// CheckTx executes a new transaction against the application to determine
 	// its validity and whether it should be added to the mempool.
-	CheckTx(tx types.Tx) (*abcicli.ReqRes, error)
+	CheckTx(tx types.Tx, sender p2p.ID) (*abcicli.ReqRes, error)
 
 	// RemoveTxByKey removes a transaction, identified by its key,
 	// from the mempool.
@@ -89,10 +90,6 @@ type Mempool interface {
 	// EnableTxsAvailable initializes the TxsAvailable channel, ensuring it will
 	// trigger once every height when transactions are available.
 	EnableTxsAvailable()
-
-	// Set a callback function to be called when a transaction is removed from
-	// the mempool.
-	SetTxRemovedCallback(cb func(types.TxKey))
 
 	// Size returns the number of transactions in the mempool.
 	Size() int

mempool/mocks/mempool.go
@@ -8,6 +8,8 @@ import (
 
 	mock "github.com/stretchr/testify/mock"
 
+	p2p "github.com/cometbft/cometbft/p2p"
+
 	types "github.com/cometbft/cometbft/types"
 
 	v1 "github.com/cometbft/cometbft/api/cometbft/abci/v1"
@@ -18,9 +20,9 @@ type Mempool struct {
 	mock.Mock
 }
 
-// CheckTx provides a mock function with given fields: tx
-func (_m *Mempool) CheckTx(tx types.Tx) (*abcicli.ReqRes, error) {
-	ret := _m.Called(tx)
+// CheckTx provides a mock function with given fields: tx, sender
+func (_m *Mempool) CheckTx(tx types.Tx, sender p2p.ID) (*abcicli.ReqRes, error) {
+	ret := _m.Called(tx, sender)
 
 	if len(ret) == 0 {
 		panic("no return value specified for CheckTx")
@@ -28,19 +30,19 @@ func (_m *Mempool) CheckTx(tx types.Tx) (*abcicli.ReqRes, error) {
 
 	var r0 *abcicli.ReqRes
 	var r1 error
-	if rf, ok := ret.Get(0).(func(types.Tx) (*abcicli.ReqRes, error)); ok {
-		return rf(tx)
+	if rf, ok := ret.Get(0).(func(types.Tx, p2p.ID) (*abcicli.ReqRes, error)); ok {
+		return rf(tx, sender)
 	}
-	if rf, ok := ret.Get(0).(func(types.Tx) *abcicli.ReqRes); ok {
-		r0 = rf(tx)
+	if rf, ok := ret.Get(0).(func(types.Tx, p2p.ID) *abcicli.ReqRes); ok {
+		r0 = rf(tx, sender)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*abcicli.ReqRes)
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(types.Tx) error); ok {
-		r1 = rf(tx)
+	if rf, ok := ret.Get(1).(func(types.Tx, p2p.ID) error); ok {
+		r1 = rf(tx, sender)
 	} else {
 		r1 = ret.Error(1)
 	}
@@ -157,11 +159,6 @@ func (_m *Mempool) RemoveTxByKey(txKey types.TxKey) error {
 	}
 
 	return r0
-}
-
-// SetTxRemovedCallback provides a mock function with given fields: cb
-func (_m *Mempool) SetTxRemovedCallback(cb func(types.TxKey)) {
-	_m.Called(cb)
 }
 
 // Size provides a mock function with given fields:

abci/client/client.go
@@ -36,6 +36,13 @@ type Client interface {
 	// with the exception of `CheckTxAsync` which we maintain
 	// for the v0 mempool. We should explore refactoring the
 	// mempool to remove this vestige behavior.
+	//
+	// SetResponseCallback is not used anymore. The callback was invoked only by the mempool on
+	// CheckTx responses, only during rechecking. Now the responses are handled by the callback of
+	// the *ReqRes struct returned by CheckTxAsync. This callback is more flexible as it allows to
+	// pass other information such as the sender.
+	//
+	// Deprecated: Do not use.
 	SetResponseCallback(cb Callback)
 	CheckTxAsync(ctx context.Context, req *types.CheckTxRequest) (*ReqRes, error)
 }
@@ -107,7 +114,7 @@ func (r *ReqRes) InvokeCallback() {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
 
-	if r.cb != nil {
+	if r.cb != nil && r.Response != nil {
 		r.cb(r.Response)
 	}
 	r.callbackInvoked = true

abci/client/grpc_client.go
@@ -30,7 +30,7 @@ type grpcClient struct {
 	mtx   sync.Mutex
 	addr  string
 	err   error
-	resCb func(*types.Request, *types.Response) // listens to all callbacks
+	resCb Callback // listens to all callbacks
 }
 
 func NewGRPCClient(addr string, mustConnect bool) Client {

abci/client/local_client.go
@@ -61,7 +61,9 @@ func (app *localClient) CheckTxAsync(ctx context.Context, req *types.CheckTxRequ
 }
 
 func (app *localClient) callback(req *types.Request, res *types.Response) *ReqRes {
-	app.Callback(req, res)
+	if app.Callback != nil {
+		app.Callback(req, res)
+	}
 	rr := newLocalReqRes(req, res)
 	rr.callbackInvoked = true
 	return rr
@@ -70,6 +72,7 @@ func (app *localClient) callback(req *types.Request, res *types.Response) *ReqRe
 func newLocalReqRes(req *types.Request, res *types.Response) *ReqRes {
 	reqRes := NewReqRes(req)
 	reqRes.Response = res
+	reqRes.Done() // release waiters on response
 	return reqRes
 }
 

abci/client/socket_client.go
@@ -40,8 +40,8 @@ type socketClient struct {
 
 	mtx     sync.Mutex
 	err     error
-	reqSent *list.List                            // list of requests sent, waiting for response
-	resCb   func(*types.Request, *types.Response) // called on all requests, if set.
+	reqSent *list.List // list of requests sent, waiting for response
+	resCb   Callback   // called on all requests, if set.
 }
 
 var _ Client = (*socketClient)(nil)

abci/client/unsync_local_client.go
@@ -55,7 +55,9 @@ func (app *unsyncLocalClient) CheckTxAsync(ctx context.Context, req *types.Check
 }
 
 func (app *unsyncLocalClient) callback(req *types.Request, res *types.Response) *ReqRes {
-	app.Callback(req, res)
+	if app.Callback != nil {
+		app.Callback(req, res)
+	}
 	rr := newLocalReqRes(req, res)
 	rr.callbackInvoked = true
 	return rr

commit cd465c51f9253a579d01298ce698806bf2283bd9

fix(rpc): Race condition when waiting for response on `broadcast_tx_sync/commit` (#3193)
This PR fixes a race condition generated when calling
`broadcast_tx_sync` and `broadcast_tx_commit`. When closing the context,
we want to also close the goroutine waiting for the response with
`reqRes.Wait()`. We don't need to do `reqRes.Done()` because there is a
chance it may become negative; instead, we just let the ABCI client to
do it.

This bug was recently introduced by #3131.


---

#### PR checklist

- [ ] Tests written/updated
- [ ] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)
- [ ] Updated relevant documentation (`docs/` or `spec/`) and code
comments
- [ ] Title follows the [Conventional
Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec

rpc/core/mempool.go
@@ -47,7 +47,9 @@ func (env *Environment) BroadcastTxSync(ctx *rpctypes.Context, tx types.Tx) (*ct
 		return nil, err
 	}
 	go func() {
-		reqRes.Wait() // wait for response
+		// Wait for a response. The ABCI client guarantees that it will eventually call
+		// reqRes.Done(), even in the case of error.
+		reqRes.Wait()
 		select {
 		case <-ctx.Context().Done():
 		default:
@@ -58,7 +60,6 @@ func (env *Environment) BroadcastTxSync(ctx *rpctypes.Context, tx types.Tx) (*ct
 
 	select {
 	case <-ctx.Context().Done():
-		reqRes.Done() // release waiter on goroutine
 		return nil, ErrTxBroadcast{Source: ctx.Context().Err(), ErrReason: ErrConfirmationNotReceived}
 	case res := <-resCh:
 		return &ctypes.ResultBroadcastTx{
@@ -110,7 +111,9 @@ func (env *Environment) BroadcastTxCommit(ctx *rpctypes.Context, tx types.Tx) (*
 		return nil, ErrTxBroadcast{Source: err, ErrReason: ErrCheckTxFailed}
 	}
 	go func() {
-		reqRes.Wait() // wait for response
+		// Wait for a response. The ABCI client guarantees that it will eventually call
+		// reqRes.Done(), even in the case of error.
+		reqRes.Wait()
 		select {
 		case <-ctx.Context().Done():
 		default:
@@ -121,7 +124,6 @@ func (env *Environment) BroadcastTxCommit(ctx *rpctypes.Context, tx types.Tx) (*
 
 	select {
 	case <-ctx.Context().Done():
-		reqRes.Done() // release waiter on goroutine
 		return nil, ErrTxBroadcast{Source: ctx.Context().Err(), ErrReason: ErrConfirmationNotReceived}
 	case checkTxRes := <-checkTxResCh:
 		if checkTxRes.Code != abci.CodeTypeOK {
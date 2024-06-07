5451d87ca3494fd138c43df4eb1c0f1a45215fbc
Dev Ojha <ValarDragon@users.noreply.github.com>
2024-06-07 17:55:58 +0900

    perf(p2p): Remove broadcast return channel (#3182)
    Remove the return channel and waitgroup from switch.Broadcast as its
    unused. This was costing us some CPU overhead (2% of broadcast routine
    as it stands right now, but broadcast routine time should be dropping)
    and one extra goroutine. This removes more overhead than what #3180 may
    introduce.
    
    It also better highlights the point that maybe all broadcast usecases
    should consider using TrySend (And we potentially adapt channel
    buffers), ala the point in #3151 . (This would have significant
    system-wide impact, by significantly lowering the number of
    timers/scheduler overhead we have)
    
    ---
    
    #### PR checklist
    
    - [x] Tests written/updated - Simplifies code, the behavior being
    removed was never tested!
    - [x] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [x] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    - [x] Title follows the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec
    
    ---------
    
    Co-authored-by: Anton Kaliaev <anton.kalyaev@gmail.com>
    (cherry picked from commit e731a3fdf9187fe07b2385463320669fcac569b6)
    
    # Conflicts:
    #	p2p/switch.go
    #	p2p/switch_test.go

--- p2p/switch_test.go
+++ p2p/switch_test.go
@@ -838,11 +838,10 @@ func BenchmarkSwitchBroadcast(b *testing.B) {
 
 	b.ResetTimer()
 
-	numSuccess, numFailure := 0, 0
-
 	// Send random message from foo channel to another
 	for i := 0; i < b.N; i++ {
 		chID := byte(i % 4)
+<<<<<<< HEAD
 		successChan := sw.Broadcast(Envelope{ChannelID: chID, Message: chMsg})
 		for s := range successChan {
 			if s {
@@ -851,9 +850,10 @@ func BenchmarkSwitchBroadcast(b *testing.B) {
 				numFailure++
 			}
 		}
+=======
+		s1.Broadcast(Envelope{ChannelID: chID})
+>>>>>>> e731a3fdf (perf(p2p): Remove broadcast return channel (#3182))
 	}
-
-	b.Logf("success: %v, failure: %v", numSuccess, numFailure)
 }
 
 func BenchmarkSwitchTryBroadcast(b *testing.B) {

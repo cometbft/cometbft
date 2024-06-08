3b42be824b09ddf03ec36b5b71f603ca34f70a3d
Dev Ojha <dojha@berkeley.edu>
2024-06-08 16:33:22 -0400

    Remove mtx for Initial Height

--- internal/consensus/reactor.go
+++ internal/consensus/reactor.go
@@ -269,9 +269,9 @@ func (conR *Reactor) Receive(e p2p.Envelope) {
 		switch msg := msg.(type) {
 		case *NewRoundStepMessage:
 			// TODO: Set this up on load
-			conR.conS.mtx.RLock()
+			conR.rsMtx.RLock()
 			initialHeight := conR.conS.state.InitialHeight
-			conR.conS.mtx.RUnlock()
+			conR.rsMtx.RUnlock()
 			if err = msg.ValidateHeight(initialHeight); err != nil {
 				conR.Logger.Error("Peer sent us invalid msg", "peer", e.Src, "msg", msg, "err", err)
 				conR.Switch.StopPeerForError(e.Src, err)
@@ -372,7 +372,6 @@ func (conR *Reactor) Receive(e p2p.Envelope) {
 		}
 		switch msg := msg.(type) {
 		case *VoteSetBitsMessage:
-
 			conR.rsMtx.RLock()
 			height, votes := conR.rs.Height, conR.rs.Votes
 			conR.rsMtx.RUnlock()
@@ -566,6 +565,7 @@ func (conR *Reactor) updateRoundStateNoCsLock() {
 	rs := conR.conS.getRoundState()
 	conR.rsMtx.Lock()
 	conR.rs = rs
+	conR.initialHeight = conR.conS.state.InitialHeight
 	conR.rsMtx.Unlock()
 }
 

c6ec8ae58b8ab0b8509542300c8115e27cfa55fb
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-06-28 11:10:39 +0400

    fix(rpc): fix nil pointer error in `/tx` and `/tx_search` (#3355)
    when block is absent
    Closes #3352
    
    <!--
    
    Please add a reference to the issue that this PR addresses and indicate
    which
    files are most critical to review. If it fully addresses a particular
    issue,
    please include "Closes #XXX" (where "XXX" is the issue number).
    
    If this PR is non-trivial/large/complex, please ensure that you have
    either
    created an issue that the team's had a chance to respond to, or had some
    discussion with the team prior to submitting substantial pull requests.
    The team
    can be reached via GitHub Discussions or the Cosmos Network Discord
    server in
    the #cometbft channel. GitHub Discussions is preferred over Discord as
    it
    allows us to keep track of conversations topically.
    https://github.com/cometbft/cometbft/discussions
    
    If the work in this PR is not aligned with the team's current
    priorities, please
    be advised that it may take some time before it is merged - especially
    if it has
    not yet been discussed with the team.
    
    See the project board for the team's current priorities:
    https://github.com/orgs/cometbft/projects/1
    
    -->
    
    ---
    
    #### PR checklist
    
    - [ ] ~~Tests written/updated~~
    - [x] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] ~~Updated relevant documentation (`docs/` or `spec/`) and code
    comments~~
    - [x] Title follows the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec
    
    (cherry picked from commit 3d9bbb6f39037f4485c31d718821315a87a4388a)
    
    # Conflicts:
    #	rpc/core/tx.go

--- rpc/core/tx.go
+++ rpc/core/tx.go
@@ -34,8 +34,15 @@ func Tx(ctx *rpctypes.Context, hash []byte, prove bool) (*ctypes.ResultTx, error
 
 	var proof types.TxProof
 	if prove {
+<<<<<<< HEAD
 		block := env.BlockStore.LoadBlock(r.Height)
 		proof = block.Data.Txs.Proof(int(r.Index))
+=======
+		block, _ := env.BlockStore.LoadBlock(r.Height)
+		if block != nil {
+			proof = block.Data.Txs.Proof(int(r.Index))
+		}
+>>>>>>> 3d9bbb6f3 (fix(rpc): fix nil pointer error in `/tx` and `/tx_search` (#3355))
 	}
 
 	return &ctypes.ResultTx{
@@ -113,8 +120,15 @@ func TxSearch(
 
 		var proof types.TxProof
 		if prove {
+<<<<<<< HEAD
 			block := env.BlockStore.LoadBlock(r.Height)
 			proof = block.Data.Txs.Proof(int(r.Index))
+=======
+			block, _ := env.BlockStore.LoadBlock(r.Height)
+			if block != nil {
+				proof = block.Data.Txs.Proof(int(r.Index))
+			}
+>>>>>>> 3d9bbb6f3 (fix(rpc): fix nil pointer error in `/tx` and `/tx_search` (#3355))
 		}
 
 		apiResults = append(apiResults, &ctypes.ResultTx{

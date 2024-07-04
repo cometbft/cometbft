bd95579fa66a5c4b3832db1139a6fbde8eda1557
Sergio Mena <sergio@informal.systems>
2024-07-04 10:51:57 +0200

    fix(blocksync)!: don't block in blocksync if our voting power is blocking the chain (#3406)
    Partially addresses #3415
    
    The a node has no peers, blocksync gets stuck without switching to
    consesnus, because it needs info from other peers to have an idea of
    maximum height.
    
    However, there is an edge case (mainly when testing) where a validator
    might have >2/3 of the voting power and other validators are not
    started. In this case, we know we are blocking the chain, so we don't
    need to stay in blockchain if the only condition is that we don't have
    peers.
    
    Moreover, in order to block a chain, 1/3 of the voting power is enough,
    so the reasoning of this fix is the following:
    
    * _I am a node and I am starting... shall I run blocksync?_
    * _Well, looks like I have 1/3 of the voting power (or more) at my
    current height... so there's no way the chain could advance in my
    absence... so I don't need to blocksync"_
    
    Explanation of commits:
    
    * Commit 1: `e2e` testbed reproducing the issue
    * Commit 2: commit with a trivial change to trigger `e2e` tests. Check
    the error: ❌ next to the commit hash (3fb1057)
    * Commit 3: Tentative fix. Although there is a ❌ next to the commit hash
    (16a46ea), if you click on it, you'll see that `e2e` are passing now.
    * Commit 4: revert commit2
    * Commit 5: Move the check for "local node is blocking the chain"
    outside the pool, as suggested by @cason
    * Commit 6: Fixed unit tests
    
    All further commits: addressing other comments and tidying up the code
    
    ---
    
    #### PR checklist
    
    - [x] Tests written/updated
    - [x] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - ~[ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments~
    - [x] Title follows the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec
    
    ---------
    
    Co-authored-by: Daniel <daniel.cason@informal.systems>

--- node/node.go
+++ node/node.go
@@ -350,9 +350,10 @@ func NewNode(ctx context.Context,
 	if err != nil {
 		return nil, ErrGetPubKey{Err: err}
 	}
+	localAddr := pubKey.Address()
 
 	// Determine whether we should attempt state sync.
-	stateSync := config.StateSync.Enable && !onlyValidatorIsUs(state, pubKey)
+	stateSync := config.StateSync.Enable && !onlyValidatorIsUs(state, localAddr)
 	if stateSync && state.LastBlockHeight > 0 {
 		logger.Info("Found local state with non-zero height, skipping state sync")
 		stateSync = false
@@ -377,7 +378,7 @@ func NewNode(ctx context.Context,
 
 	// Determine whether we should do block sync. This must happen after the handshake, since the
 	// app may modify the validator set, specifying ourself as the only validator.
-	blockSync := !onlyValidatorIsUs(state, pubKey)
+	blockSync := !onlyValidatorIsUs(state, localAddr)
 	waitSync := stateSync || blockSync
 
 	logNodeStartupInfo(state, pubKey, logger, consensusLogger)
@@ -422,7 +423,7 @@ func NewNode(ctx context.Context,
 		}
 	}
 	// Don't start block sync if we're doing a state sync first.
-	bcReactor, err := createBlocksyncReactor(config, state, blockExec, blockStore, blockSync && !stateSync, logger, bsMetrics, offlineStateSyncHeight)
+	bcReactor, err := createBlocksyncReactor(config, state, blockExec, blockStore, blockSync && !stateSync, localAddr, logger, bsMetrics, offlineStateSyncHeight)
 	if err != nil {
 		return nil, ErrCreateBlockSyncReactor{Err: err}
 	}

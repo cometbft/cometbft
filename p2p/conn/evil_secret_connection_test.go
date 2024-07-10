7a2da5cc595c77b00550f6c2b06395682490e43b
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-07-10 16:12:40 +0400

    fix conflicts

--- p2p/conn/evil_secret_connection_test.go
+++ p2p/conn/evil_secret_connection_test.go
@@ -255,20 +255,12 @@ func TestMakeSecretConnection(t *testing.T) {
 		conn   *evilConn
 		errMsg string
 	}{
-<<<<<<< HEAD
 		{"refuse to share ethimeral key", newEvilConn(false, false, false, false), "EOF"},
 		{"share bad ethimeral key", newEvilConn(true, true, false, false), "wrong wireType"},
 		{"refuse to share auth signature", newEvilConn(true, false, false, false), "EOF"},
 		{"share bad auth signature", newEvilConn(true, false, true, true), "failed to decrypt SecretConnection"},
-		{"all good", newEvilConn(true, false, true, false), ""},
-=======
-		{"refuse to share ethimeral key", newEvilConn(false, false, false, false), func(err error) bool { return errors.Is(err, io.EOF) }},
-		{"share bad ethimeral key", newEvilConn(true, true, false, false), func(err error) bool { return assert.Contains(t, err.Error(), "wrong wireType") }},
-		{"refuse to share auth signature", newEvilConn(true, false, false, false), func(err error) bool { return errors.Is(err, io.EOF) }},
-		{"share bad auth signature", newEvilConn(true, false, true, true), func(err error) bool { return errors.As(err, &ErrDecryptFrame{}) }},
 		// fails with the introduction of changes PR #3419
-		// {"all good", newEvilConn(true, false, true, false), func(err error) bool { return err == nil }},
->>>>>>> e1eabe0ef (perf(p2p/conn): Use a read buffer on the secret connection (#3419))
+		// {"all good", newEvilConn(true, false, true, false), ""},
 	}
 
 	for _, tc := range testCases {

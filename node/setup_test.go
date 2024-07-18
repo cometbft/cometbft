14c5d6a6fd42bdc96259b0199020c2dcd13cabaa
Alessandro <alessandro@informal.systems>
2024-07-18 16:42:18 +0200

    refactored TestLoadStateFromDBOrGenesisDocProviderWithConfig unit test: - added a sub-test testing the function's behavior when a GenesisDocProvider provides a short SHA256Checksum

--- node/setup_test.go
+++ node/setup_test.go
@@ -9,40 +9,66 @@ import (
 	"github.com/cometbft/cometbft/config"
 	"github.com/cometbft/cometbft/internal/test"
 	"github.com/cometbft/cometbft/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLoadStateFromDBOrGenesisDocProviderWithConfig(t *testing.T) {
 	cfg := test.ResetTestRoot(t.Name())
-
 	cfg.DBBackend = string(dbm.GoLevelDBBackend)
+
 	_, stateDB, err := initDBs(cfg, config.DefaultDBProvider)
-	if err != nil {
-		t.Fatalf("state DB setup: %s", err)
-	}
+	require.NoErrorf(t, err, "state DB setup: %s", err)
 
-	genDocProvider := func() (ChecksummedGenesisDoc, error) {
-		genDocJSON, err := os.ReadFile(cfg.GenesisFile())
-		if err != nil {
-			formatStr := "reading genesis file: %s"
-			return ChecksummedGenesisDoc{}, fmt.Errorf(formatStr, err)
+	genDocProviderFunc := func(Sha256Checksum []byte) GenesisDocProvider {
+
+		return func() (ChecksummedGenesisDoc, error) {
+			genDocJSON, err := os.ReadFile(cfg.GenesisFile())
+			if err != nil {
+				formatStr := "reading genesis file: %s"
+				return ChecksummedGenesisDoc{}, fmt.Errorf(formatStr, err)
+			}
+
+			genDoc, err := types.GenesisDocFromJSON(genDocJSON)
+			if err != nil {
+				formatStr := "parsing genesis file: %s"
+				return ChecksummedGenesisDoc{}, fmt.Errorf(formatStr, err)
+			}
+
+			checksummedGenesisDoc := ChecksummedGenesisDoc{
+				GenesisDoc:     genDoc,
+				Sha256Checksum: Sha256Checksum,
+			}
+
+			return checksummedGenesisDoc, nil
 		}
-
-		genDoc, err := types.GenesisDocFromJSON(genDocJSON)
-		if err != nil {
-			formatStr := "parsing genesis file: %s"
-			return ChecksummedGenesisDoc{}, fmt.Errorf(formatStr, err)
-		}
-
-		return ChecksummedGenesisDoc{GenesisDoc: genDoc}, nil
 	}
 
-	_, _, err = LoadStateFromDBOrGenesisDocProviderWithConfig(
-		stateDB,
-		genDocProvider,
-		cfg.Storage.GenesisHash,
-		nil,
-	)
-	if err != nil {
-		t.Errorf("test failed with error: %s", err)
-	}
+	t.Run("NilGenesisChecksum", func(t *testing.T) {
+		genDocProvider := genDocProviderFunc(nil)
+
+		_, _, err = LoadStateFromDBOrGenesisDocProviderWithConfig(
+			stateDB,
+			genDocProvider,
+			cfg.Storage.GenesisHash,
+			nil,
+		)
+		require.Error(t, err)
+		require.ErrorAs(t, err, &ErrSaveGenesisDocHash{})
+
+		wantErr := "failed to save genesis doc hash to db: value cannot be nil"
+		assert.EqualError(t, err, wantErr)
+	})
+
+	t.Run("ShorterGenesisChecksum", func(t *testing.T) {
+		genDocProvider := genDocProviderFunc([]byte("shorter"))
+
+		_, _, err = LoadStateFromDBOrGenesisDocProviderWithConfig(
+			stateDB,
+			genDocProvider,
+			cfg.Storage.GenesisHash,
+			nil,
+		)
+		assert.Error(t, err)
+	})
 }

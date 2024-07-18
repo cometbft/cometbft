ac7b659c7d8ce5c9d34ff6fbeee84d51badd6393
Sergio Mena <sergio@informal.systems>
2024-07-18 20:40:14 +0200

    Added all supported key types on some TCs in `file_test.go` + fixed 2 bugs in TCs

--- privval/file_test.go
+++ privval/file_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/cometbft/cometbft/crypto"
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/cometbft/cometbft/crypto/tmhash"
+	kt "github.com/cometbft/cometbft/internal/keytypes"
 	cmtrand "github.com/cometbft/cometbft/internal/rand"
 	cmtjson "github.com/cometbft/cometbft/libs/json"
 	"github.com/cometbft/cometbft/types"
@@ -20,63 +21,85 @@ import (
 )
 
 func TestGenLoadValidator(t *testing.T) {
-	privVal, tempKeyFileName, tempStateFileName := newTestFilePV(t, nil) // TODO test with all key types
+	for _, keyType := range kt.ListSupportedKeyTypes() {
+		t.Run(keyType, func(t *testing.T) {
+			keyGenF := func() (crypto.PrivKey, error) {
+				return kt.GenPrivKey(keyType)
+			}
+			privVal, tempKeyFileName, tempStateFileName := newTestFilePV(t, keyGenF)
 
-	height := int64(100)
-	privVal.LastSignState.Height = height
-	privVal.Save()
-	addr := privVal.GetAddress()
+			height := int64(100)
+			privVal.LastSignState.Height = height
+			privVal.Save()
+			addr := privVal.GetAddress()
 
-	privVal = LoadFilePV(tempKeyFileName, tempStateFileName)
-	assert.Equal(t, addr, privVal.GetAddress(), "expected privval addr to be the same")
-	assert.Equal(t, height, privVal.LastSignState.Height, "expected privval.LastHeight to have been saved")
+			privVal = LoadFilePV(tempKeyFileName, tempStateFileName)
+			assert.Equal(t, addr, privVal.GetAddress(), "expected privval addr to be the same")
+			assert.Equal(t, height, privVal.LastSignState.Height, "expected privval.LastHeight to have been saved")
+		})
+	}
 }
 
 func TestResetValidator(t *testing.T) {
-	privVal, _, tempStateFileName := newTestFilePV(t, nil) // TODO test with all key types
-	emptyState := FilePVLastSignState{filePath: tempStateFileName}
+	for _, keyType := range kt.ListSupportedKeyTypes() {
+		t.Run(keyType, func(t *testing.T) {
+			keyGenF := func() (crypto.PrivKey, error) {
+				return kt.GenPrivKey(keyType)
+			}
+			privVal, _, tempStateFileName := newTestFilePV(t, keyGenF)
+			emptyState := FilePVLastSignState{filePath: tempStateFileName}
 
-	// new priv val has empty state
-	assert.Equal(t, privVal.LastSignState, emptyState)
+			// new priv val has empty state
+			assert.Equal(t, privVal.LastSignState, emptyState)
 
-	// test vote
-	height, round := int64(10), int32(1)
-	voteType := types.PrevoteType
-	randBytes := cmtrand.Bytes(tmhash.Size)
-	blockID := types.BlockID{Hash: randBytes, PartSetHeader: types.PartSetHeader{}}
-	vote := newVote(privVal.Key.Address, height, round, voteType, blockID)
-	err := privVal.SignVote("mychainid", vote.ToProto(), false)
-	require.NoError(t, err, "expected no error signing vote")
+			// test vote
+			height, round := int64(10), int32(1)
+			voteType := types.PrevoteType
+			randBytes := cmtrand.Bytes(tmhash.Size)
+			blockID := types.BlockID{Hash: randBytes, PartSetHeader: types.PartSetHeader{}}
+			vote := newVote(privVal.Key.Address, height, round, voteType, blockID)
+			err := privVal.SignVote("mychainid", vote.ToProto(), false)
+			require.NoError(t, err, "expected no error signing vote")
 
-	// priv val after signing is not same as empty
-	assert.NotEqual(t, privVal.LastSignState, emptyState)
+			// priv val after signing is not same as empty
+			assert.NotEqual(t, privVal.LastSignState, emptyState)
 
-	// priv val after AcceptNewConnection is same as empty
-	privVal.Reset()
-	assert.Equal(t, privVal.LastSignState, emptyState)
+			// priv val after AcceptNewConnection is same as empty
+			privVal.Reset()
+			assert.Equal(t, privVal.LastSignState, emptyState)
+		})
+	}
 }
 
 func TestLoadOrGenValidator(t *testing.T) {
-	assert := assert.New(t)
+	for _, keyType := range kt.ListSupportedKeyTypes() {
+		t.Run(keyType, func(t *testing.T) {
+			assert := assert.New(t)
 
-	tempKeyFile, err := os.CreateTemp("", "priv_validator_key_")
-	require.NoError(t, err)
-	tempStateFile, err := os.CreateTemp("", "priv_validator_state_")
-	require.NoError(t, err)
+			tempKeyFile, err := os.CreateTemp("", "priv_validator_key_"+keyType+"_")
+			require.NoError(t, err)
+			tempStateFile, err := os.CreateTemp("", "priv_validator_state_"+keyType+"_")
+			require.NoError(t, err)
 
-	tempKeyFilePath := tempKeyFile.Name()
-	err = os.Remove(tempKeyFilePath)
-	require.NoError(t, err)
-	tempStateFilePath := tempStateFile.Name()
-	err = os.Remove(tempStateFilePath)
-	require.NoError(t, err)
+			tempKeyFilePath := tempKeyFile.Name()
+			err = os.Remove(tempKeyFilePath)
+			require.NoError(t, err)
+			tempStateFilePath := tempStateFile.Name()
+			err = os.Remove(tempStateFilePath)
+			require.NoError(t, err)
 
-	privVal, err := LoadOrGenFilePV(tempKeyFilePath, tempStateFilePath, nil) // TODO extend for all key types
-	require.NoError(t, err)
-	addr := privVal.GetAddress()
-	privVal, err = LoadOrGenFilePV(tempKeyFilePath, tempStateFilePath, nil)
-	require.NoError(t, err)
-	assert.Equal(addr, privVal.GetAddress(), "expected privval addr to be the same")
+			keyGenF := func() (crypto.PrivKey, error) {
+				return kt.GenPrivKey(keyType)
+			}
+			privVal, err := LoadOrGenFilePV(tempKeyFilePath, tempStateFilePath, keyGenF)
+			require.NoError(t, err)
+			addr := privVal.GetAddress()
+			// passing nil because we won't generate this time, so doesn't matter
+			privVal, err = LoadOrGenFilePV(tempKeyFilePath, tempStateFilePath, nil)
+			require.NoError(t, err)
+			assert.Equal(addr, privVal.GetAddress(), "expected privval addr to be the same")
+		})
+	}
 }
 
 func TestUnmarshalValidatorState(t *testing.T) {
@@ -144,118 +167,158 @@ func TestUnmarshalValidatorKey(t *testing.T) {
 }
 
 func TestSignVote(t *testing.T) {
-	assert := assert.New(t)
+	for _, keyType := range kt.ListSupportedKeyTypes() {
+		t.Run(keyType, func(t *testing.T) {
+			assert := assert.New(t)
+			chainID := "mychainid" + keyType
 
-	privVal, _, _ := newTestFilePV(t, nil) // TODO test with all key types
+			keyGenF := func() (crypto.PrivKey, error) {
+				return kt.GenPrivKey(keyType)
+			}
+			privVal, _, _ := newTestFilePV(t, keyGenF)
 
-	randbytes := cmtrand.Bytes(tmhash.Size)
-	randbytes2 := cmtrand.Bytes(tmhash.Size)
+			randbytes := cmtrand.Bytes(tmhash.Size)
+			randbytes2 := cmtrand.Bytes(tmhash.Size)
 
-	block1 := types.BlockID{
-		Hash:          randbytes,
-		PartSetHeader: types.PartSetHeader{Total: 5, Hash: randbytes},
+			block1 := types.BlockID{
+				Hash:          randbytes,
+				PartSetHeader: types.PartSetHeader{Total: 5, Hash: randbytes},
+			}
+			block2 := types.BlockID{
+				Hash:          randbytes2,
+				PartSetHeader: types.PartSetHeader{Total: 10, Hash: randbytes2},
+			}
+
+			height, round := int64(10), int32(1)
+			voteType := types.PrevoteType
+
+			// sign a vote for first time
+			vote := newVote(privVal.Key.Address, height, round, voteType, block1)
+			v := vote.ToProto()
+			err := privVal.SignVote(chainID, v, false)
+			require.NoError(t, err, "expected no error signing vote")
+			vote.Signature = v.Signature
+			err = vote.ValidateBasic()
+			require.NoError(t, err)
+
+			// Verify vote signature
+			pubKey, err := privVal.GetPubKey()
+			require.NoError(t, err)
+			err = vote.Verify(chainID, pubKey)
+			require.NoError(t, err)
+
+			// try to sign the same vote again; should be fine
+			err = privVal.SignVote(chainID, v, false)
+			require.NoError(t, err, "expected no error on signing same vote")
+
+			// now try some bad votes
+			cases := []*types.Vote{
+				newVote(privVal.Key.Address, height, round-1, voteType, block1),   // round regression
+				newVote(privVal.Key.Address, height-1, round, voteType, block1),   // height regression
+				newVote(privVal.Key.Address, height-2, round+4, voteType, block1), // height regression and different round
+				newVote(privVal.Key.Address, height, round, voteType, block2),     // different block
+			}
+
+			for _, c := range cases {
+				cpb := c.ToProto()
+				err = privVal.SignVote(chainID, cpb, false)
+				require.Error(t, err, "expected error on signing conflicting vote")
+			}
+
+			// try signing a vote with a different time stamp
+			sig := vote.Signature
+			vote.Signature = nil
+			vote.Timestamp = vote.Timestamp.Add(time.Duration(1000))
+			v2 := vote.ToProto()
+			err = privVal.SignVote(chainID, v2, false)
+			require.NoError(t, err)
+			assert.Equal(sig, v2.Signature)
+		})
 	}
-	block2 := types.BlockID{
-		Hash:          randbytes2,
-		PartSetHeader: types.PartSetHeader{Total: 10, Hash: randbytes2},
-	}
-
-	height, round := int64(10), int32(1)
-	voteType := types.PrevoteType
-
-	// sign a vote for first time
-	vote := newVote(privVal.Key.Address, height, round, voteType, block1)
-	v := vote.ToProto()
-	err := privVal.SignVote("mychainid", v, false)
-	require.NoError(t, err, "expected no error signing vote")
-
-	// try to sign the same vote again; should be fine
-	err = privVal.SignVote("mychainid", v, false)
-	require.NoError(t, err, "expected no error on signing same vote")
-
-	// now try some bad votes
-	cases := []*types.Vote{
-		newVote(privVal.Key.Address, height, round-1, voteType, block1),   // round regression
-		newVote(privVal.Key.Address, height-1, round, voteType, block1),   // height regression
-		newVote(privVal.Key.Address, height-2, round+4, voteType, block1), // height regression and different round
-		newVote(privVal.Key.Address, height, round, voteType, block2),     // different block
-	}
-
-	for _, c := range cases {
-		cpb := c.ToProto()
-		err = privVal.SignVote("mychainid", cpb, false)
-		require.Error(t, err, "expected error on signing conflicting vote")
-	}
-
-	// try signing a vote with a different time stamp
-	sig := vote.Signature
-	vote.Timestamp = vote.Timestamp.Add(time.Duration(1000))
-	err = privVal.SignVote("mychainid", v, false)
-	require.NoError(t, err)
-	assert.Equal(sig, vote.Signature)
 }
 
 func TestSignProposal(t *testing.T) {
-	assert := assert.New(t)
+	for _, keyType := range kt.ListSupportedKeyTypes() {
+		t.Run(keyType, func(t *testing.T) {
+			assert := assert.New(t)
+			chainID := "mychainid" + keyType
 
-	privVal, _, _ := newTestFilePV(t, nil) // TODO test with all key types
+			keyGenF := func() (crypto.PrivKey, error) {
+				return kt.GenPrivKey(keyType)
+			}
+			privVal, _, _ := newTestFilePV(t, keyGenF)
 
-	randbytes := cmtrand.Bytes(tmhash.Size)
-	randbytes2 := cmtrand.Bytes(tmhash.Size)
+			randbytes := cmtrand.Bytes(tmhash.Size)
+			randbytes2 := cmtrand.Bytes(tmhash.Size)
 
-	block1 := types.BlockID{
-		Hash:          randbytes,
-		PartSetHeader: types.PartSetHeader{Total: 5, Hash: randbytes},
+			block1 := types.BlockID{
+				Hash:          randbytes,
+				PartSetHeader: types.PartSetHeader{Total: 5, Hash: randbytes},
+			}
+			block2 := types.BlockID{
+				Hash:          randbytes2,
+				PartSetHeader: types.PartSetHeader{Total: 10, Hash: randbytes2},
+			}
+			height, round := int64(10), int32(1)
+
+			// sign a proposal for first time
+			proposal := newProposal(height, round, block1)
+			pbp := proposal.ToProto()
+			err := privVal.SignProposal(chainID, pbp)
+			sig := pbp.Signature
+			require.NoError(t, err, "expected no error signing proposal")
+
+			// try to sign the same proposal again; should be fine
+			err = privVal.SignProposal(chainID, pbp)
+			require.NoError(t, err, "expected no error on signing same proposal")
+
+			// Verify proposal signature
+			pubKey, err := privVal.GetPubKey()
+			require.NoError(t, err)
+			assert.True(pubKey.VerifySignature(types.ProposalSignBytes(chainID, pbp), sig))
+
+			// now try some bad Proposals
+			cases := []*types.Proposal{
+				newProposal(height, round-1, block1),   // round regression
+				newProposal(height-1, round, block1),   // height regression
+				newProposal(height-2, round+4, block1), // height regression and different round
+				newProposal(height, round, block2),     // different block
+			}
+
+			for _, c := range cases {
+				err = privVal.SignProposal(chainID, c.ToProto())
+				require.Error(t, err, "expected error on signing conflicting proposal")
+			}
+
+			// try signing a proposal with a different time stamp
+			proposal.Timestamp = proposal.Timestamp.Add(time.Duration(1000))
+			pbp2 := proposal.ToProto()
+			err = privVal.SignProposal(chainID, pbp2)
+			require.NoError(t, err)
+			assert.Equal(sig, pbp2.Signature)
+		})
 	}
-	block2 := types.BlockID{
-		Hash:          randbytes2,
-		PartSetHeader: types.PartSetHeader{Total: 10, Hash: randbytes2},
-	}
-	height, round := int64(10), int32(1)
-
-	// sign a proposal for first time
-	proposal := newProposal(height, round, block1)
-	pbp := proposal.ToProto()
-	err := privVal.SignProposal("mychainid", pbp)
-	require.NoError(t, err, "expected no error signing proposal")
-
-	// try to sign the same proposal again; should be fine
-	err = privVal.SignProposal("mychainid", pbp)
-	require.NoError(t, err, "expected no error on signing same proposal")
-
-	// now try some bad Proposals
-	cases := []*types.Proposal{
-		newProposal(height, round-1, block1),   // round regression
-		newProposal(height-1, round, block1),   // height regression
-		newProposal(height-2, round+4, block1), // height regression and different round
-		newProposal(height, round, block2),     // different block
-	}
-
-	for _, c := range cases {
-		err = privVal.SignProposal("mychainid", c.ToProto())
-		require.Error(t, err, "expected error on signing conflicting proposal")
-	}
-
-	// try signing a proposal with a different time stamp
-	sig := proposal.Signature
-	proposal.Timestamp = proposal.Timestamp.Add(time.Duration(1000))
-	err = privVal.SignProposal("mychainid", pbp)
-	require.NoError(t, err)
-	assert.Equal(sig, proposal.Signature)
 }
 
 func TestSignBytes(t *testing.T) {
-	privVal, _, _ := newTestFilePV(t, nil) // TODO test with all key types
-	testBytes := []byte("test bytes for signing")
+	for _, keyType := range kt.ListSupportedKeyTypes() {
+		t.Run(keyType, func(t *testing.T) {
+			keyGenF := func() (crypto.PrivKey, error) {
+				return kt.GenPrivKey(keyType)
+			}
+			privVal, _, _ := newTestFilePV(t, keyGenF)
+			testBytes := []byte("test bytes for signing TODO: REMOVE ME AFTER FIXING BLS")
 
-	// Sign the test bytes
-	sig, err := privVal.SignBytes(testBytes)
-	require.NoError(t, err, "expected no error signing bytes")
+			// Sign the test bytes
+			sig, err := privVal.SignBytes(testBytes)
+			require.NoError(t, err, "expected no error signing bytes")
 
-	// Verify the signature
-	pubKey, err := privVal.GetPubKey()
-	require.NoError(t, err, "expected no error getting public key")
-	assert.True(t, pubKey.VerifySignature(testBytes, sig), "signature verification failed")
+			// Verify the signature
+			pubKey, err := privVal.GetPubKey()
+			require.NoError(t, err, "expected no error getting public key")
+			assert.True(t, pubKey.VerifySignature(testBytes, sig), "signature verification failed")
+		})
+	}
 }
 
 func TestDifferByTimestamp(t *testing.T) {

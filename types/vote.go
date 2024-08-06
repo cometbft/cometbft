9b458e0b61810a13009898fd708a2be650f4c388
chillyvee <blockhead@chillvalidation.com>
2024-08-06 18:52:03 +0100

    fix(types) CV only check invalid vote extension when extensionsEnabled

--- types/vote.go
+++ types/vote.go
@@ -428,15 +428,15 @@ func SignAndCheckVote(
 		return false, &ErrVoteExtensionInvalid{ExtSignature: v.ExtensionSignature}
 	}
 
-	isNil := vote.BlockID.IsZero()
-	extSignature := (len(v.ExtensionSignature) > 0)
-	if extSignature == (!isPrecommit || isNil) {
-		// Non-recoverable because the vote is malformed
-		return false, &ErrVoteExtensionInvalid{ExtSignature: v.ExtensionSignature}
-	}
-
 	vote.ExtensionSignature = nil
 	if extensionsEnabled {
+		isNil := vote.BlockID.IsZero()
+		extSignature := (len(v.ExtensionSignature) > 0)
+		if extSignature == (!isPrecommit || isNil) {
+			// Non-recoverable because the vote is malformed
+			return false, &ErrVoteExtensionInvalid{ExtSignature: v.ExtensionSignature}
+		}
+
 		vote.ExtensionSignature = v.ExtensionSignature
 	}
 

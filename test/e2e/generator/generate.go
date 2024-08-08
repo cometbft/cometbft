2f39bd2495bef4e04935f9695fee4c936678e20c
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-08-08 17:39:00 +0400

    fix conflicts

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -13,12 +13,6 @@ import (
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
 
-<<<<<<< HEAD
-=======
-	"github.com/cometbft/cometbft/crypto/bls12381"
-	"github.com/cometbft/cometbft/crypto/ed25519"
-	"github.com/cometbft/cometbft/crypto/secp256k1"
->>>>>>> ab5b20ace (feat(crypto)!: remove sr25519 curve (#3646))
 	e2e "github.com/cometbft/cometbft/test/e2e/pkg"
 	"github.com/cometbft/cometbft/version"
 )
@@ -74,10 +68,6 @@ var (
 	pbtsUpdateHeight           = uniformChoice{int64(-1), int64(0), int64(1)}                // -1: genesis, 0: InitChain, 1: (use offset)
 	pbtsEnabled                = weightedChoice{true: 3, false: 1}
 	pbtsHeightOffset           = uniformChoice{int64(0), int64(10), int64(100)}
-<<<<<<< HEAD
-=======
-	keyType                    = uniformChoice{ed25519.KeyType, secp256k1.KeyType, bls12381.KeyType}
->>>>>>> ab5b20ace (feat(crypto)!: remove sr25519 curve (#3646))
 )
 
 type generateConfig struct {
--- test/e2e/pkg/testnet.go
+++ test/e2e/pkg/testnet.go
@@ -20,6 +20,7 @@ import (
 	_ "embed"
 
 	"github.com/cometbft/cometbft/crypto"
+	"github.com/cometbft/cometbft/crypto/bls12381"
 	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/cometbft/cometbft/crypto/secp256k1"
 	rpchttp "github.com/cometbft/cometbft/rpc/client/http"
@@ -751,21 +752,15 @@ func (g *keyGenerator) Generate(keyType string) crypto.PrivKey {
 		panic(err) // this shouldn't happen
 	}
 	switch keyType {
-	case "secp256k1":
+	case secp256k1.KeyType:
 		return secp256k1.GenPrivKeySecp256k1(seed)
-<<<<<<< HEAD
-	case "sr25519":
-		return sr25519.GenPrivKeyFromSecret(seed)
-	case "", "ed25519":
-=======
 	case bls12381.KeyType:
 		pk, err := bls12381.GenPrivKey()
 		if err != nil {
 			panic(fmt.Sprintf("unrecoverable error when generating key; key type %s, err %v", bls12381.KeyType, err))
 		}
 		return pk
-	case ed25519.KeyType:
->>>>>>> ab5b20ace (feat(crypto)!: remove sr25519 curve (#3646))
+	case "", ed25519.KeyType:
 		return ed25519.GenPrivKeyFromSecret(seed)
 	default:
 		panic("KeyType not supported") // should not make it this far

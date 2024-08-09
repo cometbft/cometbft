cometbft/cometbft

commit d4a82b7ef59b5000d29db65941ee4ddbf3d6f8d5

refactor(mempool): Add `Iterator` to replace `TxsFront` and `TxsWaitChan` methods (#3459)
Closes #3303 

This PR adds:
- the `Entry` interface, implemented by the existing `mempoolTx` data
type,
- the `Iterator` interface, with one method `WaitNextCh() <-chan Entry`.

On the routine that sends transactions to a peer, we can now use an
`Iterator` to select the next mempool entry to send, instead of the
methods `TxsFront` and `TxsWaitChan` in `CListMempool`. These two
methods leak implementation details about the CList and are now marked
as deprecated. Now all the logic needed to wait for and pick the next
available entry is implemented in `CListMempool.WaitNextCh`.

---

#### PR checklist

- [X] Tests written/updated
- [x] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)
- [ ] Updated relevant documentation (`docs/` or `spec/`) and code
comments
- [x] Title follows the [Conventional
Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec

---------

Co-authored-by: Anton Kaliaev <anton.kalyaev@gmail.com>

mempool/reactor.go
@@ -12,7 +12,6 @@ import (
 	abcicli "github.com/cometbft/cometbft/abci/client"
 	protomem "github.com/cometbft/cometbft/api/cometbft/mempool/v1"
 	cfg "github.com/cometbft/cometbft/config"
-	"github.com/cometbft/cometbft/internal/clist"
 	"github.com/cometbft/cometbft/libs/log"
 	"github.com/cometbft/cometbft/p2p"
 	"github.com/cometbft/cometbft/types"
@@ -208,8 +207,6 @@ type PeerState interface {
 
 // Send new mempool txs to peer.
 func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
-	var next *clist.CElement
-
 	// If the node is catching up, don't start this routine immediately.
 	if memR.WaitSync() {
 		select {
@@ -235,26 +232,24 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 		time.Sleep(PeerCatchupSleepIntervalMS * time.Millisecond)
 	}
 
+	iter := memR.mempool.NewIterator()
+	var entry Entry
 	for {
 		// In case of both next.NextWaitChan() and peer.Quit() are variable at the same time
 		if !memR.IsRunning() || !peer.IsRunning() {
 			return
 		}
 
-		// This happens because the CElement we were looking at got garbage
-		// collected (removed). That is, .NextWait() returned nil. Go ahead and
-		// start from the beginning.
-		if next == nil {
-			select {
-			case <-memR.mempool.TxsWaitChan(): // Wait until a tx is available
-				if next = memR.mempool.TxsFront(); next == nil {
-					continue
-				}
-			case <-peer.Quit():
-				return
-			case <-memR.Quit():
-				return
+		select {
+		case entry = <-iter.WaitNextCh():
+			// If the entry we were looking at got garbage collected (removed), try again.
+			if entry == nil {
+				continue
 			}
+		case <-peer.Quit():
+			return
+		case <-memR.Quit():
+			return
 		}
 
 		// If we suspect that the peer is lagging behind, at least by more than
@@ -263,8 +258,7 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 		// node. See [RFC 103] for an analysis on this optimization.
 		//
 		// [RFC 103]: https://github.com/cometbft/cometbft/pull/735
-		memTx := next.Value.(*mempoolTx)
-		if peerState.GetHeight() < memTx.Height()-1 {
+		if peerState.GetHeight() < entry.Height()-1 {
 			time.Sleep(PeerCatchupSleepIntervalMS * time.Millisecond)
 			continue
 		}
@@ -272,25 +266,15 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 		// NOTE: Transaction batching was disabled due to
 		// https://github.com/tendermint/tendermint/issues/5796
 
-		if !memTx.isSender(peer.ID()) {
+		if !entry.IsSender(peer.ID()) {
 			success := peer.Send(p2p.Envelope{
 				ChannelID: MempoolChannel,
-				Message:   &protomem.Txs{Txs: [][]byte{memTx.tx}},
+				Message:   &protomem.Txs{Txs: [][]byte{entry.Tx()}},
 			})
 			if !success {
 				time.Sleep(PeerCatchupSleepIntervalMS * time.Millisecond)
 				continue
 			}
-		}
-
-		select {
-		case <-next.NextWaitChan():
-			// see the start of the for loop for nil check
-			next = next.Next()
-		case <-peer.Quit():
-			return
-		case <-memR.Quit():
-			return
 		}
 	}
 }

mempool/clist_mempool.go
@@ -221,6 +221,8 @@ func (mem *CListMempool) Flush() {
 // FIXME: leaking implementation details!
 //
 // Safe for concurrent use by multiple goroutines.
+//
+// Deprecated: Use CListIterator instead.
 func (mem *CListMempool) TxsFront() *clist.CElement {
 	return mem.txs.Front()
 }
@@ -230,6 +232,8 @@ func (mem *CListMempool) TxsFront() *clist.CElement {
 // element)
 //
 // Safe for concurrent use by multiple goroutines.
+//
+// Deprecated: Use CListIterator instead.
 func (mem *CListMempool) TxsWaitChan() <-chan struct{} {
 	return mem.txs.WaitChan()
 }
@@ -781,4 +785,49 @@ func (rc *recheck) setRecheckFull() bool {
 // progress.
 func (rc *recheck) consideredFull() bool {
 	return rc.recheckFull.Load()
+}
+
+// CListIterator implements an Iterator that traverses the CList sequentially. When the current
+// entry is removed from the mempool, the iterator starts from the beginning of the CList. When it
+// reaches the end, it waits until a new entry is appended.
+type CListIterator struct {
+	txs    *clist.CList    // to wait on and retrieve the first entry
+	cursor *clist.CElement // pointer to the current entry in the list
+}
+
+func (mem *CListMempool) NewIterator() Iterator {
+	return &CListIterator{
+		txs: mem.txs,
+	}
+}
+
+// WaitNextCh returns a channel to wait for the next available entry. The channel will be explicitly
+// closed when the entry gets removed before it is added to the channel, or when reaching the end of
+// the list.
+//
+// Unsafe for concurrent use by multiple goroutines.
+func (iter *CListIterator) WaitNextCh() <-chan Entry {
+	ch := make(chan Entry)
+	// Spawn goroutine that waits for the next entry, saves it locally, and puts it in the channel.
+	go func() {
+		if iter.cursor == nil {
+			// We are at the beginning of the iteration or the saved entry got removed: wait until
+			// the list becomes not empty and select the first entry.
+			<-iter.txs.WaitChan()
+			// Note that Front can return nil.
+			iter.cursor = iter.txs.Front()
+		} else {
+			// Wait for the next entry after the current one.
+			<-iter.cursor.NextWaitChan()
+			// If the current entry is the last one or was removed, Next will return nil.
+			iter.cursor = iter.cursor.Next()
+		}
+		if iter.cursor != nil {
+			ch <- iter.cursor.Value.(Entry)
+		} else {
+			// Unblock the receiver (it will receive nil).
+			close(ch)
+		}
+	}()
+	return ch
 }

mempool/clist_mempool_test.go
@@ -1,6 +1,7 @@
 package mempool
 
 import (
+	"bytes"
 	"context"
 	"encoding/binary"
 	"errors"
@@ -146,10 +147,11 @@ func TestReapMaxBytesMaxGas(t *testing.T) {
 
 	// Ensure gas calculation behaves as expected
 	checkTxs(t, mp, 1)
-	tx0 := mp.TxsFront().Value.(*mempoolTx)
-	require.Equal(t, tx0.gasWanted, int64(1), "transactions gas was set incorrectly")
+	iter := mp.NewIterator()
+	tx0 := <-iter.WaitNextCh()
+	require.Equal(t, tx0.GasWanted(), int64(1), "transactions gas was set incorrectly")
 	// ensure each tx is 20 bytes long
-	require.Len(t, tx0.tx, 20, "Tx is longer than 20 bytes")
+	require.Len(t, tx0.Tx(), 20, "Tx is longer than 20 bytes")
 	mp.Flush()
 
 	// each table driven test creates numTxsToCreate txs with checkTx, and at the end clears all remaining txs.
@@ -996,6 +998,44 @@ func TestMempoolConcurrentCheckTxAndUpdate(t *testing.T) {
 
 	// All added transactions should have been removed from the mempool.
 	require.Zero(t, mp.Size())
+}
+
+func TestMempoolIterator(t *testing.T) {
+	app := kvstore.NewInMemoryApplication()
+	cc := proxy.NewLocalClientCreator(app)
+
+	cfg := test.ResetTestRoot("mempool_test")
+	mp, cleanup := newMempoolWithAppAndConfig(cc, cfg)
+	defer cleanup()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	n := numTxs
+
+	// Spawn a goroutine that iterates on the list until counting n entries.
+	counter := 0
+	go func() {
+		defer wg.Done()
+
+		iter := mp.NewIterator()
+		for counter < n {
+			entry := <-iter.WaitNextCh()
+			require.True(t, bytes.Equal(kvstore.NewTxFromID(counter), entry.Tx()))
+			counter++
+		}
+	}()
+
+	// Add n transactions with sequential ids.
+	for i := 0; i < n; i++ {
+		tx := kvstore.NewTxFromID(i)
+		rr, err := mp.CheckTx(tx, "")
+		require.NoError(t, err)
+		rr.Wait()
+	}
+
+	wg.Wait()
+	require.Equal(t, n, counter)
 }
 
 func newMempoolWithAsyncConnection(t *testing.T) (*CListMempool, cleanupFunc) {

mempool/mempool.go
@@ -148,3 +148,25 @@ func PostCheckMaxGas(maxGas int64) PostCheckFunc {
 
 // TxKey is the fixed length array key used as an index.
 type TxKey [sha256.Size]byte
+
+// An entry in the mempool.
+type Entry interface {
+	// Tx returns the transaction stored in the entry.
+	Tx() types.Tx
+
+	// Height returns the height of the latest block at the moment the entry was created.
+	Height() int64
+
+	// GasWanted returns the amount of gas required by the transaction.
+	GasWanted() int64
+
+	// IsSender returns whether we received the transaction from the given peer ID.
+	IsSender(peerID p2p.ID) bool
+}
+
+// An iterator is used to iterate through the mempool entries.
+// Multiple iterators should be allowed to run concurrently.
+type Iterator interface {
+	// WaitNextCh returns a channel on which to wait for the next available entry.
+	WaitNextCh() <-chan Entry
+}

mempool/mempoolTx.go
@@ -19,12 +19,19 @@ type mempoolTx struct {
 	senders sync.Map
 }
 
-// Height returns the height for this transaction.
+func (memTx *mempoolTx) Tx() types.Tx {
+	return memTx.tx
+}
+
 func (memTx *mempoolTx) Height() int64 {
 	return atomic.LoadInt64(&memTx.height)
 }
 
-func (memTx *mempoolTx) isSender(peerID p2p.ID) bool {
+func (memTx *mempoolTx) GasWanted() int64 {
+	return memTx.gasWanted
+}
+
+func (memTx *mempoolTx) IsSender(peerID p2p.ID) bool {
 	_, ok := memTx.senders.Load(peerID)
 	return ok
 }

commit 1ac883258c786d1f88976f6536c422e2e3d97930

fix(mempool): tx is skipped when sending to a lagging peer or sending fails (#3647)
Fixes a bug introduced in #3459 where instead of retrying the same tx
when the peer is lagging, it was skipping it and moving on to the next
entry. The same used to happen when sending the transaction failed:
instead of retrying to send, it skipped it and proceeded to the next tx.
This PR also reverts #3430, which was not working as expected.

Note that the first commit introduces a new test
`TestMempoolReactorSendLaggingPeer` that initially fails and then it's
fixed:
https://github.com/cometbft/cometbft/actions/runs/10300181146/job/28509056466?pr=3647

---

#### PR checklist

- [X] Tests written/updated
- ~~[ ] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)~~
- ~~[ ] Updated relevant documentation (`docs/` or `spec/`) and code
comments~~
- [X] Title follows the [Conventional
Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec

mempool/reactor.go
@@ -217,21 +217,6 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 		}
 	}
 
-	var peerState PeerState
-	// Wait until the peer's state is ready. We initialize it in the consensus reactor, but when we
-	// add the peer in Switch, the order in which we call reactors#AddPeer is different every time
-	// due to us using a map. Sometimes other reactors will be initialized before the consensus
-	// reactor. We should wait a few milliseconds and retry. We assume the pointer to the state is
-	// set once and never unset.
-	for {
-		if ps, ok := peer.Get(types.PeerStateKey).(PeerState); ok {
-			peerState = ps
-			break
-		}
-		// Peer does not have a state yet.
-		time.Sleep(PeerCatchupSleepIntervalMS * time.Millisecond)
-	}
-
 	iter := memR.mempool.NewIterator()
 	var entry Entry
 	for {
@@ -257,23 +242,49 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 		// reduces the mempool size and the recheck-tx rate of the receiving
 		// node. See [RFC 103] for an analysis on this optimization.
 		//
-		// [RFC 103]: https://github.com/cometbft/cometbft/pull/735
-		if peerState.GetHeight() < entry.Height()-1 {
-			time.Sleep(PeerCatchupSleepIntervalMS * time.Millisecond)
-			continue
+		// [RFC 103]: https://github.com/CometBFT/cometbft/blob/main/docs/references/rfc/rfc-103-incoming-txs-when-catching-up.md
+		for {
+			// Make sure the peer's state is up to date. The peer may not have a
+			// state yet. We set it in the consensus reactor, but when we add
+			// peer in Switch, the order we call reactors#AddPeer is different
+			// every time due to us using a map. Sometimes other reactors will
+			// be initialized before the consensus reactor. We should wait a few
+			// milliseconds and retry.
+			peerState, ok := peer.Get(types.PeerStateKey).(PeerState)
+			if ok && peerState.GetHeight()+1 >= entry.Height() {
+				break
+			}
+			select {
+			case <-time.After(PeerCatchupSleepIntervalMS * time.Millisecond):
+			case <-peer.Quit():
+				return
+			case <-memR.Quit():
+				return
+			}
 		}
 
 		// NOTE: Transaction batching was disabled due to
 		// https://github.com/tendermint/tendermint/issues/5796
 
-		if !entry.IsSender(peer.ID()) {
+		// Do not send this transaction if we receive it from peer.
+		if entry.IsSender(peer.ID()) {
+			continue
+		}
+
+		for {
 			success := peer.Send(p2p.Envelope{
 				ChannelID: MempoolChannel,
 				Message:   &protomem.Txs{Txs: [][]byte{entry.Tx()}},
 			})
-			if !success {
-				time.Sleep(PeerCatchupSleepIntervalMS * time.Millisecond)
-				continue
+			if success {
+				break
+			}
+			select {
+			case <-time.After(PeerCatchupSleepIntervalMS * time.Millisecond):
+			case <-peer.Quit():
+				return
+			case <-memR.Quit():
+				return
 			}
 		}
 	}

mempool/reactor_test.go
@@ -156,6 +156,37 @@ func TestReactorNoBroadcastToSender(t *testing.T) {
 	ensureNoTxs(t, reactors[1], 100*time.Millisecond)
 }
 
+// Test that a lagging peer does not receive txs.
+func TestMempoolReactorSendLaggingPeer(t *testing.T) {
+	config := cfg.TestConfig()
+	const n = 2
+	reactors, _ := makeAndConnectReactors(config, n)
+	defer func() {
+		for _, r := range reactors {
+			if err := r.Stop(); err != nil {
+				require.NoError(t, err)
+			}
+		}
+	}()
+
+	// First reactor is at height 10 and knows that its peer is lagging at height 1.
+	reactors[0].mempool.height.Store(10)
+	peerID := reactors[1].Switch.NodeInfo().ID()
+	reactors[0].Switch.Peers().Get(peerID).Set(types.PeerStateKey, peerState{1})
+
+	// Add a bunch of txs to the first reactor. The second reactor should not receive any tx.
+	txs1 := checkTxs(t, reactors[0].mempool, numTxs)
+	ensureNoTxs(t, reactors[1], 5*PeerCatchupSleepIntervalMS*time.Millisecond)
+
+	// Now we know that the second reactor has advanced to height 9, so it should receive all txs.
+	reactors[0].Switch.Peers().Get(peerID).Set(types.PeerStateKey, peerState{9})
+	waitForReactors(t, txs1, reactors, checkTxsInOrder)
+
+	// Add a bunch of txs to first reactor. The second reactor should receive them all.
+	txs2 := checkTxs(t, reactors[0].mempool, numTxs)
+	waitForReactors(t, append(txs1, txs2...), reactors, checkTxsInOrder)
+}
+
 func TestReactor_MaxTxBytes(t *testing.T) {
 	config := cfg.TestConfig()
 
68dd2b36dff9117c9897ba6dcfab8a74abde9725
hvanz <hernan.vanzetto@gmail.com>
2024-08-12 10:53:26 +0300

    break, don't continue

--- mempool/reactor.go
+++ mempool/reactor.go
@@ -275,7 +275,7 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 			// The entry may have been removed from the mempool since it was
 			// chosen at the beginning of the loop. Skip it if that's the case.
 			if !memR.mempool.Contains(entry.Tx().Key()) {
-				continue
+				break
 			}
 
 			success := peer.Send(p2p.Envelope{

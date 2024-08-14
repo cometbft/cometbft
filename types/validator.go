ed086740bdc46f7d3729732a91e09c7fac1f9f5b
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-08-14 13:07:12 +0400

    Update types/validator.go
    Co-authored-by: Sergio Mena <sergio@informal.systems>

--- types/validator.go
+++ types/validator.go
@@ -121,7 +121,7 @@ func ValidatorListString(vals []*Validator) string {
 // as its redundant with the pubkey. This also excludes ProposerPriority
 // which changes every round.
 //
-// If the public key is unsupported, it returns nil.
+// If the public key is unsupported, it returns nil. Caller must handle `nil` as it would handle an error
 func (v *Validator) Bytes() []byte {
 	pk, err := ce.PubKeyToProto(v.PubKey)
 	if err != nil {

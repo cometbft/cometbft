24abee972372cd1cf6b3429ae572947515cc77f6
Hernán Vanzetto <15466498+hvanz@users.noreply.github.com>
2024-08-15 17:51:55 +0300

    fix(mempool): Update variables in `CListMempool` atomically (#3700)
    Fixes #3694
    
    This PR replaces:
    ```
    type CListMempool struct {
    	txsBytes atomic.Int64 // total size of mempool, in bytes
            ...
    	txs    *clist.CList
    	txsMap sync.Map
            ...
    }
    ```
    by
    ```
    type CListMempool struct {
            ...
    	txsMtx   cmtsync.RWMutex
    	txs      *clist.CList                    // concurrent linked-list of valid txs
    	txsMap   map[types.TxKey]*clist.CElement // for quick access to txs
    	txsBytes int64                           // total size of mempool, in bytes
            ...
    }
    ```
    The new `txsMtx` mutex is used every time the variables `txs`, `txsMap`,
    and `txsBytes` are accessed.
    
    ---
    
    - [ ] Tests written/updated
    - [x] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    - [ ] Title follows the [Conventional
    Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec
    
    ---------
    
    Co-authored-by: Andy Nogueira <me@andynogueira.dev>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- rpc/client/rpc_test.go
+++ rpc/client/rpc_test.go
@@ -373,7 +373,7 @@ func TestUnconfirmedTx(t *testing.T) {
 
 	ch := make(chan *abci.CheckTxResponse, 1)
 	mempool := node.Mempool()
-	reqRes, err := mempool.CheckTx(tx)
+	reqRes, err := mempool.CheckTx(tx, "")
 	require.NoError(t, err)
 	ch <- reqRes.Response.GetCheckTx()
 

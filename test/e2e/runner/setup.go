b92931ffb6ed3f8343514c4a73d8f1ea21497070
Daniel <daniel.cason@informal.systems>
2024-09-10 17:59:26 +0200

    fix(e2e): viper errors with invalid `config` and `genesis` manifest keys (#4016)
    Addresses comments
    https://github.com/cometbft/cometbft/pull/3969#pullrequestreview-2277655582
    and
    https://github.com/cometbft/cometbft/pull/3967#pullrequestreview-2277635023
    on previous PRs.
    
    The solution is in fact simple, as it is possible to tell viper to
    return an error if one of the configured entries (key/value pairs) were
    not applied to the destination structure, because they don't exist or
    are invalid.
    
    ---
    
    #### PR checklist
    
    - [ ] Tests written/updated
    - [ ] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Andy Nogueira <me@andynogueira.dev>

--- test/e2e/runner/setup.go
+++ test/e2e/runner/setup.go
@@ -172,23 +172,26 @@ func MakeGenesis(testnet *e2e.Testnet) (types.GenesisDoc, error) {
 
 	// Customized genesis fields provided in the manifest
 	if len(testnet.Genesis) > 0 {
-		viper.Reset()
-		viper.SetConfigType("json")
+		v := viper.New()
+		v.SetConfigType("json")
 
-		for _, entry := range testnet.Genesis {
-			tokens := strings.Split(entry, " = ")
-			key, value := tokens[0], tokens[1]
-			logger.Debug("Applying Genesis config", key, value)
-			viper.Set(key, value)
+		for _, field := range testnet.Genesis {
+			key, value, err := e2e.ParseKeyValueField("genesis", field)
+			if err != nil {
+				return genesis, err
+			}
+			logger.Debug("Applying 'genesis' field", key, value)
+			v.Set(key, value)
 		}
 
 		// We use viper because it leaves untouched keys that are not set.
 		// The GenesisDoc does not use the original `mapstructure` tag.
-		err := viper.Unmarshal(&genesis, func(d *mapstructure.DecoderConfig) {
+		err := v.Unmarshal(&genesis, func(d *mapstructure.DecoderConfig) {
 			d.TagName = "json"
+			d.ErrorUnused = true
 		})
 		if err != nil {
-			return genesis, err
+			return genesis, fmt.Errorf("failed parsing 'genesis' field: %v", err)
 		}
 	}
 
@@ -340,15 +343,20 @@ func MakeConfig(node *e2e.Node) (*config.Config, error) {
 
 	// We currently need viper in order to parse config files.
 	if len(node.Config) > 0 {
-		viper.Reset()
-		for _, entry := range node.Config {
-			tokens := strings.Split(entry, " = ")
-			key, value := tokens[0], tokens[1]
-			logger.Debug("Applying Comet config", "node", node.Name, key, value)
-			viper.Set(key, value)
+		v := viper.New()
+		for _, field := range node.Config {
+			key, value, err := e2e.ParseKeyValueField("config", field)
+			if err != nil {
+				return nil, err
+			}
+			logger.Debug("Applying 'config' field", "node", node.Name, key, value)
+			v.Set(key, value)
 		}
-		if err := viper.Unmarshal(cfg); err != nil {
-			return nil, err
+		err := v.Unmarshal(cfg, func(d *mapstructure.DecoderConfig) {
+			d.ErrorUnused = true
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed parsing 'config' field of node %v: %v", node.Name, err)
 		}
 	}
 

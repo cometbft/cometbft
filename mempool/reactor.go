d62516290c5efba06b98f23e4ca58c0f68908a54
HuangYi <huang@crypto.com>
2024-09-19 17:01:06 +0800

    fix: "mempool is full" message flood log file
    Solution:
    - change it to debug level

--- mempool/reactor.go
+++ mempool/reactor.go
@@ -172,12 +172,15 @@ func (memR *Reactor) TryAddTx(tx types.Tx, sender p2p.Peer) (*abcicli.ReqRes, er
 	}
 
 	reqRes, err := memR.mempool.CheckTx(tx, senderID)
-	switch {
-	case errors.Is(err, ErrTxInCache):
-		memR.Logger.Debug("Tx already exists in cache", "tx", log.NewLazySprintf("%X", tx.Hash()), "sender", senderID)
-		return nil, err
-	case err != nil:
-		memR.Logger.Info("Could not check tx", "tx", log.NewLazySprintf("%X", tx.Hash()), "sender", senderID, "err", err)
+	if err != nil {
+		switch {
+		case errors.Is(err, ErrTxInCache):
+			memR.Logger.Debug("Tx already exists in cache", "tx", log.NewLazySprintf("%X", tx.Hash()), "sender", senderID)
+		case errors.Is(err, ErrMempoolIsFull{}):
+			memR.Logger.Debug(err.Error())
+		default:
+			memR.Logger.Info("Could not check tx", "tx", log.NewLazySprintf("%X", tx.Hash()), "sender", senderID, "err", err)
+		}
 		return nil, err
 	}
 

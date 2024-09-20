cometbft/cometbft

commit 30c508a91385b03e11ae3fc411c6e0701b1544c5

chore(crypto/bls12381): remove cosmos/crypto wrapper for BLS curve (#3603)
I don't think it adds much value; hence, we can use the [underlying
library](https://github.com/supranational/blst/blob/master/bindings/go)
directly. This PR is the preparation for the BLS signature aggregation.

* remove `cosmos/crypto` wrapper for BLS curve

---

#### PR checklist

- [x] Tests written/updated
- [ ] ~~Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)~~
- [ ] ~~Updated relevant documentation (`docs/` or `spec/`) and code
comments~~
- [x] Title follows the [Conventional
Commits](https://www.conventionalcommits.org/en/v1.0.0/) spec

crypto/bls12381/key_bls12381.go
@@ -1,13 +1,15 @@
-//go:build ((linux && amd64) || (linux && arm64) || (darwin && amd64) || (darwin && arm64) || (windows && amd64)) && bls12381
+//go:build bls12381
 
 package bls12381
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
+	"errors"
+
+	blst "github.com/supranational/blst/bindings/go"
 
 	"github.com/cometbft/cometbft/crypto"
-	bls12381 "github.com/cosmos/crypto/curves/bls12381"
-
 	"github.com/cometbft/cometbft/crypto/tmhash"
 	cmtjson "github.com/cometbft/cometbft/libs/json"
 )
@@ -15,6 +17,21 @@ import (
 const (
 	// Enabled indicates if this curve is enabled.
 	Enabled = true
+)
+
+var (
+	// ErrDeserialization is returned when deserialization fails.
+	ErrDeserialization = errors.New("bls12381: deserialization error")
+
+	dstMinSig = []byte("BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_NUL_")
+)
+
+// For minimal-pubkey-size operations.
+type (
+	blstPublicKey          = blst.P1Affine
+	blstSignature          = blst.P2Affine
+	blstAggregateSignature = blst.P1Aggregate
+	blstAggregatePublicKey = blst.P2Aggregate
 )
 
 // -------------------------------------.
@@ -34,37 +51,39 @@ func init() {
 
 var _ crypto.PrivKey = &PrivKey{}
 
-type PrivKey []byte
+type PrivKey struct {
+	sk *blst.SecretKey
+}
 
 // NewPrivateKeyFromBytes build a new key from the given bytes.
-func NewPrivateKeyFromBytes(bz []byte) (PrivKey, error) {
-	secretKey, err := bls12381.SecretKeyFromBytes(bz)
-	if err != nil {
-		return nil, err
+func NewPrivateKeyFromBytes(bz []byte) (*PrivKey, error) {
+	sk := new(blst.SecretKey).Deserialize(bz)
+	if sk == nil {
+		return nil, ErrDeserialization
 	}
-	return secretKey.Marshal(), nil
+	return &PrivKey{sk: sk}, nil
 }
 
 // GenPrivKey generates a new key.
-func GenPrivKey() (PrivKey, error) {
-	secretKey, err := bls12381.RandKey()
-	return PrivKey(secretKey.Marshal()), err
+func GenPrivKey() (*PrivKey, error) {
+	var ikm [32]byte
+	_, err := rand.Read(ikm[:])
+	if err != nil {
+		return nil, err
+	}
+	sk := blst.KeyGen(ikm[:])
+	return &PrivKey{sk: sk}, nil
 }
 
 // Bytes returns the byte representation of the Key.
 func (privKey PrivKey) Bytes() []byte {
-	return privKey
+	return privKey.sk.Serialize()
 }
 
 // PubKey returns the private key's public key. If the privkey is not valid
 // it returns a nil value.
 func (privKey PrivKey) PubKey() crypto.PubKey {
-	secretKey, err := bls12381.SecretKeyFromBytes(privKey)
-	if err != nil {
-		return nil
-	}
-
-	return PubKey(secretKey.PublicKey().Marshal())
+	return &PubKey{pk: new(blstPublicKey).From(privKey.sk)}
 }
 
 // Type returns the type.
@@ -75,18 +94,19 @@ func (PrivKey) Type() string {
 // Sign signs the given byte array. If msg is larger than
 // MaxMsgLen, SHA256 sum will be signed instead of the raw bytes.
 func (privKey PrivKey) Sign(msg []byte) ([]byte, error) {
-	secretKey, err := bls12381.SecretKeyFromBytes(privKey)
-	if err != nil {
-		return nil, err
-	}
-
 	if len(msg) > MaxMsgLen {
 		hash := sha256.Sum256(msg)
-		sig := secretKey.Sign(hash[:])
-		return sig.Marshal(), nil
+		signature := new(blstSignature).Sign(privKey.sk, hash[:], dstMinSig)
+		return signature.Compress(), nil
 	}
-	sig := secretKey.Sign(msg)
-	return sig.Marshal(), nil
+
+	signature := new(blstSignature).Sign(privKey.sk, msg, dstMinSig)
+	return signature.Compress(), nil
+}
+
+// Zeroize clears the private key.
+func (privKey *PrivKey) Zeroize() {
+	privKey.sk.Zeroize()
 }
 
 // ===============================================================================================
@@ -99,46 +119,50 @@ func (privKey PrivKey) Sign(msg []byte) ([]byte, error) {
 
 var _ crypto.PubKey = &PubKey{}
 
-type PubKey []byte
+type PubKey struct {
+	pk *blstPublicKey
+}
+
+// NewPublicKeyFromBytes returns a new public key from the given bytes.
+func NewPublicKeyFromBytes(bz []byte) (*PubKey, error) {
+	pk := new(blstPublicKey).Deserialize(bz)
+	if pk == nil {
+		return nil, ErrDeserialization
+	}
+	return &PubKey{pk: pk}, nil
+}
 
 // Address returns the address of the key.
 //
 // The function will panic if the public key is invalid.
 func (pubKey PubKey) Address() crypto.Address {
-	pk, _ := bls12381.PublicKeyFromBytes(pubKey)
-	if len(pk.Marshal()) != PubKeySize {
-		panic("pubkey is incorrect size")
-	}
-	return crypto.Address(tmhash.SumTruncated(pubKey))
+	return crypto.Address(tmhash.SumTruncated(pubKey.pk.Serialize()))
 }
 
 // VerifySignature verifies the given signature.
 func (pubKey PubKey) VerifySignature(msg, sig []byte) bool {
-	if len(sig) != SignatureLength {
+	signature := new(blstSignature).Uncompress(sig)
+	if signature == nil {
 		return false
 	}
 
-	pubK, err := bls12381.PublicKeyFromBytes(pubKey)
-	if err != nil { // invalid pubkey
+	// Group check signature. Do not check for infinity since an aggregated signature
+	// could be infinite.
+	if !signature.SigValidate(false) {
 		return false
 	}
 
 	if len(msg) > MaxMsgLen {
 		hash := sha256.Sum256(msg)
-		msg = hash[:]
+		return signature.Verify(false, pubKey.pk, false, hash[:], dstMinSig)
 	}
 
-	ok, err := bls12381.VerifySignature(sig, [MaxMsgLen]byte(msg[:MaxMsgLen]), pubK)
-	if err != nil { // bad signature
-		return false
-	}
-
-	return ok
+	return signature.Verify(false, pubKey.pk, false, msg, dstMinSig)
 }
 
 // Bytes returns the byte format.
 func (pubKey PubKey) Bytes() []byte {
-	return pubKey
+	return pubKey.pk.Serialize()
 }
 
 // Type returns the key's type.

crypto/bls12381/key_test.go
@@ -1,4 +1,4 @@
-//go:build ((linux && amd64) || (linux && arm64) || (darwin && amd64) || (darwin && arm64) || (windows && amd64)) && bls12381
+//go:build bls12381
 
 package bls12381_test
 
@@ -15,10 +15,12 @@ import (
 func TestNewPrivateKeyFromBytes(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 
 	privKeyBytes := privKey.Bytes()
 	privKey2, err := bls12381.NewPrivateKeyFromBytes(privKeyBytes)
 	require.NoError(t, err)
+	defer privKey2.Zeroize()
 
 	assert.Equal(t, privKey, privKey2)
 
@@ -29,16 +31,19 @@ func TestNewPrivateKeyFromBytes(t *testing.T) {
 func TestGenPrivateKey(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 	assert.NotNil(t, privKey)
 }
 
 func TestPrivKeyBytes(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 
 	privKeyBytes := privKey.Bytes()
 	privKey2, err := bls12381.NewPrivateKeyFromBytes(privKeyBytes)
 	require.NoError(t, err)
+	defer privKey2.Zeroize()
 
 	assert.Equal(t, privKey, privKey2)
 }
@@ -53,6 +58,7 @@ func TestPrivKeyPubKey(t *testing.T) {
 func TestPrivKeyType(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 
 	assert.Equal(t, "bls12_381", privKey.Type())
 }
@@ -60,6 +66,7 @@ func TestPrivKeyType(t *testing.T) {
 func TestPrivKeySignAndPubKeyVerifySignature(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 	pubKey := privKey.PubKey()
 
 	msg := crypto.CRandBytes(32)
@@ -83,17 +90,10 @@ func TestPrivKeySignAndPubKeyVerifySignature(t *testing.T) {
 	assert.True(t, pubKey.VerifySignature(msg, sig))
 }
 
-func TestPrivKeyPubKey_CorruptPrivKey(t *testing.T) {
-	privKey, err := bls12381.GenPrivKey()
-	require.NoError(t, err)
-	privKey = privKey[1:] // corrupt key
-	pubKey := privKey.PubKey()
-	require.Nil(t, pubKey)
-}
-
 func TestPubKey(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 	pubKey := privKey.PubKey()
 	assert.NotNil(t, pubKey)
 }
@@ -101,7 +101,23 @@ func TestPubKey(t *testing.T) {
 func TestPubKeyType(t *testing.T) {
 	privKey, err := bls12381.GenPrivKey()
 	require.NoError(t, err)
+	defer privKey.Zeroize()
 	pubKey := privKey.PubKey()
 
 	assert.Equal(t, "bls12_381", pubKey.Type())
+}
+
+func TestConst(t *testing.T) {
+	privKey, err := bls12381.GenPrivKey()
+	require.NoError(t, err)
+	defer privKey.Zeroize()
+	assert.Equal(t, bls12381.PrivKeySize, len(privKey.Bytes()))
+
+	pubKey := privKey.PubKey()
+	assert.Equal(t, bls12381.PubKeySize, len(pubKey.Bytes()))
+
+	msg := crypto.CRandBytes(32)
+	sig, err := privKey.Sign(msg)
+	require.NoError(t, err)
+	assert.Equal(t, bls12381.SignatureLength, len(sig))
 }

crypto/bls12381/const.go
@@ -4,7 +4,7 @@ const (
 	// PrivKeySize defines the length of the PrivKey byte array.
 	PrivKeySize = 32
 	// PubKeySize defines the length of the PubKey byte array.
-	PubKeySize = 48
+	PubKeySize = 96
 	// SignatureLength defines the byte length of a BLS signature.
 	SignatureLength = 96
 	// KeyType is the string constant for the BLS12-381 algorithm.

crypto/bls12381/key.go
@@ -1,4 +1,4 @@
-//go:build !(((linux && amd64) || (linux && arm64) || (darwin && amd64) || (darwin && arm64) || (windows && amd64)) && bls12381)
+//go:build !bls12381
 
 package bls12381
 
@@ -60,6 +60,11 @@ func (PrivKey) Sign([]byte) ([]byte, error) {
 	panic("bls12_381 is disabled")
 }
 
+// Zeroize always panics.
+func (PrivKey) Zeroize() {
+	panic("bls12_381 is disabled")
+}
+
 // ===============================================================================================
 // Public Key
 // ===============================================================================================
@@ -73,6 +78,11 @@ var _ crypto.PubKey = &PubKey{}
 
 // PubKey represents a BLS private key noop when blst is not set as a build flag and cgo is disabled.
 type PubKey []byte
+
+// NewPublicKeyFromBytes returns ErrDisabled.
+func NewPublicKeyFromBytes([]byte) (*PubKey, error) {
+	return nil, ErrDisabled
+}
 
 // Address always panics.
 func (PubKey) Address() crypto.Address {

crypto/encoding/codec.go
@@ -57,14 +57,14 @@ func PubKeyToProto(k crypto.PubKey) (pc.PublicKey, error) {
 				Secp256K1: k,
 			},
 		}
-	case bls12381.PubKey:
+	case *bls12381.PubKey:
 		if !bls12381.Enabled {
 			return kp, ErrUnsupportedKey{Key: k}
 		}
 
 		kp = pc.PublicKey{
 			Sum: &pc.PublicKey_Bls12381{
-				Bls12381: k,
+				Bls12381: k.Bytes(),
 			},
 		}
 	default:
@@ -110,9 +110,7 @@ func PubKeyFromProto(k pc.PublicKey) (crypto.PubKey, error) {
 				Want: bls12381.PubKeySize,
 			}
 		}
-		pk := make(bls12381.PubKey, bls12381.PubKeySize)
-		copy(pk, k.Bls12381)
-		return pk, nil
+		return bls12381.NewPublicKeyFromBytes(k.Bls12381)
 	default:
 		return nil, ErrUnsupportedKey{Key: k}
 	}
@@ -161,9 +159,7 @@ func PubKeyFromTypeAndBytes(pkType string, bytes []byte) (crypto.PubKey, error) 
 			}
 		}
 
-		pk := make(bls12381.PubKey, bls12381.PubKeySize)
-		copy(pk, bytes)
-		pubKey = pk
+		return bls12381.NewPublicKeyFromBytes(bytes)
 	default:
 		return nil, ErrUnsupportedKey{Key: pkType}
 	}

crypto/encoding/codec_test.go
@@ -34,6 +34,7 @@ func TestPubKeyToFromProto(t *testing.T) {
 	if bls12381.Enabled {
 		privKey, err := bls12381.GenPrivKey()
 		require.NoError(t, err)
+		defer privKey.Zeroize()
 		pk = privKey.PubKey()
 		proto, err := PubKeyToProto(pk)
 		require.NoError(t, err)

go.mod
@@ -42,7 +42,6 @@ require (
 	github.com/cometbft/cometbft-db v0.13.0
 	github.com/cometbft/cometbft-load-test v0.1.0
 	github.com/cometbft/cometbft/api v1.0.0-rc.1
-	github.com/cosmos/crypto v0.1.2
 	github.com/cosmos/gogoproto v1.5.0
 	github.com/dgraph-io/badger/v4 v4.2.0
 	github.com/go-git/go-git/v5 v5.12.0
@@ -52,6 +51,7 @@ require (
 	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/oasisprotocol/curve25519-voi v0.0.0-20220708102147-0a8a51822cae
 	github.com/pelletier/go-toml/v2 v2.2.2
+	github.com/supranational/blst v0.3.12
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
 	golang.org/x/sync v0.8.0
 	golang.org/x/text v0.16.0
@@ -133,7 +133,6 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	github.com/supranational/blst v0.3.12 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect

go.sum
@@ -92,8 +92,6 @@ github.com/cometbft/cometbft/api v1.0.0-rc.1 h1:GtdXwDGlqwHYs16A4egjwylfYOMYyEac
 github.com/cometbft/cometbft/api v1.0.0-rc.1/go.mod h1:NDFKiBBD8HJC6QQLAoUI99YhsiRZtg2+FJWfk6A6m6o=
 github.com/containerd/continuity v0.3.0 h1:nisirsYROK15TAMVukJOUyGJjz4BNQJBVsNvAXZJ/eg=
 github.com/containerd/continuity v0.3.0/go.mod h1:wJEAIwKOm/pBZuBd0JmeTvnLquTB1Ag8espWhkykbPM=
-github.com/cosmos/crypto v0.1.2 h1:Yn500sPY+9sKVdhiPUSDtt8JOpBGMB515dOmla4zfls=
-github.com/cosmos/crypto v0.1.2/go.mod h1:b6VWz3HczIpBaQPvI7KrbQeF3pXHh0al3T5e0uwMBQw=
 github.com/cosmos/gogoproto v1.5.0 h1:SDVwzEqZDDBoslaeZg+dGE55hdzHfgUA40pEanMh52o=
 github.com/cosmos/gogoproto v1.5.0/go.mod h1:iUM31aofn3ymidYG6bUR5ZFrk+Om8p5s754eMUcyp8I=
 github.com/cpuguy83/go-md2man/v2 v2.0.4/go.mod h1:tgQtvFlXSQOSOSIRvRPT7W67SCa46tRHOmNcaadrF8o=

commit a36d85fc7c768f26f116d53128120fd2f4eafd38

fix(bls12381): NewPublicKeyFromBytes: add missing infinity check (#4104)
Fixes a regression introduced in
https://github.com/cometbft/cometbft/pull/3603

---------

Co-authored-by: Daniel <daniel.cason@informal.systems>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

crypto/bls12381/key_bls12381.go
@@ -23,6 +23,9 @@ const (
 var (
 	// ErrDeserialization is returned when deserialization fails.
 	ErrDeserialization = errors.New("bls12381: deserialization error")
+	// ErrInfinitePubKey is returned when the public key is infinite. It is part
+	// of a more comprehensive subgroup check on the key.
+	ErrInfinitePubKey = errors.New("bls12381: pubkey is infinite")
 
 	dstMinSig = []byte("BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_NUL_")
 )
@@ -161,6 +164,10 @@ func NewPublicKeyFromBytes(bz []byte) (*PubKey, error) {
 	pk := new(blstPublicKey).Deserialize(bz)
 	if pk == nil {
 		return nil, ErrDeserialization
+	}
+	// Subgroup and infinity check
+	if !pk.KeyValidate() {
+		return nil, ErrInfinitePubKey
 	}
 	return &PubKey{pk: pk}, nil
 }

tests.mk
@@ -83,4 +83,4 @@ split-test-packages:$(BUILDDIR)/packages.txt
 # Used by the GitHub CI, in order to run tests in parallel
 test-group-%:split-test-packages
 	cat $(BUILDDIR)/packages.txt.$*
-	cat $(BUILDDIR)/packages.txt.$* | xargs go test -mod=readonly -timeout=400s -race -coverprofile=$(BUILDDIR)/$*.profile.out
+	cat $(BUILDDIR)/packages.txt.$* | xargs go test -tags bls12381 -mod=readonly -timeout=400s -race -coverprofile=$(BUILDDIR)/$*.profile.out

crypto/bls12381/key_test.go
@@ -3,10 +3,13 @@
 package bls12381_test
 
 import (
+	"encoding/hex"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	blst "github.com/supranational/blst/bindings/go"
 
 	"github.com/cometbft/cometbft/crypto"
 	"github.com/cometbft/cometbft/crypto/bls12381"
@@ -166,4 +169,48 @@ func TestPubKey_MarshalJSON(t *testing.T) {
 	pubKey2 := new(bls12381.PubKey)
 	err = pubKey2.UnmarshalJSON(jsonBytes)
 	require.NoError(t, err)
+}
+
+func TestPubKey_NewPublicKeyFromInvalidBytes(t *testing.T) {
+	unmarshal := func(s string) ([]byte, error) {
+		type blstPublicKey = blst.P1Affine
+
+		bz, err := hex.DecodeString(s)
+		if err != nil {
+			return nil, err
+		}
+		pk := new(blstPublicKey).Uncompress(bz)
+		if pk == nil {
+			return nil, bls12381.ErrDeserialization
+		}
+		pkc := pk.Serialize()
+		if pkc == nil {
+			return nil, errors.New("could not serialize pubkey")
+		}
+		return pkc, nil
+	}
+
+	testCases := []struct {
+		desc        string
+		pkStr       string
+		expectedErr error
+	}{
+		{"NotInG1", "8123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef", bls12381.ErrInfinitePubKey},
+		{"InfFalseB", "800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", bls12381.ErrDeserialization},
+		{"InfTrueB", "c01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", bls12381.ErrDeserialization},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			bz, err := unmarshal(tc.pkStr)
+			if err != nil {
+				t.Log(tc.desc, "unmarshal error", err)
+				require.Equal(t, tc.expectedErr, err)
+			}
+
+			_, err = bls12381.NewPublicKeyFromBytes(bz)
+			require.Equal(t, tc.expectedErr, err)
+			t.Log(tc.desc, "NewPrivateKeyFromBytes error", err)
+		})
+	}
 }
0c7821941b1a6dd01b584cc7d75cb46217bf7513
hvanz <hernan.vanzetto@gmail.com>
2024-09-24 12:34:39 +0200

    panic if lane does not exist

--- mempool/clist_mempool.go
+++ mempool/clist_mempool.go
@@ -291,7 +291,7 @@ func (mem *CListMempool) LaneSizes(lane LaneID) (numTxs int, bytes int64) {
 
 	txs, ok := mem.lanes[lane]
 	if !ok {
-		return 0, bytes
+		panic(fmt.Errorf("lane %s not found while fetching lane size", lane))
 	}
 	return txs.Len(), bytes
 }
@@ -450,15 +450,14 @@ func (mem *CListMempool) handleCheckTxResponse(tx types.Tx, sender p2p.ID) func(
 		}
 
 		// Add tx to mempool and notify that new txs are available.
-		if mem.addTx(tx, res.GasWanted, sender, lane) {
-			mem.notifyTxsAvailable()
+		mem.addTx(tx, res.GasWanted, sender, lane)
+		mem.notifyTxsAvailable()
 
-			if mem.onNewTx != nil {
-				mem.onNewTx(tx)
-			}
-
-			mem.updateSizeMetrics(lane)
+		if mem.onNewTx != nil {
+			mem.onNewTx(tx)
 		}
+
+		mem.updateSizeMetrics(lane)
 
 		return nil
 	}
@@ -466,15 +465,14 @@ func (mem *CListMempool) handleCheckTxResponse(tx types.Tx, sender p2p.ID) func(
 
 // Called from:
 //   - handleCheckTxResponse (lock not held) if tx is valid
-func (mem *CListMempool) addTx(tx types.Tx, gasWanted int64, sender p2p.ID, lane LaneID) bool {
+func (mem *CListMempool) addTx(tx types.Tx, gasWanted int64, sender p2p.ID, lane LaneID) {
 	mem.txsMtx.Lock()
 	defer mem.txsMtx.Unlock()
 
 	// Get lane's clist.
 	txs, ok := mem.lanes[lane]
 	if !ok {
-		mem.logger.Error("Lane does not exist, not adding TX", "tx", log.NewLazySprintf("%X", tx.Hash()), "lane", lane)
-		return false
+		panic(fmt.Errorf("lane %s not found while adding tx %X", lane, log.NewLazySprintf("%X", tx.Hash())))
 	}
 
 	// Increase sequence number.
@@ -514,7 +512,6 @@ func (mem *CListMempool) addTx(tx types.Tx, gasWanted int64, sender p2p.ID, lane
 		"height", mem.height.Load(),
 		"total", mem.numTxs,
 	)
-	return true
 }
 
 // RemoveTxByKey removes a transaction from the mempool by its TxKey index.

75e0d70d09fe36e31f554c97335615fe42cb96aa
hvanz <hernan.vanzetto@gmail.com>
2024-10-06 00:23:11 +0200

    Add GetSenders to mempool interface and Senders to Entry interface

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -104,6 +104,9 @@ type Mempool interface {
 
 	// SizeBytes returns the total size of all txs in the mempool.
 	SizeBytes() int64
+
+	// GetSenders returns the list of node IDs from which we receive the given transaction.
+	GetSenders(txKey types.TxKey) ([]p2p.ID, error)
 }
 
 // PreCheckFunc is an optional filter executed before CheckTx and rejects
@@ -166,6 +169,9 @@ type Entry interface {
 
 	// IsSender returns whether we received the transaction from the given peer ID.
 	IsSender(peerID p2p.ID) bool
+
+	// Senders returns the list of registered peers that sent us the transaction.
+	Senders() []p2p.ID
 }
 
 // An iterator is used to iterate through the mempool entries.

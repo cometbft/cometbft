b2869181a08b7246f104a3510db2f5997c620469
xujiangyu <xu15701157125@163.com>
2024-10-17 18:15:36 +0800

    docs: comment on docs

--- node/doc.go
+++ node/doc.go
@@ -1,39 +1,37 @@
-/*
-Package node is the main entry point, where the Node struct, which
-represents a full node, is defined.
+// Package node is the main entry point, where the Node struct, which
+// represents a full node, is defined.
 
-Adding new p2p.Reactor(s)
+// Adding new p2p.Reactor(s)
 
-To add a new p2p.Reactor, use the CustomReactors option:
+// To add a new p2p.Reactor, use the CustomReactors option:
 
-	node, err := NewNode(
-			config,
-			privVal,
-			nodeKey,
-			clientCreator,
-			genesisDocProvider,
-			dbProvider,
-			metricsProvider,
-			logger,
-			CustomReactors(map[string]p2p.Reactor{"CUSTOM": customReactor}),
-	)
+// 	node, err := NewNode(
+// 			config,
+// 			privVal,
+// 			nodeKey,
+// 			clientCreator,
+// 			genesisDocProvider,
+// 			dbProvider,
+// 			metricsProvider,
+// 			logger,
+// 			CustomReactors(map[string]p2p.Reactor{"CUSTOM": customReactor}),
+// 	)
 
-Replacing existing p2p.Reactor(s)
+// Replacing existing p2p.Reactor(s)
 
-To replace the built-in p2p.Reactor, use the CustomReactors option:
+// To replace the built-in p2p.Reactor, use the CustomReactors option:
 
-	node, err := NewNode(
-			config,
-			privVal,
-			nodeKey,
-			clientCreator,
-			genesisDocProvider,
-			dbProvider,
-			metricsProvider,
-			logger,
-			CustomReactors(map[string]p2p.Reactor{"BLOCKSYNC": customBlocksyncReactor}),
-	)
+// 	node, err := NewNode(
+// 			config,
+// 			privVal,
+// 			nodeKey,
+// 			clientCreator,
+// 			genesisDocProvider,
+// 			dbProvider,
+// 			metricsProvider,
+// 			logger,
+// 			CustomReactors(map[string]p2p.Reactor{"BLOCKSYNC": customBlocksyncReactor}),
+// 	)
 
-The list of existing reactors can be found in CustomReactors documentation.
-*/
+// The list of existing reactors can be found in CustomReactors documentation.
 package node

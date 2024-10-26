38530f05881e4f2c6e34b1900fcbf092c707901c
wesl-ee <w@wesl.ee>
2024-10-26 02:58:51 -0400

    fix(config): do not populate deprecated timeout_commit with non-zero value (#4339)
    Closes #4337

--- config/config.go
+++ config/config.go
@@ -1195,7 +1195,7 @@ func DefaultConsensusConfig() *ConsensusConfig {
 		TimeoutPrevoteDelta:              500 * time.Millisecond,
 		TimeoutPrecommit:                 1000 * time.Millisecond,
 		TimeoutPrecommitDelta:            500 * time.Millisecond,
-		TimeoutCommit:                    1000 * time.Millisecond,
+		TimeoutCommit:                    0 * time.Millisecond,
 		SkipTimeoutCommit:                false,
 		CreateEmptyBlocks:                true,
 		CreateEmptyBlocksInterval:        0 * time.Second,

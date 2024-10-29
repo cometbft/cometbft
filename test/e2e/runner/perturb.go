e7785dc43d9bae08d754396e24161d6f87797a19
Andy Nogueira <me@andynogueira.dev>
2024-10-29 11:01:30 -0400

    test(e2e): give the runner extra time to wait for a killed node (#4351)
    One reason some e2e tests are failing is that the kill perturbation has
    a short timeout value of 20 seconds, causing waitForNode to time out.
    Doubling this timeout improves test stability and helps ensure the tests
    pass
    
    [test
    failure](https://github.com/cometbft/cometbft/actions/runs/11546210646/job/32134226559)
    
    #### PR checklist
    
    - [ ] Tests written/updated
    - [x] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit e03af86de54c7dc834bfccce04d2199e40d15e13)
    
    # Conflicts:
    #	.changelog/v0.38.8/improvements/4351-e2e-kill-perturbation-timeout.md
    #	test/e2e/runner/perturb.go

--- test/e2e/runner/perturb.go
+++ test/e2e/runner/perturb.go
@@ -62,6 +62,16 @@ func PerturbNode(ctx context.Context, node *e2e.Node, perturbation e2e.Perturbat
 		if err := docker.ExecCompose(context.Background(), testnet.Dir, "start", name); err != nil {
 			return nil, err
 		}
+<<<<<<< HEAD
+=======
+		if node.PersistInterval == 0 {
+			timeout *= 5
+		} else {
+			// still need to give some extra time to the runner
+			// to wait for the node to restart when killing
+			timeout *= 2
+		}
+>>>>>>> e03af86de (test(e2e): give the runner extra time to wait for a killed node (#4351))
 
 	case e2e.PerturbationPause:
 		logger.Info("perturb node", "msg", log.NewLazySprintf("Pausing node %v...", node.Name))

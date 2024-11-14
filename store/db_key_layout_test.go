d5b074538346c0ac882a7f690f9eda84ab4e6693
Alessandro <alessandro@informal.systems>
2024-11-14 18:22:33 +0100

    Added fuzz test for `CalcBlockHashKey()`.

--- store/db_key_layout_test.go
+++ store/db_key_layout_test.go
@@ -2,18 +2,21 @@ package store
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"strconv"
 	"testing"
 )
 
+// run: go test -fuzz=FuzzCalcBlockPartKey -fuzztime 30s
 func FuzzCalcBlockPartKey(f *testing.F) {
 	layout := &v1LegacyLayout{}
 
 	f.Add(int64(0), 0)
-	f.Add(int64(1234567890123456789), 1)
-	f.Add(int64(-9223372036854775808), -1)
+	f.Add(int64(141241), 980)
+	f.Add(int64(1234567890), 12345678901)
 	f.Add(int64(9223372036854775807), 2147483647)
-	f.Add(int64(0), -2147483648)
+	f.Add(int64(42), 2147483648)
 
 	f.Fuzz(func(t *testing.T, height int64, partIndex int) {
 		key := layout.CalcBlockPartKey(height, partIndex)
@@ -45,6 +48,42 @@ func FuzzCalcBlockPartKey(f *testing.F) {
 		}
 		if gotPartIndex != partIndex {
 			t.Errorf("want partIndex %d, but got %d", partIndex, gotPartIndex)
+		}
+	})
+}
+
+// run: go test -fuzz=FuzzCalcBlockHashKey -fuzztime 30s
+func FuzzCalcBlockHashKey(f *testing.F) {
+	layout := &v1LegacyLayout{}
+
+	f.Add([]byte{})
+	f.Add([]byte{0x00})
+	f.Add([]byte{0xFF})
+	f.Add([]byte{0x01, 0x02, 0x03, 0x04})
+	f.Add(make([]byte, 32))
+
+	// empty hash: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+	f.Add(sha256.New().Sum(nil))
+
+	f.Fuzz(func(t *testing.T, hash []byte) {
+		key := layout.CalcBlockHashKey(hash)
+
+		if len(key) < 3 || key[0] != 'B' || key[1] != 'H' || key[2] != ':' {
+			t.Fatalf("key does not start with 'BH:': %s", key)
+		}
+
+		var (
+			hashHex = key[3:]
+			gotHash = make([]byte, len(hash))
+		)
+		_, err := hex.Decode(gotHash, hashHex)
+		if err != nil {
+			t.Fatalf("decoding hash from key: %s, error: %s", key, err)
+		}
+
+		// Ensure the decoded hash matches the input hash.
+		if !bytes.Equal(gotHash, hash) {
+			t.Fatalf("want hash %x\ngot: %x\n", hash, gotHash)
 		}
 	})
 }

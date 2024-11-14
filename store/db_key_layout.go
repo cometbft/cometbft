3a73693dc7a93d71057cd1f512cdee10af228f27
Alessandro <alessandro@informal.systems>
2024-11-14 15:43:37 +0100

    improved docs

--- store/db_key_layout.go
+++ store/db_key_layout.go
@@ -21,14 +21,23 @@ type BlockKeyLayout interface {
 	CalcBlockHashKey(hash []byte) []byte
 }
 
+// v1LegacyLayout is a legacy implementation of BlockKeyLayout, kept for backwards
+// compatibility. Newer code should use [v2Layout].
 type v1LegacyLayout struct{}
 
+// In the following [v1LegacyLayout] methods, we preallocate the key's slice to speed
+// up append operations and avoid extra allocations.
+// The size of the slice is the length of the prefix plus the length the string
+// representation of a 64-bit integer. Namely, the longest 64-bit int has 19 digits,
+// therefore its string representation is 20 bytes long (19 digits + 1 byte for the
+// sign).
+
 // CalcBlockCommitKey implements BlockKeyLayout.
+// It returns a database key of the form "C:<height>" to store/retrieve the commit
+// of the block at the given height to/from the database.
 func (*v1LegacyLayout) CalcBlockCommitKey(height int64) []byte {
 	const prefixLen = len("C:")
 
-	// The longest int64 has 19 digits, therefore its string representation is
-	// 20 bytes long (19 digits + 1 byte for the sign).
 	key := make([]byte, prefixLen, prefixLen+20)
 
 	key[0], key[1] = 'C', ':'
@@ -38,6 +47,8 @@ func (*v1LegacyLayout) CalcBlockCommitKey(height int64) []byte {
 }
 
 // CalcBlockHashKey implements BlockKeyLayout.
+// It returns a database key of the form "BH:hex(<hash>)" to store/retrieve a block
+// to/from the database using its hash.
 func (*v1LegacyLayout) CalcBlockHashKey(hash []byte) []byte {
 	const prefixLen = len("BH:")
 
@@ -50,12 +61,11 @@ func (*v1LegacyLayout) CalcBlockHashKey(hash []byte) []byte {
 }
 
 // CalcBlockMetaKey implements BlockKeyLayout.
+// It returns a database key of the form "H:<height>" to store/retrieve the metadata
+// of the block at the given height to/from the database.
 func (*v1LegacyLayout) CalcBlockMetaKey(height int64) []byte {
 	const prefixLen = len("H:")
 
-	// the longest int64 has 19 digits, therefore its string representation is
-	// 20 bytes long (19 digits + 1 byte for the sign).
-	// 2 is the length of "H:"
 	key := make([]byte, prefixLen, prefixLen+20)
 
 	key[0], key[1] = 'H', ':'
@@ -65,15 +75,13 @@ func (*v1LegacyLayout) CalcBlockMetaKey(height int64) []byte {
 }
 
 // CalcBlockPartKey implements BlockKeyLayout.
+// It returns a database key of the form "P:<height>:<partIndex>" to store/retrieve a
+// block part to/from the database.
 func (*v1LegacyLayout) CalcBlockPartKey(height int64, partIndex int) []byte {
 	const prefixLen = len("P:")
 
-	// Preallocate the slice to speed up append operations and avoid extra
-	// allocations.
-	// The longest int64 has 19 digits, therefore its string representation is
-	// 20 bytes long (19 digits + 1 byte for the sign). Here we have 2 ints,
-	// therefore 20+20 bytes.
-	// The total size is is: 2 (prefixLen) + 20 + 1 (len(":")) + 20
+	// Here we have 2 ints, therefore 20+20 bytes.
+	// The total size is : 2 (prefixLen) + 20 + 1 (len(":")) + 20
 	key := make([]byte, prefixLen, prefixLen+20+1+20)
 
 	key[0], key[1] = 'P', ':'
@@ -85,11 +93,11 @@ func (*v1LegacyLayout) CalcBlockPartKey(height int64, partIndex int) []byte {
 }
 
 // CalcExtCommitKey implements BlockKeyLayout.
+// It returns a database key of the form "EC:<height>" to store/retrieve the
+// ExtendedCommit for the given height to/from the database.
 func (*v1LegacyLayout) CalcExtCommitKey(height int64) []byte {
 	const prefixLen = len("EC:")
 
-	// the longest int64 has 19 digits, therefore its string representation is
-	// 20 bytes long (19 digits + 1 byte for the sign).
 	key := make([]byte, prefixLen, prefixLen+20)
 
 	key[0], key[1], key[2] = 'E', 'C', ':'
@@ -99,12 +107,11 @@ func (*v1LegacyLayout) CalcExtCommitKey(height int64) []byte {
 }
 
 // CalcSeenCommitKey implements BlockKeyLayout.
+// It returns a database key of the form "SC:<height>" to store/retrieve a locally
+// seen commit for the given height to/from the database.
 func (*v1LegacyLayout) CalcSeenCommitKey(height int64) []byte {
 	const prefixLen = len("SC:")
 
-	// the longest int64 has 19 digits, therefore its string representation is
-	// 20 bytes long (19 digits + 1 byte for the sign).
-	// 3 is the length of "SC:"
 	key := make([]byte, prefixLen, prefixLen+20)
 
 	key[0], key[1], key[2] = 'S', 'C', ':'

cometbft/cometbft

commit daf0234dabd15c89e790c8c724fa6185c8973ecd

feat(e2e): introduce oscillations for validators and consensus params (#2283)
Partially addresses #2458

This PR adds a new config to the `e2e` manifest that causes the `e2e`
app to flip, at every height:

* The consensus param, by flipping between to valid lists of key types
* The validator set, by picking one validator, and flipping its power
between 0 and 1 in alternate heights

This allows us to simulate chains with changes to these data structures
at every height.

IMPORTANT: Do _not_ review commit by commit, as I radically changed the
approach to flipping validators (to another way more surgical)

---

#### PR checklist

- [x] Tests written/updated
- ~[ ] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)~
- [x] Updated relevant documentation (`docs/` or `spec/`) and code
comments

---------

Co-authored-by: Alessandro <alessandro@informal.systems>
Co-authored-by: Daniel Cason <daniel.cason@informal.systems>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
Co-authored-by: Hernán Vanzetto <15466498+hvanz@users.noreply.github.com>

test/e2e/generator/generate.go
@@ -73,6 +73,7 @@ var (
 	pbtsEnabled                = weightedChoice{true: 3, false: 1}
 	pbtsHeightOffset           = uniformChoice{int64(0), int64(10), int64(100)}
 	keyType                    = uniformChoice{ed25519.KeyType, secp256k1.KeyType, bls12381.KeyType}
+	constantFlip               = uniformChoice{true, false}
 )
 
 type generateConfig struct {
@@ -138,7 +139,7 @@ func generateTestnet(r *rand.Rand, opt map[string]any, upgradeVersion string, pr
 		ABCIProtocol:        nodeABCIProtocols.Choose(r).(string),
 		InitialHeight:       int64(opt["initialHeight"].(int)),
 		InitialState:        opt["initialState"].(map[string]string),
-		Validators:          &map[string]int64{},
+		Validators:          map[string]int64{},
 		ValidatorUpdatesMap: map[string]map[string]int64{},
 		KeyType:             keyType.Choose(r).(string),
 		Evidence:            evidence.Choose(r).(int),
@@ -172,6 +173,7 @@ func generateTestnet(r *rand.Rand, opt map[string]any, upgradeVersion string, pr
 	}
 
 	manifest.VoteExtensionSize = voteExtensionSize.Choose(r).(uint)
+	manifest.ConstantFlip = constantFlip.Choose(r).(bool)
 
 	manifest.PbtsUpdateHeight = pbtsUpdateHeight.Choose(r).(int64)
 	if manifest.PbtsUpdateHeight == 1 {
@@ -222,7 +224,7 @@ func generateTestnet(r *rand.Rand, opt map[string]any, upgradeVersion string, pr
 
 		weight := int64(30 + r.Intn(71))
 		if startAt == 0 {
-			(*manifest.Validators)[name] = weight
+			manifest.Validators[name] = weight
 		} else {
 			manifest.ValidatorUpdatesMap[strconv.FormatInt(startAt+5, 10)] = map[string]int64{name: weight}
 		}
@@ -236,7 +238,7 @@ func generateTestnet(r *rand.Rand, opt map[string]any, upgradeVersion string, pr
 		startAt := manifest.NodesMap[name].StartAt
 		var weight int64
 		if startAt == 0 {
-			weight = (*manifest.Validators)[name]
+			weight = manifest.Validators[name]
 		} else {
 			weight = manifest.ValidatorUpdatesMap[strconv.FormatInt(startAt+5, 10)][name]
 		}
@@ -252,8 +254,8 @@ func generateTestnet(r *rand.Rand, opt map[string]any, upgradeVersion string, pr
 	switch opt["validators"].(string) {
 	case "genesis":
 	case "initchain":
-		manifest.ValidatorUpdatesMap["0"] = *manifest.Validators
-		manifest.Validators = &map[string]int64{}
+		manifest.ValidatorUpdatesMap["0"] = manifest.Validators
+		manifest.Validators = map[string]int64{}
 	default:
 		return manifest, fmt.Errorf("invalid validators option %q", opt["validators"])
 	}

test/e2e/pkg/testnet.go
@@ -318,17 +318,21 @@ func NewTestnetFromManifest(manifest Manifest, file string, ifd InfrastructureDa
 	}
 
 	// Set up genesis validators. If not specified explicitly, use all validator nodes.
-	if manifest.Validators == nil {
-		validatorsMap := make(map[string]int64)
+	if len(testnet.Validators) == 0 {
+		if testnet.Validators == nil { // Can this ever happen?
+			testnet.Validators = make(map[string]int64)
+		}
 		for _, node := range testnet.Nodes {
 			if node.Mode == ModeValidator {
-				validatorsMap[node.Name] = 100
+				testnet.Validators[node.Name] = 100
 			}
 		}
-		manifest.Validators = &validatorsMap
 	}
 
 	// Set up validator updates.
+	// NOTE: This map traversal is non-deterministic, but that's acceptable because
+	// the loop only constructs another map.
+	// We don't rely on traversal order for any side effects.
 	for heightStr, validators := range manifest.ValidatorUpdatesMap {
 		height, err := strconv.Atoi(heightStr)
 		if err != nil {
@@ -343,6 +347,30 @@ func NewTestnetFromManifest(manifest Manifest, file string, ifd InfrastructureDa
 			valUpdate[node.Name] = power
 		}
 		testnet.ValidatorUpdates[int64(height)] = valUpdate
+	}
+
+	if testnet.ConstantFlip {
+		// Pick "lowest" validator by name
+		var minNode string
+		for n := range testnet.Validators {
+			if len(minNode) == 0 || n < minNode {
+				minNode = n
+			}
+		}
+		if len(minNode) == 0 {
+			return nil, errors.New("`testnet.Validators` is empty")
+		}
+
+		const flipSpan = 3000
+		for i := max(1, manifest.InitialHeight); i < manifest.InitialHeight+flipSpan; i++ {
+			if _, ok := testnet.ValidatorUpdates[i]; ok {
+				continue
+			}
+			valUpdate := map[string]int64{
+				minNode: i % 2, // flipping every height
+			}
+			testnet.ValidatorUpdates[i] = valUpdate
+		}
 	}
 
 	return testnet, testnet.Validate()

test/e2e/runner/setup.go
@@ -161,7 +161,7 @@ func MakeGenesis(testnet *e2e.Testnet) (types.GenesisDoc, error) {
 	if testnet.PbtsUpdateHeight == -1 {
 		genesis.ConsensusParams.Feature.PbtsEnableHeight = testnet.PbtsEnableHeight
 	}
-	for valName, power := range *testnet.Manifest.Validators {
+	for valName, power := range testnet.Validators {
 		validator := testnet.LookupNode(valName)
 		if validator == nil {
 			return types.GenesisDoc{}, fmt.Errorf("unknown validator %q for genesis doc", valName)
@@ -410,6 +410,7 @@ func MakeAppConfig(node *e2e.Node) ([]byte, error) {
 		"pbts_update_height":            node.Testnet.PbtsUpdateHeight,
 		"no_lanes":                      node.Testnet.Manifest.NoLanes,
 		"lanes":                         node.Testnet.Manifest.Lanes,
+		"constant_flip":                 node.Testnet.ConstantFlip,
 	}
 	switch node.ABCIProtocol {
 	case e2e.ProtocolUNIX:

test/e2e/app/app.go
@@ -12,6 +12,7 @@ import (
 	"math/big"
 	"os"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 	"sync"
@@ -38,6 +39,7 @@ const (
 	voteExtensionKey    string = "extensionSum"
 	voteExtensionMaxVal int64  = 128
 	prefixReservedKey   string = "reservedTxKey_"
+	prefixValidator     string = "Validator_"
 	suffixChainID       string = "ChainID"
 	suffixVoteExtHeight string = "VoteExtensionsHeight"
 	suffixPbtsHeight    string = "PbtsHeight"
@@ -57,7 +59,8 @@ type Application struct {
 	restoreSnapshot *abci.Snapshot
 	restoreChunks   [][]byte
 	// It's OK not to persist this, as it is not part of the state machine
-	seenTxs sync.Map // cmttypes.TxKey -> uint64
+	seenTxs     sync.Map // cmttypes.TxKey -> uint64
+	allKeyTypes []string // Cached slice of all supported key types in CometBFT
 
 	lanePriorities map[string]uint32
 }
@@ -146,6 +149,13 @@ type Config struct {
 	// Mapping from lane IDs to lane priorities. These lanes will be used by the
 	// application for setting up the mempool and for classifying transactions.
 	Lanes map[string]uint32 `toml:"lanes"`
+
+	// If true, the application will return validator updates and
+	// `ConsensusParams` updates at every height.
+	// This is useful to create a more dynamic testnet.
+	// * An existing validator will be chosen, and its power will alternate 0 and 1
+	// * `ConsensusParams` will be flipping on and off key types not set at genesis
+	ConstantFlip bool `toml:"constant_flip"`
 }
 
 func DefaultConfig(dir string) *Config {
@@ -177,14 +187,19 @@ func NewApplication(cfg *Config) (*Application, error) {
 	if err != nil {
 		return nil, err
 	}
+	allKeyTypes := make([]string, 0, len(cmttypes.ABCIPubKeyTypesToNames))
+	for keyType := range cmttypes.ABCIPubKeyTypesToNames {
+		allKeyTypes = append(allKeyTypes, keyType)
+	}
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 	logger.Info("Application started!")
 	if cfg.NoLanes {
 		return &Application{
-			logger:    logger,
-			state:     state,
-			snapshots: snapshots,
-			cfg:       cfg,
+			logger:      logger,
+			state:       state,
+			snapshots:   snapshots,
+			cfg:         cfg,
+			allKeyTypes: allKeyTypes,
 		}, nil
 	}
 
@@ -194,6 +209,7 @@ func NewApplication(cfg *Config) (*Application, error) {
 		snapshots:      snapshots,
 		cfg:            cfg,
 		lanePriorities: cfg.Lanes,
+		allKeyTypes:    allKeyTypes,
 	}, nil
 }
 
@@ -262,6 +278,28 @@ func (app *Application) updateFeatureEnableHeights(currentHeight int64) *cmtprot
 	return params
 }
 
+func (app *Application) flipConsensusParams(params *cmtproto.ConsensusParams, height int64) (*cmtproto.ConsensusParams, error) {
+	if !app.cfg.ConstantFlip {
+		return params, nil
+	}
+	if height < 0 {
+		return nil, fmt.Errorf("cannot flip ConsensusParams on height < 0 (%d)", height)
+	}
+
+	keyTypes := app.allKeyTypes
+	if height%2 == 0 {
+		keyTypes = []string{app.cfg.KeyType}
+	}
+	if params == nil {
+		params = &cmtproto.ConsensusParams{}
+	}
+	params.Validator = &cmtproto.ValidatorParams{
+		PubKeyTypes: keyTypes,
+	}
+	app.logger.Info("flipping key types", "PubKeyTypes", keyTypes)
+	return params, nil
+}
+
 // Info implements ABCI.
 func (app *Application) InitChain(_ context.Context, req *abci.InitChainRequest) (*abci.InitChainResponse, error) {
 	r := &abci.Request{Value: &abci.Request_InitChain{InitChain: &abci.InitChainRequest{}}}
@@ -296,6 +334,9 @@ func (app *Application) InitChain(_ context.Context, req *abci.InitChainRequest)
 	}
 
 	params := app.updateFeatureEnableHeights(0)
+	if params, err = app.flipConsensusParams(params, 0); err != nil {
+		return nil, err
+	}
 
 	resp := &abci.InitChainResponse{
 		ConsensusParams: params,
@@ -404,10 +445,13 @@ func (app *Application) FinalizeBlock(_ context.Context, req *abci.FinalizeBlock
 
 	valUpdates, err := app.validatorUpdates(uint64(req.Height))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	params := app.updateFeatureEnableHeights(req.Height)
+	if params, err = app.flipConsensusParams(params, req.Height); err != nil {
+		return nil, err
+	}
 
 	if app.cfg.FinalizeBlockDelay != 0 {
 		time.Sleep(app.cfg.FinalizeBlockDelay)
@@ -428,7 +472,7 @@ func (app *Application) FinalizeBlock(_ context.Context, req *abci.FinalizeBlock
 					},
 					{
 						Key:   "height",
-						Value: strconv.Itoa(int(req.Height)),
+						Value: strconv.FormatInt(req.Height, 10),
 					},
 				},
 			},
@@ -818,8 +862,8 @@ func (app *Application) storeValidator(valUpdate *abci.ValidatorUpdate) error {
 	if err != nil {
 		return err
 	}
-	addr := pubKey.Address().String()
 	if valUpdate.Power > 0 {
+		addr := pubKey.Address().String()
 		app.logger.Info("setting validator in app_state", "addr", addr)
 		pk, err := cryptoenc.PubKeyToProto(pubKey)
 		if err != nil {
@@ -829,31 +873,50 @@ func (app *Application) storeValidator(valUpdate *abci.ValidatorUpdate) error {
 		if err != nil {
 			return fmt.Errorf("failed to marshal pubkey: %w", err)
 		}
-		app.state.Set(prefixReservedKey+addr, hex.EncodeToString(pubKeyBytes))
+		app.state.Set(prefixReservedKey+prefixValidator+addr, hex.EncodeToString(pubKeyBytes))
 	}
 	return nil
 }
 
 // validatorUpdates generates a validator set update.
 func (app *Application) validatorUpdates(height uint64) (abci.ValidatorUpdates, error) {
+	// updates is map[string]uint8 of the form "validator_name" => voting_power
 	updates := app.cfg.ValidatorUpdates[strconv.FormatUint(height, 10)]
 	if len(updates) == 0 {
 		return nil, nil
 	}
 
-	valUpdates := abci.ValidatorUpdates{}
-	for keyString, power := range updates {
-		keyBytes, err := base64.StdEncoding.DecodeString(keyString)
+	// Collect the validator names into a slice and sort it to ensure deterministic
+	// iteration when creating the ValidatorUpdates below, since map traversal is
+	// non-deterministic.
+	validatorsNames := make([]string, 0, len(updates))
+	for validatorName := range updates {
+		validatorsNames = append(validatorsNames, validatorName)
+	}
+	slices.Sort(validatorsNames)
+
+	validatorsUpdates := make(abci.ValidatorUpdates, len(updates))
+	for i, validatorName := range validatorsNames {
+		power := updates[validatorName]
+
+		keyBytes, err := base64.StdEncoding.DecodeString(validatorName)
 		if err != nil {
-			return nil, fmt.Errorf("invalid base64 pubkey value %q: %w", keyString, err)
+			formatStr := "invalid base64 pubkey value %q: %w"
+			return nil, fmt.Errorf(formatStr, validatorName, err)
 		}
-		valUpdate := abci.ValidatorUpdate{Power: int64(power), PubKeyType: app.cfg.KeyType, PubKeyBytes: keyBytes}
-		valUpdates = append(valUpdates, valUpdate)
-		if err := app.storeValidator(&valUpdate); err != nil {
+
+		validatorUpdate := abci.ValidatorUpdate{
+			Power:       int64(power),
+			PubKeyType:  app.cfg.KeyType,
+			PubKeyBytes: keyBytes,
+		}
+
+		validatorsUpdates[i] = validatorUpdate
+		if err := app.storeValidator(&validatorUpdate); err != nil {
 			return nil, err
 		}
 	}
-	return valUpdates, nil
+	return validatorsUpdates, nil
 }
 
 // logAbciRequest log the request using the app's logger.
@@ -867,6 +930,28 @@ func (app *Application) logABCIRequest(req *abci.Request) error {
 	}
 	app.logger.Info(s)
 	return nil
+}
+
+func (app *Application) loadPubKey(addr string) (crypto.PubKey, error) {
+	pubKeyHex := app.state.Get(prefixReservedKey + prefixValidator + addr)
+	if len(pubKeyHex) == 0 {
+		return nil, fmt.Errorf("unknown validator with address %q", addr)
+	}
+	pubKeyBytes, err := hex.DecodeString(pubKeyHex)
+	if err != nil {
+		return nil, fmt.Errorf("could not hex-decode public key for validator address %s, err %w", addr, err)
+	}
+	var pubKeyProto cryptoproto.PublicKey
+	err = pubKeyProto.Unmarshal(pubKeyBytes)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal public key for validator address %s, err %w", addr, err)
+	}
+	pubKey, err := cryptoenc.PubKeyFromProto(pubKeyProto)
+	if err != nil {
+		return nil, fmt.Errorf("could not obtain a public key from its proto for validator address %s, err %w", addr, err)
+	}
+
+	return pubKey, nil
 }
 
 // parseTx parses a tx in 'key=value' format into a key and value.
@@ -943,22 +1028,9 @@ func (app *Application) verifyAndSum(
 
 		// ... and verify
 		valAddr := crypto.Address(vote.Validator.Address).String()
-		pubKeyHex := app.state.Get(prefixReservedKey + valAddr)
-		if len(pubKeyHex) == 0 {
-			return 0, fmt.Errorf("received vote from unknown validator with address %q", valAddr)
-		}
-		pubKeyBytes, err := hex.DecodeString(pubKeyHex)
+		pubKey, err := app.loadPubKey(valAddr)
 		if err != nil {
-			return 0, fmt.Errorf("could not hex-decode public key for validator address %s, err %w", valAddr, err)
-		}
-		var pubKeyProto cryptoproto.PublicKey
-		err = pubKeyProto.Unmarshal(pubKeyBytes)
-		if err != nil {
-			return 0, fmt.Errorf("unable to unmarshal public key for validator address %s, err %w", valAddr, err)
-		}
-		pubKey, err := cryptoenc.PubKeyFromProto(pubKeyProto)
-		if err != nil {
-			return 0, fmt.Errorf("could not obtain a public key from its proto for validator address %s, err %w", valAddr, err)
+			return 0, err
 		}
 		if !pubKey.VerifySignature(extSignBytes, vote.ExtensionSignature) {
 			return 0, errors.New("received vote with invalid signature")

test/e2e/networks/ci.toml
@@ -17,6 +17,7 @@ abci_protocol = "builtin"
 prometheus = true
 peer_gossip_intraloop_sleep_duration = "50ms"
 abci_tests_enabled = true
+constant_flip = true
 
 [validators]
 validator01 = 100

test/e2e/node/config.go
@@ -53,6 +53,8 @@ type Config struct {
 
 	NoLanes bool              `toml:"no_lanes"`
 	Lanes   map[string]uint32 `toml:"lanes"`
+
+	ConstantFlip bool `toml:"constant_flip"`
 }
 
 // App extracts out the application specific configuration parameters.
@@ -77,6 +79,7 @@ func (cfg *Config) App() *app.Config {
 		PbtsUpdateHeight:           cfg.PbtsUpdateHeight,
 		NoLanes:                    cfg.NoLanes,
 		Lanes:                      cfg.Lanes,
+		ConstantFlip:               cfg.ConstantFlip,
 	}
 }
 

test/e2e/pkg/manifest.go
@@ -29,7 +29,7 @@ type Manifest struct {
 	// specifying an empty set will start with no validators in genesis, and
 	// the application must return the validator set in InitChain via the
 	// setting validator_update.0 (see below).
-	Validators *map[string]int64 `toml:"validators"`
+	Validators map[string]int64 `toml:"validators"`
 
 	// ValidatorUpdatesMap is a map of heights to validator names and their power,
 	// and will be returned by the ABCI application. For example, the following
@@ -165,6 +165,13 @@ type Manifest struct {
 	// configuration files for all nodes. The format is "key = value".
 	// Example: "p2p.send_rate = 512000".
 	Config []string `toml:"config"`
+
+	// If true, the application will return validator updates and
+	// `ConsensusParams` updates at every height.
+	// This is useful to create a more dynamic testnet.
+	// * An existing validator will be chosen, and its power will alternate between 0 and 1.
+	// * `ConsensusParams` will be flipping on and off key types not set at genesis.
+	ConstantFlip bool `toml:"constant_flip"`
 }
 
 // ManifestNode represents a node in a testnet manifest.

test/e2e/tests/block_test.go
@@ -150,16 +150,27 @@ func TestBlock_Time(t *testing.T) {
 	blocks := fetchBlockChain(t)
 	testnet := loadTestnet(t)
 
+	// blocks are 1-indexed, i.e., blocks[0] = height 1, blocks[1] = height 2, etc.
 	lastBlock := blocks[0]
 	valSchedule := newValidatorSchedule(t, &testnet)
 	for _, block := range blocks[1:] {
 		require.Less(t, lastBlock.Time, block.Time)
 		lastBlock = block
 
-		valSchedule.Increment(t, 1)
 		if testnet.PbtsEnableHeight == 0 || block.Height < testnet.PbtsEnableHeight {
 			expTime := block.LastCommit.MedianTime(valSchedule.Set)
-			require.Equal(t, expTime, block.Time, "height=%d", block.Height)
+
+			require.Equal(
+				t,
+				expTime,
+				block.Time,
+				"height=%d, valSet=%s\n%s",
+				block.Height,
+				valSchedule.Set,
+				block.LastCommit.StringIndented("  "),
+			)
 		}
+
+		valSchedule.IncreaseHeight(t, 1)
 	}
 }

commit c6fa22554832d4cd788e286864400ad7026921fc

fix(e2e):  temporary disable validator set oscillation in generated manifests (#4478)
Workaround for #4492.

This PR:

1. Describes the issue with introduced by
https://github.com/cometbft/cometbft/pull/2283 in comments in the source
code (check FIXMEs and TODOs)
2. Temporarily disables the validator set oscillations in the generated
manifest, used in nightly runs

Item 2. has the side effect that not only the validator set oscillations
but also the consensus params oscillations are disabled, while we are
only aware of issues in the former.

---

#### PR checklist

- [ ] Tests written/updated
- [ ] Changelog entry added in `.changelog` (we use
[unclog](https://github.com/informalsystems/unclog) to manage our
changelog)
- [ ] Updated relevant documentation (`docs/` or `spec/`) and code
comments

---------

Co-authored-by: Andy Nogueira <me@andynogueira.dev>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

test/e2e/generator/generate.go
@@ -73,7 +73,8 @@ var (
 	pbtsEnabled                = weightedChoice{true: 3, false: 1}
 	pbtsHeightOffset           = uniformChoice{int64(0), int64(10), int64(100)}
 	keyType                    = uniformChoice{ed25519.KeyType, secp256k1.KeyType, bls12381.KeyType}
-	constantFlip               = uniformChoice{true, false}
+	// TODO: reinstate this once the oscillation logic is fixed.
+	// constantFlip               = uniformChoice{true, false}.
 )
 
 type generateConfig struct {
@@ -173,7 +174,9 @@ func generateTestnet(r *rand.Rand, opt map[string]any, upgradeVersion string, pr
 	}
 
 	manifest.VoteExtensionSize = voteExtensionSize.Choose(r).(uint)
-	manifest.ConstantFlip = constantFlip.Choose(r).(bool)
+	// TODO: reinstate this once the oscillation logic is fixed.
+	// manifest.ConstantFlip = constantFlip.Choose(r).(bool)
+	manifest.ConstantFlip = false
 
 	manifest.PbtsUpdateHeight = pbtsUpdateHeight.Choose(r).(int64)
 	if manifest.PbtsUpdateHeight == 1 {

test/e2e/pkg/testnet.go
@@ -371,6 +371,14 @@ func NewTestnetFromManifest(manifest Manifest, file string, ifd InfrastructureDa
 
 		const flipSpan = 3000
 		for i := max(1, manifest.InitialHeight); i < manifest.InitialHeight+flipSpan; i++ {
+			// FIXME: we do not flip the validator when there is
+			// **any** scheduled validator update for that height.
+			// We may have a validator update that affects a
+			// **different** validator here and the height can be
+			// odd. We add our validator back in odd heights, so we
+			// are skipping this in this case. Therefore, in the
+			// next even height we are removing a validator that is
+			// not present in the validator set.
 			if _, ok := testnet.ValidatorUpdates[i]; ok {
 				continue
 			}

test/e2e/runner/setup.go
@@ -458,6 +458,8 @@ func MakeAppConfig(node *e2e.Node) ([]byte, error) {
 		}
 	}
 
+	// TODO: check if the produced validator updates is indeed valid.
+	// This goes to the application configuration file.
 	if len(node.Testnet.ValidatorUpdates) > 0 {
 		validatorUpdates := map[string]map[string]int64{}
 		for height, validators := range node.Testnet.ValidatorUpdates {
@@ -469,6 +471,10 @@ func MakeAppConfig(node *e2e.Node) ([]byte, error) {
 				}
 				updateVals[base64.StdEncoding.EncodeToString(validator.PrivvalKey.PubKey().Bytes())] = power
 			}
+			// TODO: the validator updates are written to the toml
+			// file in lexicographical order. This means that
+			// update 1000 comes after update 1, and much before
+			// update 2. Consider producing `0001` instead of `1`.
 			validatorUpdates[strconv.FormatInt(height, 10)] = updateVals
 		}
 		cfg["validator_update"] = validatorUpdates
1d9465d00e0cb965fc720cf5775ffbde7282d3f6
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-11-22 14:43:27 +0100

    feat(mocks): `mockery` v2.49.0 changes mock format (#4521)
    This PR generates new mocks because the version of mockery changes.
    Marking it as draft until all tests pass.
    
    The resolution comes from https://github.com/vektra/mockery/issues/848 .
    
    We should think of a way to make our mocks compatible with the upcoming
    `v3` release.
    
    ---
    
    #### PR checklist
    
    - [ ] Tests written/updated
    - [x ] Changelog entry added in `.changelog` (we use
    [unclog](https://github.com/informalsystems/unclog) to manage our
    changelog)
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    
    ---------
    
    Co-authored-by: Andy Nogueira <me@andynogueira.dev>
    (cherry picked from commit cdab56d7ddee35708f6188c8845a6ec5b2570b8f)
    
    # Conflicts:
    #	scripts/mockery_generate.sh

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -2,7 +2,10 @@
 #
 # Invoke Mockery v2 to update generated mocks for the given type.
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> cdab56d7d (feat(mocks): `mockery` v2.49.0 changes mock format (#4521))
 go run github.com/vektra/mockery/v2@latest --disable-version-string --case underscore --name "$*"
 

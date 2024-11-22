74a8dffd19ea806ddf9f4e7de4006a1db0874323
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-11-22 14:55:16 +0100

    fixed conflict

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -4,7 +4,3 @@
 # Last change was made based on changes for main in https://github.com/tendermint/tendermint/pull/9196 
 
 go run github.com/vektra/mockery/v2@latest --disable-version-string --case underscore --name "$*"
-<<<<<<< HEAD
-=======
-
->>>>>>> cdab56d7d (feat(mocks): `mockery` v2.49.0 changes mock format (#4521))

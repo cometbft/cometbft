fa2d3705d0e4cb6284519904d3f13f01a35ef0e2
Alessandro <alessandro@informal.systems>
2024-11-29 17:37:12 +0100

    Added unit tests for `PrefixDB` `Delete`.

--- internal/storage/prefixdb_test.go
+++ internal/storage/prefixdb_test.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"testing"
 )
 
@@ -138,15 +139,15 @@ func TestPrefixDBSet(t *testing.T) {
 			vals = [][]byte{{0x01}, {0x02}}
 		)
 		if err := prefixDB.Set(keys[0], vals[0]); err != nil {
-			t.Fatalf("unexpected error: %s", err)
+			t.Fatalf("unsynced Set unexpected error: %s", err)
 		}
 
 		if err := prefixDB.SetSync(keys[1], vals[1]); err != nil {
-			t.Fatalf("unexpected error: %s", err)
+			t.Fatalf("synced Set unexpected error: %s", err)
 		}
 
 		for i, key := range keys {
-			// we are calling PebbleDB's [SetSync] directly, therefore we must u
+			// we are calling PebbleDB's [Get] directly, therefore we must u
 			// prepend the prefix to the key ourselves.
 			prefixedKey := append(prefixDB.prefix, key...)
 			storedVal, err := prefixDB.db.Get(prefixedKey)
@@ -161,4 +162,81 @@ func TestPrefixDBSet(t *testing.T) {
 			}
 		}
 	})
+}
+
+func TestPrefixDBDelete(t *testing.T) {
+	pebbleDB, dbCloser, err := newTestEmptyDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(dbCloser)
+
+	prefixDB := &PrefixDB{
+		db:     pebbleDB,
+		prefix: []byte{'t', 'e', 's', 't'},
+	}
+
+	t.Run("EmptyKeyErr", func(t *testing.T) {
+		if err := prefixDB.Delete(nil); !errors.Is(err, errKeyEmpty) {
+			t.Errorf("expected %s, got: %s", errKeyEmpty, err)
+		}
+	})
+
+	t.Run("KeyNotExistNoErr", func(t *testing.T) {
+		key := []byte{'a'}
+		if err := prefixDB.Delete(key); err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		}
+	})
+
+	t.Run("KeyExistNoErr", func(t *testing.T) {
+		if err := deletePrefixDBHelper(prefixDB, false); err != nil {
+			t.Errorf("unsynced Delete unexpected error: %s", err)
+		}
+
+		if err := deletePrefixDBHelper(prefixDB, true); err != nil {
+			t.Errorf("synced Delete unexpected error: %s", err)
+		}
+	})
+}
+
+// deletePrefixDBHelper is a utility function supporting TestPrefixDBDelete.
+// It writes a key-value pair to the database, deletes it, then checks that the key
+// is deleted.
+func deletePrefixDBHelper(pDB *PrefixDB, synced bool) error {
+	var (
+		key = []byte{'a'}
+		val = []byte{0x01}
+		// we are calling the underlying DB's methods directly, therefore we must
+		// append the prefix to the key ourselves.
+		prefixedKey = append(pDB.prefix, key...)
+	)
+	if err := pDB.db.SetSync(prefixedKey, val); err != nil {
+		return fmt.Errorf("writing to test DB: %s", err)
+	}
+
+	if !synced {
+		if err := pDB.Delete(key); err != nil {
+			return fmt.Errorf("unsynced Delete unexpected error: %s", err)
+		}
+	} else {
+		if err := pDB.DeleteSync(key); err != nil {
+			return fmt.Errorf("synced Delete unexpected error: %s", err)
+		}
+	}
+
+	// check key is deleted
+	gotVal, err := pDB.db.Get(prefixedKey)
+	if err != nil {
+		return fmt.Errorf("reading form test DB: %s", err)
+	}
+	if len(gotVal) > 0 {
+		// our implementation of PebbleDB does not return an error if a key
+		// is not found. Instead, it returns a nil error and a nil value.
+		// Therefore, to check if the deletion was successful we must check
+		// that the value has 0 length (len(nil_slice)==0).
+		return fmt.Errorf("expected nil value, got: %v", gotVal)
+	}
+
+	return nil
 }

5e0f60cc0a5020417467b1868aa42f0da889f9e7
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-11-29 09:48:35 +0400

    chore(p2p): reintroduce p2p.ID type
    Closes #4549

--- mempool/mempoolTx.go
+++ mempool/mempoolTx.go
@@ -5,7 +5,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/cometbft/cometbft/p2p/nodekey"
+	"github.com/cometbft/cometbft/p2p"
 	"github.com/cometbft/cometbft/types"
 )
 
@@ -35,13 +35,13 @@ func (memTx *mempoolTx) GasWanted() int64 {
 	return memTx.gasWanted
 }
 
-func (memTx *mempoolTx) IsSender(peerID nodekey.ID) bool {
+func (memTx *mempoolTx) IsSender(peerID p2p.ID) bool {
 	_, ok := memTx.senders.Load(peerID)
 	return ok
 }
 
 // Add the peer ID to the list of senders. Return true iff it exists already in the list.
-func (memTx *mempoolTx) addSender(peerID nodekey.ID) bool {
+func (memTx *mempoolTx) addSender(peerID p2p.ID) bool {
 	if len(peerID) == 0 {
 		return false
 	}
--- p2p/types.go
+++ p2p/types.go
@@ -4,12 +4,15 @@ import (
 	"github.com/cosmos/gogoproto/proto"
 
 	tmp2p "github.com/cometbft/cometbft/api/cometbft/p2p/v1"
+	"github.com/cometbft/cometbft/p2p/nodekey"
 	"github.com/cometbft/cometbft/p2p/transport/tcp/conn"
 	"github.com/cometbft/cometbft/types"
 )
 
 type (
 	ConnectionStatus = conn.ConnectionStatus
+	// ID is the unique identifier for a peer.
+	ID = nodekey.ID
 )
 
 // Envelope contains a message with sender routing info.

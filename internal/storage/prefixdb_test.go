ab6178cf934190312af034c676f62ff6202dda12
Alessandro <alessandro@informal.systems>
2024-12-02 11:27:19 +0100

    Added unit tests for `Print`, `prependPrefix`, `prefixedIteratorBounds`.

--- internal/storage/prefixdb_test.go
+++ internal/storage/prefixdb_test.go
@@ -2,8 +2,12 @@ package storage
 
 import (
 	"bytes"
+	"encoding/hex"
 	"errors"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -198,6 +202,221 @@ func TestPrefixDBDelete(t *testing.T) {
 			t.Errorf("synced Delete unexpected error: %s", err)
 		}
 	})
+}
+
+func TestPrefixDBPrint(t *testing.T) {
+	pebbleDB, dbCloser, err := newTestEmptyDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(dbCloser)
+
+	prefixDB := &PrefixDB{
+		db:     pebbleDB,
+		prefix: []byte{'t', 'e', 's', 't'},
+	}
+
+	kvPairs := map[string]string{
+		"a": "1",
+		"b": "2",
+		"c": "3",
+	}
+	for k, v := range kvPairs {
+		prefixedKey := prependPrefix(prefixDB.prefix, []byte(k))
+		if err := pebbleDB.Set(prefixedKey, []byte(v)); err != nil {
+			t.Fatalf("writing key %s to test DB: %s", prefixedKey, err)
+		}
+	}
+
+	// Print() writes to os.Stdout, so we need to do some awkward shenanigans to
+	// capture the output and check it's correct.
+	r, w, err := os.Pipe()
+	if err != nil {
+		const format = "Error creating pipe to capture os.Stdout contents: %s"
+		t.Fatalf(format, err)
+	}
+
+	// // store os.Stdout and redirect it to print to the writer we just created
+	stdOut := os.Stdout
+	os.Stdout = w
+
+	if err := prefixDB.Print(); err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	w.Close()
+
+	// restore os.Stdout
+	os.Stdout = stdOut
+
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, r); err != nil {
+		t.Fatalf("reading os.Stdout contents: %s", err)
+	}
+	r.Close()
+
+	outputStr := buf.String()
+	for k, v := range kvPairs {
+		var (
+			kStr = strings.ToUpper(hex.EncodeToString([]byte(k)))
+			vStr = strings.ToUpper(hex.EncodeToString([]byte(v)))
+
+			wantStr = "[" + kStr + "]:\t[" + vStr + "]\n"
+		)
+		if !strings.Contains(outputStr, wantStr) {
+			const formatStr = "\nthis line was not printed: %q\nfull print: %q"
+			t.Errorf(formatStr, wantStr, outputStr)
+		}
+	}
+}
+
+func TestPrependPrefix(t *testing.T) {
+	var (
+		prefix = []byte{'t', 'e', 's', 't'}
+		key    = []byte{'k', 'e', 'y'}
+
+		testCases = []struct {
+			prefix []byte
+			key    []byte
+			want   []byte
+		}{
+			{ // non-nil prefix and key
+				prefix: prefix,
+				key:    key,
+				want:   append(prefix, key...),
+			},
+			{ // nil key
+				prefix: prefix,
+				key:    nil,
+				want:   prefix,
+			},
+			{ // nil prefix
+				prefix: nil,
+				key:    key,
+				want:   key,
+			},
+			{ //  nil prefix and key
+				prefix: nil,
+				key:    nil,
+				want:   []byte{},
+			},
+		}
+	)
+
+	for i, tc := range testCases {
+		got := prependPrefix(tc.prefix, tc.key)
+		if !bytes.Equal(got, tc.want) {
+			t.Errorf("test %d: want %s, but got %s", i, tc.want, got)
+		}
+	}
+}
+
+func TestPrefixedIteratorBounds(t *testing.T) {
+	var (
+		prefix        = []byte{'t', 'e', 's', 't'}
+		start         = []byte{'k', 'e', 'y', '1'}
+		end           = []byte{'k', 'e', 'y', '2'}
+		prefixedStart = append(prefix, start...)
+		prefixedEnd   = append(prefix, end...)
+
+		successCases = []struct {
+			prefix    []byte
+			start     []byte
+			end       []byte
+			wantStart []byte
+			wantEnd   []byte
+			wantErr   error
+		}{
+			{ // valid prefix and bounds
+				prefix:    prefix,
+				start:     start,
+				end:       end,
+				wantStart: prefixedStart,
+				wantEnd:   prefixedEnd,
+				wantErr:   nil,
+			},
+			{ // nil end (upper bound is incremented prefix)
+				prefix:    prefix,
+				start:     start,
+				end:       nil,
+				wantStart: prefixedStart,
+				wantEnd:   incrementBigEndian(prefix),
+				wantErr:   nil,
+			},
+			{ // nil start and end
+				prefix:    prefix,
+				start:     nil,
+				end:       nil,
+				wantStart: prefix,
+				wantEnd:   incrementBigEndian(prefix),
+				wantErr:   nil,
+			},
+			{ // nil prefix
+				prefix:    nil,
+				start:     start,
+				end:       end,
+				wantStart: start,
+				wantEnd:   end,
+				wantErr:   nil,
+			},
+		}
+	)
+	for i, tc := range successCases {
+		start, end, err := prefixedIteratorBounds(tc.prefix, tc.start, tc.end)
+		if err != nil {
+			t.Errorf("test %d: unexpected error: %s", i, err)
+		}
+
+		if !bytes.Equal(start, tc.wantStart) {
+			t.Errorf("unexpected start bound: got %s, want %s", start, tc.wantStart)
+		}
+		if !bytes.Equal(end, tc.wantEnd) {
+			t.Errorf("unexpected end bound: got %s, want %s", end, tc.wantEnd)
+		}
+	}
+
+	failureCases := []struct {
+		start   []byte
+		end     []byte
+		wantErr error
+	}{
+		{ // empty start
+			start:   []byte{},
+			end:     end,
+			wantErr: errKeyEmpty,
+		},
+		{ // empty end
+			start:   start,
+			end:     []byte{},
+			wantErr: errKeyEmpty,
+		},
+	}
+	for i, tc := range failureCases {
+		_, _, err := prefixedIteratorBounds(prefix, tc.start, tc.end)
+		if !errors.Is(err, tc.wantErr) {
+			t.Errorf("test %d: want error %q, but got %q", i, tc.wantErr, err)
+		}
+	}
+}
+
+func TestIncrementBigEndian(t *testing.T) {
+	testCases := []struct {
+		input      []byte
+		wantResult []byte
+	}{
+		{[]byte{0xFE}, []byte{0xFF}},             // simple increment
+		{[]byte{0xFF}, nil},                      // overflow
+		{[]byte{0x00, 0x01}, []byte{0x00, 0x02}}, // simple increment
+		{[]byte{0x00, 0xFF}, []byte{0x01, 0x00}}, // carry over
+		{[]byte{0xFF, 0xFF}, nil},                // overflow
+		{[]byte{}, []byte{}},                     // no-op
+	}
+
+	for i, tc := range testCases {
+		gotResult := incrementBigEndian(tc.input)
+		if !bytes.Equal(gotResult, tc.wantResult) {
+			t.Errorf("test %d: want: %v, got: %v", i, tc.wantResult, gotResult)
+		}
+	}
 }
 
 // deletePrefixDBHelper is a utility function supporting TestPrefixDBDelete.

bc84cd501f4c0abd288f9b664bbd47eba9c605a1
Alessandro Sforzin <alessandro@informal.systems>
2024-12-04 14:40:17 +0100

    regenerated mocks

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 #
 # Invoke Mockery v2 to update generated mocks for the given type.
-# Last change was made based on changes for main in https://github.com/tendermint/tendermint/pull/9196 
+# Last change was made based on changes for main in https://github.com/tendermint/tendermint/pull/9196
 
-<<<<<<< HEAD
-go run github.com/vektra/mockery/v2@latest --disable-version-string --case underscore --name "$*"
-=======
 go run github.com/vektra/mockery/v2@v2.49.2 --disable-version-string --case underscore --name "$*"
->>>>>>> 13d852b43 (build(deps): Mock generation no longer uses the `latest` tag on `mockery`. (#4605))

72ffddbbcd81951f2088322200d9eefea12f722e
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-12-05 10:38:35 +0400

    fix mockery version + replace type

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -2,5 +2,4 @@
 #
 # Invoke Mockery v2 to update generated mocks for the given type.
 
-go run github.com/vektra/mockery/v2@latest --disable-version-string --case underscore --name "$*"
-
+go run github.com/vektra/mockery/v2@v2.50.0 --name "$*"

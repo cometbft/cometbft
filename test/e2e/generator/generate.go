c3e1c499df9a89a4c16a4d5ec629c2b3f56d761b
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2024-12-23 16:02:05 +0100

    Added comment on why VE size is lowered

--- test/e2e/generator/generate.go
+++ test/e2e/generator/generate.go
@@ -68,11 +68,15 @@ var (
 	voteExtensionsUpdateHeight = uniformChoice{int64(-1), int64(0), int64(1)} // -1: genesis, 0: InitChain, 1: (use offset)
 	voteExtensionEnabled       = weightedChoice{true: 3, false: 1}
 	voteExtensionsHeightOffset = uniformChoice{int64(0), int64(10), int64(100)}
-	voteExtensionSize          = uniformChoice{uint(128), uint(512), uint(2048 / 2), uint(8192 / 2)} // TODO: define the right values depending on experiment results.
-	pbtsUpdateHeight           = uniformChoice{int64(-1), int64(0), int64(1)}                        // -1: genesis, 0: InitChain, 1: (use offset)
-	pbtsEnabled                = weightedChoice{true: 3, false: 1}
-	pbtsHeightOffset           = uniformChoice{int64(0), int64(10), int64(100)}
-	keyType                    = uniformChoice{ed25519.KeyType, secp256k1.KeyType, bls12381.KeyType}
+	// We have explicitly left the division by 2 here to explain why it is needed.
+	// When adding support for Non Replay-Protected Vote Extensions to the e2e,
+	// we double the size of the VE. This message was too big when contacting
+	// remote signers and surpassing also the maximum size of p2p messages.
+	voteExtensionSize = uniformChoice{uint(128), uint(512), uint(2048 / 2), uint(8192 / 2)} // TODO: define the right values depending on experiment results.
+	pbtsUpdateHeight  = uniformChoice{int64(-1), int64(0), int64(1)}                        // -1: genesis, 0: InitChain, 1: (use offset)
+	pbtsEnabled       = weightedChoice{true: 3, false: 1}
+	pbtsHeightOffset  = uniformChoice{int64(0), int64(10), int64(100)}
+	keyType           = uniformChoice{ed25519.KeyType, secp256k1.KeyType, bls12381.KeyType}
 	// TODO: reinstate this once the oscillation logic is fixed.
 	// constantFlip               = uniformChoice{true, false}.
 )

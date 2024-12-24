e25ee4589ce38b19f39f3d5266b4824756187fd4
Anton Kaliaev <anton.kalyaev@gmail.com>
2024-12-24 18:22:15 +0400

    fix(types): update MaxSignatureSize to include secp256k1eth

--- types/signable.go
+++ types/signable.go
@@ -3,6 +3,7 @@ package types
 import (
 	"github.com/cometbft/cometbft/crypto/bls12381"
 	"github.com/cometbft/cometbft/crypto/ed25519"
+	"github.com/cometbft/cometbft/crypto/secp256k1eth"
 	cmtmath "github.com/cometbft/cometbft/libs/math"
 )
 
@@ -11,7 +12,7 @@ import (
 // XXX: secp256k1 does not have max signature size defined.
 var MaxSignatureSize = cmtmath.MaxInt(
 	ed25519.SignatureSize,
-	bls12381.SignatureLength)
+	cmtmath.MaxInt(secp256k1eth.SignatureLength, bls12381.SignatureLength))
 
 // Signable is an interface for all signable things.
 // It typically removes signatures before serializing.

f2d9ae0c0e2cc1b220b93665aa8b5baf3e013a8a
Anton Kaliaev <anton.kalyaev@gmail.com>
2025-01-23 11:53:43 +0400

    fix(crypto/bls12381): modify `Sign`, `Verify` (#4813)
    to use `dstMinPk`
    
    Closes #4783
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- crypto/bls12381/key_bls12381.go
+++ crypto/bls12381/key_bls12381.go
@@ -27,7 +27,7 @@ var (
 	// of a more comprehensive subgroup check on the key.
 	ErrInfinitePubKey = errors.New("bls12381: pubkey is infinite")
 
-	dstMinSig = []byte("BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_NUL_")
+	dstMinPk = []byte("BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_")
 )
 
 // For minimal-pubkey-size operations.
@@ -110,7 +110,7 @@ func (PrivKey) Type() string {
 
 // Sign signs the given byte array.
 func (privKey PrivKey) Sign(msg []byte) ([]byte, error) {
-	signature := new(blstSignature).Sign(privKey.sk, msg, dstMinSig)
+	signature := new(blstSignature).Sign(privKey.sk, msg, dstMinPk)
 	return signature.Compress(), nil
 }
 
@@ -188,7 +188,7 @@ func (pubKey PubKey) VerifySignature(msg, sig []byte) bool {
 		return false
 	}
 
-	return signature.Verify(false, pubKey.pk, false, msg, dstMinSig)
+	return signature.Verify(false, pubKey.pk, false, msg, dstMinPk)
 }
 
 // Bytes returns the byte format.

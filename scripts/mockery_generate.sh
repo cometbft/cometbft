e56860a3de7286f7a093ae2d5c2ac0d4ddc03c9f
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-01-31 22:22:04 +0300

    fix: fixed argument handling in shell script

--- scripts/mockery_generate.sh
+++ scripts/mockery_generate.sh
@@ -2,4 +2,4 @@
 #
 # Invoke Mockery v2 to update generated mocks for the given type.
 
-go run github.com/vektra/mockery/v2@v2.50.0 --name "$*"
+go run github.com/vektra/mockery/v2@v2.50.0 --name "$@"

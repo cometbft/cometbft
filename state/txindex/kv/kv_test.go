1e862af62d902fa41676fdd5aa28f48e3babc6ca
jike <jike2021@proton.me>
2025-03-20 22:30:29 +0800

    save

--- state/txindex/kv/kv_test.go
+++ state/txindex/kv/kv_test.go
@@ -5,12 +5,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"slices"
 	"testing"
 
 	"github.com/cosmos/gogoproto/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/exp/slices"
 
 	abci "github.com/cometbft/cometbft/abci/types"
 	cmtdb "github.com/cometbft/cometbft/db"

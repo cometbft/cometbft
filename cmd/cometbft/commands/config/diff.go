615b6a51b873d915c5b141d78afc72bc9c32956d
jike <jike2021@proton.me>
2025-03-21 16:25:25 +0800

    update

--- cmd/cometbft/commands/config/diff.go
+++ cmd/cometbft/commands/config/diff.go
@@ -2,9 +2,10 @@ package config
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 
 	"github.com/spf13/cobra"
-	"golang.org/x/exp/maps"
 
 	"github.com/cometbft/cometbft/internal/confix"
 )
@@ -26,7 +27,7 @@ func DiffCommand() *cobra.Command {
 
 			targetVersion := args[0]
 			if _, ok := confix.Migrations[targetVersion]; !ok {
-				return fmt.Errorf("unknown version %q, supported versions are: %q", targetVersion, maps.Keys(confix.Migrations))
+				return fmt.Errorf("unknown version %q, supported versions are: %q", targetVersion, slices.Collect(maps.Keys(confix.Migrations)))
 			}
 
 			targetVersionFile, err := confix.LoadLocalConfig(targetVersion + ".toml")

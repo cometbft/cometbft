97e4efc94d231c406c109f3c0e287f43a3b6c6c5
sashass1315 <sashass1315@gmail.com>
2025-09-11 08:48:16 +0300

    Update store.go

--- store/store.go
+++ store/store.go
@@ -388,7 +388,7 @@ func (bs *BlockStore) PruneBlocks(height int64, state sm.State) (uint64, int64, 
 			evidencePoint = h
 		}
 
-		// if height is beyond the evidence point we dont delete the header
+		// if height is beyond the evidence point we don't delete the header
 		if h < evidencePoint {
 			if err := batch.Delete(calcBlockMetaKey(h)); err != nil {
 				return 0, -1, err
@@ -397,7 +397,7 @@ func (bs *BlockStore) PruneBlocks(height int64, state sm.State) (uint64, int64, 
 		if err := batch.Delete(calcBlockHashKey(meta.BlockID.Hash)); err != nil {
 			return 0, -1, err
 		}
-		// if height is beyond the evidence point we dont delete the commit data
+		// if height is beyond the evidence point we don't delete the commit data
 		if h < evidencePoint {
 			if err := batch.Delete(calcBlockCommitKey(h)); err != nil {
 				return 0, -1, err

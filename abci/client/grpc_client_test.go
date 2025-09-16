59175b7147e587f5b69745bf888b3c0c6779aac7
forkfury <forkfury@gmail.com>
2025-09-16 18:20:50 +0200

    Update grpc_client_test.go

--- abci/client/grpc_client_test.go
+++ abci/client/grpc_client_test.go
@@ -19,14 +19,20 @@ import (
 	"github.com/cometbft/cometbft/libs/log"
 )
 
+// TestGRPC tests the gRPC ABCI client by sending multiple CheckTx requests
+// to a gRPC server and verifying the responses.
 func TestGRPC(t *testing.T) {
+	// Create a base ABCI application for testing
 	app := types.NewBaseApplication()
+	// Number of CheckTx requests to send for load testing
 	numCheckTxs := 2000
+	// Generate a unique socket file path to avoid conflicts
 	socketFile := fmt.Sprintf("/tmp/test-%08x.sock", rand.Int31n(1<<30))
 	defer os.Remove(socketFile)
+	// Format socket address for Unix domain socket
 	socket := fmt.Sprintf("unix://%v", socketFile)
 
-	// Start the listener
+	// Start the gRPC ABCI server
 	server := abciserver.NewGRPCServer(socket, app)
 	server.SetLogger(log.TestingLogger().With("module", "abci-server"))
 	err := server.Start()
@@ -38,7 +44,7 @@ func TestGRPC(t *testing.T) {
 		}
 	})
 
-	// Connect to the socket
+	// Connect to the gRPC server using insecure credentials (for testing)
 	conn, err := grpc.NewClient(socket, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	require.NoError(t, err)
 
@@ -48,20 +54,25 @@ func TestGRPC(t *testing.T) {
 		}
 	})
 
+	// Create ABCI client from gRPC connection
 	client := types.NewABCIClient(conn)
 
-	// Write requests
+	// Send multiple CheckTx requests to test client performance and reliability
 	for counter := 0; counter < numCheckTxs; counter++ {
-		// Send request
+		// Send CheckTx request with test transaction data
 		response, err := client.CheckTx(context.Background(), &types.RequestCheckTx{Tx: []byte("test")})
 		require.NoError(t, err)
+		// Verify that the transaction was accepted (code 0 means success)
 		if response.Code != 0 {
 			t.Error("CheckTx failed with ret_code", response.Code)
 		}
+		// Safety check to prevent infinite loops (should never trigger)
 		if counter > numCheckTxs {
 			t.Fatal("Too many CheckTx responses")
 		}
 		t.Log("response", counter)
+		// This condition will never be true since counter < numCheckTxs in the loop
+		// The goroutine appears to be dead code and should be removed
 		if counter == numCheckTxs {
 			go func() {
 				time.Sleep(time.Second * 1) // Wait for a bit to allow counter overflow

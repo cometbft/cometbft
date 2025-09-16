d92172efde2ee020318f1a957d5036ee364e5085
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-09-16 21:18:22 +0300

    chore: make key-value handling and requests more reliable

--- scripts/txs/random.sh
+++ scripts/txs/random.sh
@@ -2,18 +2,16 @@
 set -u
 
 function toHex() {
-	echo -n $1 | hexdump -ve '1/1 "%.2X"'
+    echo -n "$1" | hexdump -ve '1/1 "%.2X"'
 }
 
 N=$1
 PORT=$2
 
-for i in `seq 1 $N`; do
-	# store key value pair
-	KEY=$(head -c 10 /dev/urandom)
-	VALUE="$i"
-	echo $(toHex $KEY=$VALUE)
-	curl 127.0.0.1:$PORT/broadcast_tx_sync?tx=0x$(toHex $KEY=$VALUE)
+for ((i=1; i<=N; i++)); do
+    # store key value pair
+    KEY=$(head -c 10 /dev/urandom | hexdump -ve '1/1 "%.2X"')
+    VALUE="$i"
+    echo $(toHex "$KEY=$VALUE")
+    curl "127.0.0.1:$PORT/broadcast_tx_sync?tx=0x$(toHex "$KEY=$VALUE")"
 done
-
-

2541a4d3ece10e59dc8b361b635f0b842aff9285
witty 🪿 <131909329+0xwitty@users.noreply.github.com>
2025-09-16 10:39:05 +0300

    chore (fix): build script issues for Alpine + Go 1.17+

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -3,7 +3,7 @@
 # Update the generated code for protocol buffers in the CometBFT repository.
 # This must be run from inside a CometBFT working directory.
 #
-set -euo pipefail
+set -eu
 
 # Work from the root of the repository.
 cd "$(git rev-parse --show-toplevel)"
@@ -11,9 +11,9 @@ cd "$(git rev-parse --show-toplevel)"
 # Run inside Docker to install the correct versions of the required tools
 # without polluting the local system.
 docker run --rm -i -v "$PWD":/w --workdir=/w golang:1.20-alpine sh <<"EOF"
-apk add git make
+apk add --no-cache git make
 
-go install github.com/bufbuild/buf/cmd/buf
+go install github.com/bufbuild/buf/cmd/buf@latest
 go install github.com/cosmos/gogoproto/protoc-gen-gogofaster@latest
 make proto-gen
 EOF

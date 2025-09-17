80b5cc08407895b6059e62f46f2deb3101a59636
aljo242 <alex@cosmoslabs.io>
2025-09-17 11:40:09 -0400

    lint-fix

--- test/e2e/pkg/testnet.go
+++ test/e2e/pkg/testnet.go
@@ -180,7 +180,6 @@ func NewTestnetFromManifest(manifest Manifest, file string, ifd InfrastructureDa
 		Prometheus:                 manifest.Prometheus,
 		BlockMaxBytes:              manifest.BlockMaxBytes,
 		VoteExtensionsEnableHeight: manifest.VoteExtensionsEnableHeight,
-		VoteExtensionSize:          manifest.VoteExtensionSize,
 		VoteExtensionsUpdateHeight: manifest.VoteExtensionsUpdateHeight,
 		VoteExtensionSize:          manifest.VoteExtensionSize,
 		ExperimentalMaxGossipConnectionsToPersistentPeers:    manifest.ExperimentalMaxGossipConnectionsToPersistentPeers,

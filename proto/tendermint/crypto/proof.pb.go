3aa0343b2591ae9771283936310b76a611898d55
aljo242 <alex@cosmoslabs.io>
2025-09-22 17:07:52 -0400

    fix

--- proto/tendermint/crypto/proof.pb.go
+++ proto/tendermint/crypto/proof.pb.go
@@ -206,7 +206,7 @@ func (m *DominoOp) GetOutput() string {
 }
 
 // ProofOp defines an operation used for calculating Merkle root
-// The data could be arbitrary format, providing nessecary data
+// The data could be arbitrary format, providing necessary data
 // for example neighbouring node hash
 type ProofOp struct {
 	Type string `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty"`

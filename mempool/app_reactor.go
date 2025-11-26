cb3edf10f1cb749602e81e549deb7ddc37d40f5b
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-11-27 00:02:18 +0100

    implement broadcast txs

--- mempool/app_reactor.go
+++ mempool/app_reactor.go
@@ -1,8 +1,10 @@
 package mempool
 
 import (
+	"context"
 	"fmt"
 	"sync/atomic"
+	"time"
 
 	"github.com/cometbft/cometbft/config"
 	"github.com/cometbft/cometbft/p2p"
@@ -17,6 +19,9 @@ type AppReactor struct {
 	config  *config.MempoolConfig
 	mempool *AppMempool
 
+	ctx       context.Context
+	cancelCtx context.CancelFunc
+
 	switchedOn           atomic.Bool
 	waitForSwitchingOnCh chan struct{}
 }
@@ -26,9 +31,13 @@ func NewAppReactor(
 	mempool *AppMempool,
 	waitSync bool,
 ) *AppReactor {
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
 	r := &AppReactor{
 		config:               config,
 		mempool:              mempool,
+		ctx:                  ctx,
+		cancelCtx:            cancelCtx,
 		switchedOn:           atomic.Bool{},
 		waitForSwitchingOnCh: nil,
 	}
@@ -53,9 +62,41 @@ func (r *AppReactor) OnStart() error {
 
 	if !r.config.Broadcast {
 		r.Logger.Info("Tx broadcasting is disabled")
+		return nil
 	}
 
+	go func() {
+		defer func() {
+			if p := recover(); p != nil {
+				r.Logger.Error("Panic in broadcast routine", "panic", p)
+			}
+		}()
+
+		// fallback to max tx bytes if max batch bytes is not set
+		// most chains use 1MB which will definitely fit many small txs
+		maxBatchSizeBytes := r.config.MaxTxBytes
+		if r.config.MaxBatchBytes > 0 {
+			maxBatchSizeBytes = r.config.MaxBatchBytes
+		}
+
+		// safe default, could be moved to config
+		const batchWindow = 200 * time.Millisecond
+
+		r.broadcastTransactionsBatch(r.ctx, maxBatchSizeBytes, batchWindow)
+
+		r.Logger.Info("Broadcast routine stopped")
+	}()
+
 	return nil
+}
+
+func (r *AppReactor) OnStop() {
+	if !r.enabled() {
+		return
+	}
+
+	// will close the context and cancel broadcast
+	r.cancelCtx()
 }
 
 // GetChannels implements p2p.BaseReactor.
@@ -120,7 +161,7 @@ func (r *AppReactor) insertTx(peerID p2p.ID, tx types.Tx) {
 		return
 	}
 
-	txHash := fmt.Sprintf("%X", tx.Hash())
+	txHash := txHash(tx)
 	switch {
 	case errors.Is(err, ErrSeenTx):
 		r.Logger.Debug("Tx already seen", "tx", txHash, "peer", peerID)
@@ -128,6 +169,73 @@ func (r *AppReactor) insertTx(peerID p2p.ID, tx types.Tx) {
 		r.Logger.Debug("Tx too large", "err", err, "tx", txHash, "peer", peerID)
 	default:
 		r.Logger.Info("Failed to insert tx", "err", err, "tx", txHash, "peer", peerID)
+	}
+}
+
+// broadcastTransactionsBatch subscribes to new txs from app-mempool,
+// accumulates them in batches and broadcasts them to all connected peers.
+// Previously batching was disabled.
+// @see https://github.com/tendermint/tendermint/issues/5796
+func (r *AppReactor) broadcastTransactionsBatch(ctx context.Context, maxBatchSizeBytes int, window time.Duration) {
+	var (
+		batch          = [][]byte{}
+		batchSizeBytes = 0
+		timer          = time.NewTimer(window)
+	)
+
+	defer timer.Stop()
+
+	flush := func() {
+		if len(batch) > 0 {
+			// copy batch (only headers are copied)
+			sendBatch := append(make([][]byte, 0, len(batch)), batch...)
+
+			r.Switch.BroadcastAsync(p2p.Envelope{
+				Message:   &protomem.Txs{Txs: sendBatch},
+				ChannelID: MempoolChannel,
+			})
+		}
+
+		// reuse underlying array
+		batch = batch[:0]
+		batchSizeBytes = 0
+
+		// reset timer & drain if needed
+		if !timer.Stop() {
+			select {
+			case <-timer.C:
+			default:
+			}
+		}
+		timer.Reset(window)
+	}
+
+	// will be closed when r.ctx is canceled, 8 is chan size,
+	// which is ok since we send async
+	txs := r.mempool.TxStream(ctx, 8)
+
+	for {
+		select {
+		case tx, ok := <-txs:
+			if !ok {
+				// chan closed, flush and exit
+				flush()
+				return
+			}
+
+			txSizeBytes := len(tx)
+
+			// tx won't fit into batch, flush previous batch
+			if (batchSizeBytes + txSizeBytes) > maxBatchSizeBytes {
+				flush()
+			}
+
+			batch = append(batch, tx)
+			batchSizeBytes += txSizeBytes
+		case <-timer.C:
+			// window expired, flush batch, reset timer
+			flush()
+		}
 	}
 }
 
@@ -155,4 +263,8 @@ func txsFromEnvelope(e p2p.Envelope) ([]types.Tx, error) {
 		}
 		return txs, nil
 	}
+}
+
+func txHash(tx types.Tx) string {
+	return fmt.Sprintf("%X", tx.Hash())
 }

d2ed46bb55fa9f96b07bc18b247bec3743dc7015
Dmitry S <11892559+swift1337@users.noreply.github.com>
2026-01-21 13:26:22 +0100

    introduce new PeerSet

---
+++ lp2p/peerset.go
@@ -0,0 +1,280 @@
+package lp2p
+
+import (
+	"math/rand"
+	"sort"
+	"sync"
+
+	"github.com/cometbft/cometbft/libs/log"
+	"github.com/cometbft/cometbft/p2p"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/mr-tron/base58/base58"
+	"github.com/pkg/errors"
+)
+
+// PeerSet represents a single entrypoint for managing Peer's lifecycle
+type PeerSet struct {
+	host *Host
+
+	peers map[peer.ID]peerSetItem
+	mu    sync.RWMutex
+
+	metrics *p2p.Metrics
+	logger  log.Logger
+}
+
+type peerSetItem struct {
+	peer *Peer
+
+	private    bool
+	persistent bool
+}
+
+var _ p2p.IPeerSet = (*PeerSet)(nil)
+
+// NewPeerSet manager peers for a given switch
+func NewPeerSet(host *Host, metrics *p2p.Metrics, logger log.Logger) *PeerSet {
+	return &PeerSet{
+		host:    host,
+		peers:   make(map[peer.ID]peerSetItem),
+		metrics: metrics,
+		logger:  logger,
+	}
+}
+
+func (ps *PeerSet) Has(key p2p.ID) bool {
+	_, exists := ps.getByKey(key)
+
+	return exists
+}
+
+func (ps *PeerSet) Get(key p2p.ID) p2p.Peer {
+	peer, exists := ps.getByKey(key)
+	if !exists {
+		return nil
+	}
+
+	return peer
+}
+
+type PeerAddOptions struct {
+	Private       bool
+	Persistent    bool
+	OnBeforeStart func(p *Peer)
+	OnAfterStart  func(p *Peer)
+}
+
+// Add adds a new peer to the peer set.
+// Fails if peer is already present or self.
+func (ps *PeerSet) Add(id peer.ID, opts PeerAddOptions) (*Peer, error) {
+	if id == ps.host.ID() {
+		return nil, errors.New("peer is self")
+	}
+
+	addrInfo := ps.host.Peerstore().PeerInfo(id)
+	if len(addrInfo.Addrs) == 0 {
+		return nil, errors.New("peer has no addresses in peerstore")
+	}
+
+	p, err := NewPeer(ps.host, addrInfo, ps.metrics)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create peer")
+	}
+
+	set := ps.set(id, peerSetItem{
+		peer:       p,
+		private:    opts.Private,
+		persistent: opts.Persistent,
+	})
+
+	if !set {
+		return nil, errors.New("peer already exists")
+	}
+
+	if opts.OnBeforeStart != nil {
+		opts.OnBeforeStart(p)
+	}
+
+	if err := p.Start(); err != nil {
+		ps.unset(id)
+		return nil, errors.Wrap(err, "unable to start peer")
+	}
+
+	if opts.OnAfterStart != nil {
+		opts.OnAfterStart(p)
+	}
+
+	ps.metrics.Peers.Add(1)
+
+	return p, nil
+}
+
+type PeerRemovalOptions struct {
+	OnAfterStop func(p *Peer)
+}
+
+func (ps *PeerSet) Remove(key p2p.ID, opts PeerRemovalOptions) error {
+	id := ps.keyToPeerID(key)
+	if id == "" {
+		return errors.New("invalid peer key")
+	}
+
+	if id == ps.host.ID() {
+		return errors.New("peer is self")
+	}
+
+	item, ok := ps.unset(id)
+	if !ok {
+		return errors.New("peer not found")
+	}
+
+	if err := item.peer.Stop(); err != nil {
+		return errors.Wrap(err, "failed to stop peer")
+	}
+
+	if opts.OnAfterStop != nil {
+		opts.OnAfterStop(item.peer)
+	}
+
+	ps.metrics.Peers.Add(-1)
+
+	if err := ps.host.Network().ClosePeer(id); err != nil {
+		return errors.Wrap(err, "failed to close peer")
+	}
+
+	return nil
+}
+
+func (ps *PeerSet) ForEach(fn func(p2p.Peer)) {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	for _, item := range ps.peers {
+		fn(item.peer)
+	}
+}
+
+func (ps *PeerSet) Random() p2p.Peer {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	if len(ps.peers) == 0 {
+		return nil
+	}
+
+	v := rand.Intn(len(ps.peers))
+	for _, item := range ps.peers {
+		if v == 0 {
+			return item.peer
+		}
+
+		v--
+	}
+
+	return nil
+}
+
+// Copy returns a copy of the peers list.
+// This method is barely used and remains only for backwards compatibility.
+func (ps *PeerSet) Copy() []p2p.Peer {
+	ps.mu.RLock()
+
+	items := make([]peerSetItem, 0, len(ps.peers))
+	for _, item := range ps.peers {
+		items = append(items, item)
+	}
+
+	ps.mu.RUnlock()
+
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].peer.ID() < items[j].peer.ID()
+	})
+
+	results := make([]p2p.Peer, 0, len(items))
+	for _, item := range items {
+		results = append(results, item.peer)
+	}
+
+	return results
+}
+
+// Size returns the number of peers in the peerSet.
+func (ps *PeerSet) Size() int {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	return len(ps.peers)
+}
+
+func (ps *PeerSet) getByKey(key p2p.ID) (*Peer, bool) {
+	id := ps.keyToPeerID(key)
+	if id == "" {
+		return nil, false
+	}
+
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	item, ok := ps.peers[id]
+	if !ok {
+		return nil, false
+	}
+
+	return item.peer, true
+}
+
+// set adds a peer to the peer set and returns true if it was added
+func (ps *PeerSet) set(id peer.ID, item peerSetItem) bool {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	if _, ok := ps.peers[id]; ok {
+		return false
+	}
+
+	ps.peers[id] = item
+
+	return true
+}
+
+// unset removes a peer from the peer set and returns it
+// returns nil if peer is not found
+func (ps *PeerSet) unset(id peer.ID) (peerSetItem, bool) {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	item, ok := ps.peers[id]
+	if !ok {
+		return peerSetItem{}, false
+	}
+
+	delete(ps.peers, id)
+
+	return item, true
+}
+
+func (ps *PeerSet) keyToPeerID(key p2p.ID) peer.ID {
+	if key == "" {
+		return ""
+	}
+
+	b, err := base58.Decode(string(key))
+	if err != nil {
+		ps.logger.Error("Failed to decode base58 key", "peer_key", key, "err", err)
+		return ""
+	}
+
+	id, err := peer.IDFromBytes(b)
+	if err != nil {
+		ps.logger.Error("Failed to convert bytes to peer ID", "peer_key", key, "err", err)
+		return ""
+	}
+
+	return id
+}
+
+// note that peerID.String() is base58 encoded and
+// raw peerID is string([]byte)!
+func peerIDToKey(id peer.ID) p2p.ID {
+	return p2p.ID(id.String())
+}

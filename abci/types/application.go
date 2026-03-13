a1745e93e3e9c82ca660ee8b7c5a9ba8afb6d394
Matt Acciai <matt@cosmoslabs.io>
2026-03-13 11:14:04 -0400

    remove InsertTx and ReapTxs ABCI methods

--- abci/types/application.go
+++ abci/types/application.go
@@ -12,9 +12,7 @@ type Application interface {
 	Query(context.Context, *RequestQuery) (*ResponseQuery, error) // Query for state
 
 	// Mempool Connection
-	CheckTx(context.Context, *RequestCheckTx) (*ResponseCheckTx, error)    // Validate a tx for the mempool
-	InsertTx(context.Context, *RequestInsertTx) (*ResponseInsertTx, error) // Insert a tx into the mempool
-	ReapTxs(context.Context, *RequestReapTxs) (*ResponseReapTxs, error)    // Reap valid txs from the mempool
+	CheckTx(context.Context, *RequestCheckTx) (*ResponseCheckTx, error) // Validate a tx for the mempool
 
 	// Consensus Connection
 	InitChain(context.Context, *RequestInitChain) (*ResponseInitChain, error) // Initialize blockchain w validators/other info from CometBFT

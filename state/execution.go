b027fb03c728a9f7f8e5190e726eff89a1f0d083
mattac21 <matt@cosmoslabs.io>
2026-04-07 13:31:38 -0400

    fix(blocksync): ensure full verification of `second.LastCommit` during sync (#5753)
    Ensures `second.LastCommit` contains all verified signatures, not just
    2/3 (changes verification from light to full). This is an issue during
    adaptive sync since we reconstruct state based on this commit, _**not**_
    the current blocks last commit, thus we must make sure
    `second.LastCommit` is fully valid.
    
    Since blocksync and adaptivesync ensure that blocks are processed
    sequentially, this adds an optimization where we optionally skip commit
    verification. We do this when we are not processing our first height. We
    know this is ok since we have fully verified `second.LastCommit` during
    the previous iteration of the loop, so now `second.LastCommit` is the
    `first` block, and we know its commit is already validated.
    
    ---
    
    #### PR checklist
    
    - [x] Tests written/updated
    - [ ] Changelog entry added in `CHANGELOG.md`
    - [ ] Updated relevant documentation (`docs/` or `spec/`) and code
    comments
    
    ---------
    
    Co-authored-by: Dmitry S <11892559+swift1337@users.noreply.github.com>
    (cherry picked from commit d0de3f1cedce6f1887db3114fd04918293c8cb8b)
    
    # Conflicts:
    #	consensus/state_ingest.go
    #	consensus/state_ingest_test.go
    #	state/execution.go

--- state/execution.go
+++ state/execution.go
@@ -200,15 +200,43 @@ func (blockExec *BlockExecutor) ProcessProposal(
 // Validation does not mutate state, but does require historical information from the stateDB,
 // ie. to verify evidence from a validator at an old height.
 func (blockExec *BlockExecutor) ValidateBlock(state State, block *types.Block) error {
+<<<<<<< HEAD
 	err := validateBlock(state, block, blockExec.withBlockTimeTolerance)
 	if err != nil {
 		return err
+=======
+	return blockExec.validateBlockAndCheckEvidence(state, block)
+}
+
+// ValidateBlockSkipLastCommit validates the same as blockexec.ValidateBlock
+// however it performs no validation of the block's LastCommit.
+//
+// This should only be used if you know that the LastCommit has already been validated elsewhere.
+func (blockExec *BlockExecutor) ValidateBlockSkipLastCommit(state State, block *types.Block) error {
+	return blockExec.validateBlockAndCheckEvidence(state, block, withSkipLastCommit)
+}
+
+func (blockExec *BlockExecutor) validateBlockAndCheckEvidence(state State, block *types.Block, opts ...func(*blockValidationOptions)) error {
+	lastValidated := blockExec.GetLastValidatedBlock()
+
+	// safe to call with nil
+	if !lastValidated.HashesTo(block.Hash()) {
+		// always use blocktime tolerance set on the struct
+		if err := validateBlock(state, block, append(opts, blockExec.withBlockTimeTolerance)...); err != nil {
+			return err
+		}
+		blockExec.setLastValidatedBlock(lastValidated, block)
+>>>>>>> d0de3f1c (fix(blocksync): ensure full verification of `second.LastCommit` during sync (#5753))
 	}
 	return blockExec.evpool.CheckEvidence(block.Evidence.Evidence)
 }
 
 func (blockExec *BlockExecutor) withBlockTimeTolerance(opts *blockValidationOptions) {
 	opts.blockTimeTolerance = blockExec.blockTimeTolerance
+}
+
+func withSkipLastCommit(opts *blockValidationOptions) {
+	opts.skipLastCommitVerification = true
 }
 
 // ApplyVerifiedBlock does the same as `ApplyBlock`, but skips verification.
